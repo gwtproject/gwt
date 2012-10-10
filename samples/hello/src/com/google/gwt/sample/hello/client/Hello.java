6d8802f22bdc3cf6244fe0c79dd8362a5a5761a6
Matthew Dempsky <matthew@dempsky.org>
2012-10-10 12:39:13 -0700

    Say hello to Gerrit!
    Change-Id: I39083dbf9575124ea3dd5c181ce203f207cc854c

--- samples/hello/src/com/google/gwt/sample/hello/client/Hello.java
+++ samples/hello/src/com/google/gwt/sample/hello/client/Hello.java
@@ -30,7 +30,7 @@ public class Hello implements EntryPoint {
   public void onModuleLoad() {
     Button b = new Button("Click me", new ClickHandler() {
       public void onClick(ClickEvent event) {
-        Window.alert("Hello, AJAX");
+        Window.alert("Hello, Gerrit!");
       }
     });
 

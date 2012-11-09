d8ba4798c60c149403857251952975b4d31a630d
Thomas Broyer <t.broyer@gmail.com>
2012-11-09 18:16:03 +0100

    Move CompileModuleOptions to a new file and make it public.
    This allows calling CompileModule from Java without the need to fork a JVM.
    
    Change-Id: I59cbd77db6ef529ec0459a54f683f4f2331b80bb

---
+++ dev/core/src/com/google/gwt/dev/CompileModuleOptions.java
@@ -0,0 +1,10 @@
+package com.google.gwt.dev;
+
+import com.google.gwt.dev.util.arg.OptionOutDir;
+import com.google.gwt.dev.util.arg.OptionStrict;
+
+/**
+ * The complete set of options for {@link CompileModule}.
+ */
+public interface CompileModuleOptions extends CompileTaskOptions, OptionOutDir, OptionStrict {
+}

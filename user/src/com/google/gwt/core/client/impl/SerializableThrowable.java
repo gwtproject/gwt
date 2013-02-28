f768220881eb0cc7e165128c5c8dbca0e1ed1063
Goktug Gokdogan <goktug@google.com>
2013-02-28 11:30:57 -0800

    Introduces a common SerializableThrowable and refactors StackTraceDeobfuscator as public API.
    This is the first step on having a unified way to transfer and deobfuscate exceptions in GWT.
    
    Change-Id: I35321bea2dfd2ec3ff2df1b76ad4f6254b84c08a
    Review-Link: https://gwt-review.googlesource.com/#/c/2040/

--- user/src/com/google/gwt/core/client/impl/SerializableThrowable.java
+++ user/src/com/google/gwt/core/client/impl/SerializableThrowable.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,94 +16,49 @@
 
 package com.google.gwt.core.client.impl;
 
-import java.io.Serializable;
-
 /**
- * The emulated Throwable class does not serialize Throwables recursively and
- * does not serialize the stack trace.  This class is an alternative, and
- * can be used by writing a custom serializer for the class which contains a
- * Throwable. See {@link LogRecord_CustomFieldSerializer} as an example.
- *
+ * @deprecated use {@link com.google.gwt.core.shared.SerializableThrowable} instead.
  */
-public class SerializableThrowable implements Serializable {
-  private SerializableThrowable cause = null;
-  private String message = null;
-  private StackTraceElement[] stackTrace = null;
-  private String typeName = null;
+@Deprecated
+public class SerializableThrowable extends com.google.gwt.core.shared.SerializableThrowable {
 
   /**
-   * A subclass of Throwable that contains the serialized exception class type.
+   * @deprecated use com.google.gwt.core.client.SerializableThrowable instead.
    */
-  public static class ThrowableWithClassName extends Throwable {
-
-    private String typeName;
+  @Deprecated
+  public static class ThrowableWithClassName
+      extends com.google.gwt.core.shared.SerializableThrowable {
 
     public ThrowableWithClassName(String message, Throwable cause, String typeName) {
-      super(message, cause);
-      this.typeName = typeName;
+      this(message, typeName);
+      initCause(cause);
     }
 
     public ThrowableWithClassName(String message, String typeName) {
-      super(message);
-      this.typeName = typeName;
+      setMessage(message);
+      setOriginalClassName(typeName);
     }
 
     public String getExceptionClass() {
-      return typeName;
+      return getOriginalClassName();
     }
   }
 
-  /**
-   * Create a new SerializableThrowable from a Throwable.
-   */
-  public SerializableThrowable(Throwable t) {
-    message = t.getMessage();
-    if (t.getCause() != null && t.getCause() != t) {
-      cause = new SerializableThrowable(t.getCause());
-    }
-    stackTrace = t.getStackTrace();
-    typeName = t.getClass().getName();
-  }
-  
   protected SerializableThrowable() {
     // for serialization
   }
-  
-  public SerializableThrowable getCause() {
-    return cause;
-  }
-  
-  public String getMessage() {
-    return message;
-  }
-  
-  public StackTraceElement[] getStackTrace() {
-    return stackTrace;
+
+  public SerializableThrowable(Throwable t) {
+    setMessage(t.getMessage());
+    setStackTrace(t.getStackTrace());
+    initCause(t.getCause());
+    setOriginalClassName(t.getClass().getName());
   }
 
   /**
-   * Create a new Throwable from this SerializableThrowable.
+   * Create a Throwable from this SerializableThrowable.
    */
   public Throwable getThrowable() {
-    Throwable t;
-    if (cause != null) {
-      t = new ThrowableWithClassName(message, cause.getThrowable(), typeName);
-    } else {
-      t = new ThrowableWithClassName(message, typeName);
-    }
-    t.setStackTrace(stackTrace);
-    return t;
-  }
-  
-  public void setCause(SerializableThrowable c) {
-    cause = c;
-  }
-  
-  public void setMessage(String msg) {
-    message = msg;
-  }
-  
-  public void setStackTrace(StackTraceElement[] st) {
-    stackTrace = st;
+    return this;
   }
 }

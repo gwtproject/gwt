e65b7de78e2aeec7b2d862ed1f3733c5c4468bf4
Roberto Lublinerman <rluble@google.com>
2013-05-13 16:39:44 -0700

    Make sub/superclass queries in JTypeOracle  more efficient.
    Assign a unique id to each class during for global optimization, such
    that the following invariant holds:
      a.id < b.id   for b != a and b subclass of a.
      forall c.id > a.id, c.id <= max_{b subclass of a}(b.id) . c subclass of a
    
    The aforementioned invariant allows to respond to the query subclass(a,b) very 
    cheaply  with just two integer comparisons (plus pointer dereferences).
    
    Change-Id: Ibe256bc07834b1518a9e90872fdd655e3e242790

--- dev/core/src/com/google/gwt/dev/jjs/ast/JTypeOracle.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/JTypeOracle.java
@@ -23,7 +23,10 @@ import com.google.gwt.dev.util.collect.IdentityHashSet;
 import com.google.gwt.dev.util.collect.IdentitySets;
 import com.google.gwt.dev.util.collect.Lists;
 import com.google.gwt.dev.util.collect.Maps;
+import com.google.gwt.dev.util.log.speedtracer.CompilerEventType;
+import com.google.gwt.dev.util.log.speedtracer.SpeedTracerLogger;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -297,6 +300,13 @@ public class JTypeOracle implements Serializable {
   private final Map<JClassType, Map<String, JMethod>> polyClassMethodMap =
       new IdentityHashMap<JClassType, Map<String, JMethod>>();
 
+
+  /**
+   * The number of declared types.
+   */
+  private int declaredTypesSize;
+
+
   public JTypeOracle(JProgram program) {
     this.program = program;
   }
@@ -481,6 +491,8 @@ public class JTypeOracle implements Serializable {
     jsoSingleImpls.clear();
     dualImpls.clear();
 
+    this.declaredTypesSize = program.getDeclaredTypes().size();
+
     for (JDeclaredType type : program.getDeclaredTypes()) {
       if (type instanceof JClassType) {
         recordSuperSubInfo((JClassType) type);
@@ -488,6 +500,13 @@ public class JTypeOracle implements Serializable {
         recordSuperSubInfo((JInterfaceType) type);
       }
     }
+
+    /*
+     * Compute efficient representation for hierarchy sub/supertype queries.
+     */
+    computeIds();
+
+//    HierarchyEncoder.compute(program);
 
     /*
      * Now that the basic type hierarchy is computed, compute which JSOs
@@ -645,14 +664,14 @@ public class JTypeOracle implements Serializable {
    * Returns true if qType is a subclass of type, directly or indirectly.
    */
   public boolean isSubClass(JClassType type, JClassType qType) {
-    return get(subClassMap, type).contains(qType);
+    return isSubtype(qType, type);
   }
 
   /**
    * Returns true if qType is a superclass of type, directly or indirectly.
    */
   public boolean isSuperClass(JClassType type, JClassType qType) {
-    return get(superClassMap, type).contains(qType);
+    return isSubtype(type, qType);
   }
 
   /**
@@ -686,6 +705,8 @@ public class JTypeOracle implements Serializable {
       }
     }
   }
+
+
 
   public void setInstantiatedTypes(Set<JReferenceType> instantiatedTypes) {
     this.instantiatedTypes = instantiatedTypes;
@@ -1011,4 +1032,57 @@ public class JTypeOracle implements Serializable {
     }
   }
 
+  /*
+   * Make sure we recompute class ids when reloading serialized type oracles.
+   */
+  private void readObject(java.io.ObjectInputStream in) throws IOException,
+      ClassNotFoundException {
+    in.defaultReadObject();
+    // recompute class hierarchy query data structure;
+    computeIds();
+  }
+
+
+  /**
+   * Compute class ids for efficient lookups.
+   *
+   * A clever encoding of the class hierarchy reduces the cost of one of the hottest methods
+   * during optimize.
+   *
+   * NOTE: Only encodes the (single inheritance) class hierarchy.
+   *
+   * The encoding preserves the following invariant
+   * A is strict subclass of B <=>  id(A) < id(B) ^ id(B) =< max{C subclass A}(Id(C))
+   *
+   * An easy way to guaranteed this propery is to number sequentially while traversing the
+   * hierarchy tree in preorder.
+   */
+  private transient int lastClassId = -1;
+  private transient int[] maxDescendants = null;
+
+  boolean isSubtype(JClassType sub, JClassType sup) {
+    int supId = sup.getClassId();
+    int subId = sub.getClassId();
+    return supId < subId  &&
+        subId <= maxDescendants[supId];
+  }
+
+  private void computeIds() {
+    SpeedTracerLogger.Event computeHierarchyEvent =
+        SpeedTracerLogger.start(CompilerEventType.OPTIMIZE, "pass", "Compute Type Hierarchy");
+    lastClassId = -1;
+    maxDescendants = new int[declaredTypesSize];
+    computeIdTraversal(javaLangObject);
+    computeHierarchyEvent.end();
+  }
+
+  private void computeIdTraversal(JClassType type) {
+    type.setClassId(++lastClassId);
+    for (JClassType sub : get(subClassMap, type)) {
+      if (sub.getSuperClass() == type) {
+        computeIdTraversal(sub);
+      }
+    }
+    maxDescendants[type.getClassId()] = lastClassId;
+  }
 }

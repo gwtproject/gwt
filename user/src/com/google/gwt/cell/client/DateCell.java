3058ee001f3ae444ae54b29ede6a4a1bc51b1d5c
Daniel Kurka <dankurka@google.com>
2013-06-30 23:22:37 +0200

    Make DateCell use non deprecated DateTimeFormat
    fixes issue 8201
    
    Change-Id: I4fe3065313a019ef5c97427e925d45dadc6ef434

--- user/src/com/google/gwt/cell/client/DateCell.java
+++ user/src/com/google/gwt/cell/client/DateCell.java
@@ -16,7 +16,6 @@
 package com.google.gwt.cell.client;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
 import com.google.gwt.i18n.client.TimeZone;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.text.shared.SafeHtmlRenderer;
@@ -35,12 +34,17 @@ public class DateCell extends AbstractCell<Date> {
 
   private final TimeZone timeZone;
 
+  private final com.google.gwt.i18n.shared.DateTimeFormat sharedFormat;
+
+  private final com.google.gwt.i18n.shared.TimeZone timeZoneShared;
+
   /**
    * Construct a new {@link DateCell} using the format
    * {@link PredefinedFormat#DATE_FULL} and a {@link SimpleSafeHtmlRenderer}.
    */
   public DateCell() {
-    this(DateTimeFormat.getFormat(PredefinedFormat.DATE_FULL),
+    this(com.google.gwt.i18n.shared.DateTimeFormat.getFormat(
+        com.google.gwt.i18n.shared.DateTimeFormat.PredefinedFormat.DATE_FULL),
         SimpleSafeHtmlRenderer.getInstance(), null);
   }
 
@@ -52,7 +56,9 @@ public class DateCell extends AbstractCell<Date> {
    *          formatted date as HTML
    */
   public DateCell(SafeHtmlRenderer<String> renderer) {
-    this(DateTimeFormat.getFormat(PredefinedFormat.DATE_FULL), renderer, null);
+    this(com.google.gwt.i18n.shared.DateTimeFormat.getFormat(
+      com.google.gwt.i18n.shared.DateTimeFormat.PredefinedFormat.DATE_FULL),
+      renderer, null);
   }
 
   /**
@@ -60,7 +66,11 @@ public class DateCell extends AbstractCell<Date> {
    * {@link SimpleSafeHtmlRenderer}.
    *
    * @param format the {@link DateTimeFormat} used to render the date
+   *
+   * @deprecated use
+   * {@link DateCell#DateCell(com.google.gwt.i18n.shared.DateTimeFormat)}
    */
+  @Deprecated
   public DateCell(DateTimeFormat format) {
     this(format, SimpleSafeHtmlRenderer.getInstance(), null);
   }
@@ -72,7 +82,11 @@ public class DateCell extends AbstractCell<Date> {
    * @param format the {@link DateTimeFormat} used to render the date
    * @param renderer a non-null {@link SafeHtmlRenderer} used to render the
    *          formatted date
+   *
+   * @deprecated use
+   * {@link DateCell#DateCell(com.google.gwt.i18n.shared.DateTimeFormat, SafeHtmlRenderer)}
    */
+  @Deprecated
   public DateCell(DateTimeFormat format, SafeHtmlRenderer<String> renderer) {
     this(format, renderer, null);
   }
@@ -81,11 +95,15 @@ public class DateCell extends AbstractCell<Date> {
    * Construct a new {@link DateCell} using the specified format and time zone.
    *
    * @param format the {@link DateTimeFormat} used to render the date
-   * @param timezone the {@link TimeZone} used to render the date, or null to
+   * @param timeZone the {@link TimeZone} used to render the date, or null to
    *          use the default behavior for the local time zone and the rendered
    *          date. See {@link DateTimeFormat#format(Date)} and
    *          {@link Date#getTimezoneOffset()}
+   *
+   * @deprecated use
+   * {@link DateCell#DateCell(com.google.gwt.i18n.shared.DateTimeFormat, com.google.gwt.i18n.shared.TimeZone)}
    */
+  @Deprecated
   public DateCell(DateTimeFormat format, TimeZone timeZone) {
     this(format, SimpleSafeHtmlRenderer.getInstance(), timeZone);
   }
@@ -97,11 +115,15 @@ public class DateCell extends AbstractCell<Date> {
    * @param format the {@link DateTimeFormat} used to render the date
    * @param renderer a non-null {@link SafeHtmlRenderer} used to render the
    *          formatted date
-   * @param timezone the {@link TimeZone} used to render the date, or null to
+   * @param timeZone the {@link TimeZone} used to render the date, or null to
    *          use the default behavior for the local time zone and the rendered
    *          date. See {@link DateTimeFormat#format(Date)} and
    *          {@link Date#getTimezoneOffset()}
+   *
+   * @deprecated use
+   * {@link DateCell#DateCell(com.google.gwt.i18n.shared.DateTimeFormat, SafeHtmlRenderer, com.google.gwt.i18n.shared.TimeZone)}
    */
+  @Deprecated
   public DateCell(DateTimeFormat format, SafeHtmlRenderer<String> renderer,
       TimeZone timeZone) {
     if (format == null) {
@@ -111,14 +133,89 @@ public class DateCell extends AbstractCell<Date> {
       throw new IllegalArgumentException("renderer == null");
     }
     this.format = format;
+    this.sharedFormat = null;
     this.renderer = renderer;
     this.timeZone = timeZone;
+    this.timeZoneShared = null;
+  }
+
+  /**
+   * Construct a new {@link DateCell} using the specified format and a
+   * {@link SimpleSafeHtmlRenderer}.
+   *
+   * @param format the {@link com.google.gwt.i18n.shared.DateTimeFormat}
+   *          used to render the date
+   */
+  public DateCell(com.google.gwt.i18n.shared.DateTimeFormat format) {
+    this(format, SimpleSafeHtmlRenderer.getInstance(), null);
+  }
+
+  /**
+   * Construct a new {@link DateCell} using the specified format and the given
+   * {@link SafeHtmlRenderer}.
+   *
+   * @param format the {@link com.google.gwt.i18n.shared.DateTimeFormat}
+   *          used to render the date
+   * @param renderer a non-null {@link SafeHtmlRenderer} used to render the
+   *          formatted date
+   */
+  public DateCell(com.google.gwt.i18n.shared.DateTimeFormat format, SafeHtmlRenderer<String> renderer) {
+    this(format, renderer, null);
+  }
+
+  /**
+   * Construct a new {@link DateCell} using the specified format and time zone.
+   *
+   * @param format the {@link com.google.gwt.i18n.shared.DateTimeFormat}
+   *          used to render the date
+   * @param timeZone the {@link com.google.gwt.i18n.shared.TimeZone}
+   *          used to render the date, or null to
+   *          use the default behavior for the local time zone and the rendered
+   *          date. See {@link DateTimeFormat#format(Date)} and
+   *          {@link Date#getTimezoneOffset()}
+   */
+  public DateCell(com.google.gwt.i18n.shared.DateTimeFormat format,
+      com.google.gwt.i18n.shared.TimeZone timeZone) {
+    this(format, SimpleSafeHtmlRenderer.getInstance(), timeZone);
+  }
+
+  /**
+   * Construct a new {@link DateCell} using the specified format, the given
+   * {@link SafeHtmlRenderer}, and the specified time zone.
+   *
+   * @param format the {@link com.google.gwt.i18n.shared.DateTimeFormat}
+   *          used to render the date
+   * @param renderer a non-null {@link SafeHtmlRenderer} used to render the
+   *          formatted date
+   * @param timeZone the {@link com.google.gwt.i18n.shared.TimeZone}
+   *          used to render the date, or null to
+   *          use the default behavior for the local time zone and the rendered
+   *          date. See {@link DateTimeFormat#format(Date)} and
+   *          {@link Date#getTimezoneOffset()}
+   */
+  public DateCell(com.google.gwt.i18n.shared.DateTimeFormat format, SafeHtmlRenderer<String> renderer,
+      com.google.gwt.i18n.shared.TimeZone timeZone) {
+    if (format == null) {
+      throw new IllegalArgumentException("format == null");
+    }
+    if (renderer == null) {
+      throw new IllegalArgumentException("renderer == null");
+    }
+    this.format = null;
+    this.sharedFormat = format;
+    this.renderer = renderer;
+    this.timeZone = null;
+    this.timeZoneShared = timeZone;
   }
 
   @Override
   public void render(Context context, Date value, SafeHtmlBuilder sb) {
     if (value != null) {
-      sb.append(renderer.render(format.format(value, timeZone)));
+      if (sharedFormat != null) {
+        sb.append(renderer.render(sharedFormat.format(value, timeZoneShared)));
+      } else {
+        sb.append(renderer.render(format.format(value, timeZone)));
+      }   
     }
   }
 }

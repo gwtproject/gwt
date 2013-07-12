b80b52f4a396220ad15a6fbe47e7a915c77856d7
Alberto Mancini <ab.mancini@gmail.com>
2013-07-12 12:23:28 +0200

    Adds LoadedMetadataEvent and relative Handler to MediaBase
    Change-Id: Idd02bdf0baeac57a90c5cd8355f028f44dff7443

--- user/src/com/google/gwt/event/dom/client/HasLoadedMetadataHandlers.java
+++ user/src/com/google/gwt/event/dom/client/HasLoadedMetadataHandlers.java
@@ -13,19 +13,27 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.google.gwt.event.dom.client;
 
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
+
 /**
- * This is a convenience interface that includes all media handlers defined by
- * the core GWT system.
- * 
+ * A widget that implements this interface provides registration for
+ * {@link LoadedMetadataHandler} instances.
+ *
  * <p>
  * <span style="color:red">Experimental API: This API is still under development
  * and is subject to change.
  * </span>
  * </p>
  */
-public interface HasAllMediaHandlers extends HasEndedHandlers,
-  HasProgressHandlers, HasCanPlayThroughHandlers, HasLoadedMetadataHandlers {
+public interface HasLoadedMetadataHandlers extends HasHandlers {
+  /**
+   * Adds a {@link LoadedMetadataEvent} handler.
+   *
+   * @param handler the media loadedmetadata handler
+   * @return {@link HandlerRegistration} used to remove this handler
+   */
+  HandlerRegistration addLoadedMetadataHandler(LoadedMetadataHandler handler);
 }
--- user/src/com/google/gwt/user/client/impl/DOMImplStandard.java
+++ user/src/com/google/gwt/user/client/impl/DOMImplStandard.java
@@ -275,6 +275,7 @@ abstract class DOMImplStandard extends DOMImpl {
       case "canplaythrough":
       case "ended":
       case "progress":
+      case "loadedmetadata":
         // First call removeEventListener, so as not to add the same event listener more than once
         elem.removeEventListener(eventTypeName, @com.google.gwt.user.client.impl.DOMImplStandard::dispatchEvent, false);
         elem.addEventListener(eventTypeName, @com.google.gwt.user.client.impl.DOMImplStandard::dispatchEvent, false);

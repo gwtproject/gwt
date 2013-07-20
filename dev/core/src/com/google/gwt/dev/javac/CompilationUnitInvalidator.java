b9163b2efdcf57371b3222c837ec55639587d7be
Jared King <jaredking@google.com>
2013-07-19 19:18:02 -0500

    Improve CompilationUnitInvalidator performance for large GWT compiles by reducing copying and preferring map-lookups over sequential scanning.
    While compiling three UI's that I've worked on, the time spent
    in the old version varied from about 0.5 seconds up to 12
    seconds.  The new version takes negligible time up-to about
    100ms for the same codebases.
    
    Change-Id: I099e0093b2d48b6d8c5e19634d028db40382d96c
    Review-Link: https://gwt-review.googlesource.com/#/c/3800/

--- dev/core/src/com/google/gwt/dev/javac/CompilationUnitInvalidator.java
+++ dev/core/src/com/google/gwt/dev/javac/CompilationUnitInvalidator.java
@@ -17,14 +17,13 @@ package com.google.gwt.dev.javac;
 
 import com.google.gwt.core.ext.TreeLogger;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Map.Entry;
 
 /**
  * Helper class to invalidate units in a set based on errors or references to
@@ -32,50 +31,111 @@ import java.util.Map.Entry;
  */
 public class CompilationUnitInvalidator {
 
+  /**
+   * Mutates {@code units} by retaining only valid units.  A unit is invalid if
+   * it
+   * <ul>
+   * <li>has errors,
+   * <li>depends on a member of {@code validClasses} that has errors,
+   * <li>depends on another member of {@code units} that has errors, or
+   * <li>references a source name that is not provided by a good member of
+   * {@code units} or {@code validClasses}.
+   * </ul>
+   */
   public static void retainValidUnits(TreeLogger logger,
       Collection<CompilationUnit> units, Map<String, CompiledClass> validClasses) {
     logger = logger.branch(TreeLogger.TRACE, "Removing invalidated units");
 
-    // Assume all units are valid at first.
-    Set<CompilationUnit> currentlyValidUnits = new LinkedHashSet<CompilationUnit>();
-    Set<String> currentlyValidClasses = new HashSet<String>();
+    // Build a map of api-refs -> dependent units.
+    // This map excludes refs that point at refs provided by good validClasses.
+    Map<String, List<CompilationUnit>> depsNeeded = new HashMap<String, List<CompilationUnit>>();
+
+    // This set contains the source names of types provided by units, and after
+    // initial population, may shrink as problems are discovered in individual
+    // units.
+    Set<String> depsProvided = new HashSet<String>();
+
+    // These are all of the invalid units
+    Set<CompilationUnit> allBrokenUnits = new HashSet<CompilationUnit>();
+
+    // Populate depsNeeded, depsProvided, and allBrokenUnits with their initial values.
+    // At first, only compilation units that directly contain an error are known to be
+    // broken, not their dependencies.
     for (CompilationUnit unit : units) {
-      if (!unit.isError()) {
-        currentlyValidUnits.add(unit);
+      if (unit.isError()) {
+        // It is bad and can be removed immediately
+        allBrokenUnits.add(unit);
+      } else {
+        // Update set of dependencies the unit provides
         for (CompiledClass cc : unit.getCompiledClasses()) {
-          currentlyValidClasses.add(cc.getSourceName());
+          depsProvided.add(cc.getSourceName());
+        }
+
+        // Update map of dependencies that the unit needs
+        for (String ref : unit.getDependencies().getApiRefs()) {
+          // Check validClasses
+          CompiledClass compiledClass = validClasses.get(ref);
+          if ((compiledClass == null) || compiledClass.getUnit().isError()) {
+            // we'll put this into the double-check pot
+            appendAtKey(depsNeeded, ref, unit);
+          }
         }
       }
     }
-    for (Entry<String, CompiledClass> entry : validClasses.entrySet()) {
-      if (!entry.getValue().getUnit().isError()) {
-        currentlyValidClasses.add(entry.getKey());
-      }
-    }
 
+    // Repeatedly remove CompilationUnits that have a dependency that's known to
+    // be broken.
     boolean changed;
     do {
-      changed = false;
-      for (Iterator<CompilationUnit> it = currentlyValidUnits.iterator(); it.hasNext();) {
-        CompilationUnit unitToCheck = it.next();
-        List<String> invalidRefs = unitToCheck.getDependencies().findMissingApiRefs(
-            currentlyValidClasses);
-        if (invalidRefs.size() > 0) {
-          it.remove();
-          for (CompiledClass cc : unitToCheck.getCompiledClasses()) {
-            currentlyValidClasses.remove(cc.getSourceName());
+      // Find the missing deps for this pass
+      Map<String, List<CompilationUnit>> missing =
+          new HashMap<String, List<CompilationUnit>>(depsNeeded);
+      missing.keySet().removeAll(depsProvided);
+
+      // Process the units with missing deps
+      for (Map.Entry<String, List<CompilationUnit>> brokenEntry : missing.entrySet()) {
+        List<CompilationUnit> brokenUnits = brokenEntry.getValue();
+        allBrokenUnits.addAll(brokenUnits);
+
+        // Remove the broken units from the provides set
+        for (CompilationUnit brokenUnit : brokenUnits) {
+          for (CompiledClass cc : brokenUnit.getCompiledClasses()) {
+            depsProvided.remove(cc.getSourceName());
           }
-          changed = true;
-          TreeLogger branch = logger.branch(TreeLogger.DEBUG,
-              "Compilation unit '" + unitToCheck
-                  + "' is removed due to invalid reference(s):");
-          for (String ref : invalidRefs) {
-            branch.log(TreeLogger.DEBUG, ref);
+        }
+
+        // Log it to maintain some logging compatibility with prior versions
+        // of this class.
+        for (CompilationUnit compilationUnit : brokenUnits) {
+          if (!allBrokenUnits.contains(compilationUnit)) {
+            TreeLogger branch = logger.branch(TreeLogger.DEBUG,
+                "Compilation unit '" + compilationUnit
+                + "' is removed due to invalid reference(s):");
+            branch.log(TreeLogger.DEBUG, brokenEntry.getKey());
           }
         }
       }
+
+      // Having found and removed some units with missing deps, remove their
+      // needs.
+      depsNeeded.keySet().removeAll(missing.keySet());
+
+      changed = !missing.isEmpty();
     } while (changed);
 
-    units.retainAll(currentlyValidUnits);
+    units.removeAll(allBrokenUnits);
+  }
+
+  /**
+   * Appends the value to a list found in the map at the given key.  Creates and
+   * adds a new list to the map if there is no entry at key.
+   */
+  private static <K, V> void appendAtKey(Map<K, List<V>> map, K key, V value) {
+    List<V> list = map.get(key);
+    if (list == null) {
+      list = new ArrayList<V>();
+      map.put(key, list);
+    }
+    list.add(value);
   }
 }

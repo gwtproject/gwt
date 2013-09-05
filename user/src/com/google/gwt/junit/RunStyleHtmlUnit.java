0637f970389a9cf567fd1126d85f1e66e67ba38b
Goktug Gokdogan <goktug@google.com>
2013-09-04 20:02:19 -0700

    Upgrades third-party libraries that GWT transitively depends on via htmlunit.
    Some major upgrades:
      /htmlunit
      /jetty
      /apache/commons
      /apache/http
      /xerces
      /tomcat/servlet-api
    
    Fixes issue 7224
    
    Change-Id: I36cc569c2926aa59ee7375e719b917d6d3681a43
    Review-Link: https://gwt-review.googlesource.com/#/c/4323/

--- user/src/com/google/gwt/junit/RunStyleHtmlUnit.java
+++ user/src/com/google/gwt/junit/RunStyleHtmlUnit.java
@@ -236,8 +236,8 @@ public class RunStyleHtmlUnit extends RunStyle {
   private static Map<String, BrowserVersion> createBrowserMap() {
     Map<String, BrowserVersion> browserMap = new HashMap<String, BrowserVersion>();
     for (BrowserVersion browser : new BrowserVersion[] {
-        BrowserVersion.FIREFOX_3, BrowserVersion.FIREFOX_3_6, BrowserVersion.INTERNET_EXPLORER_6,
-        BrowserVersion.INTERNET_EXPLORER_7}) {
+        BrowserVersion.FIREFOX_17, BrowserVersion.INTERNET_EXPLORER_8,
+        BrowserVersion.INTERNET_EXPLORER_9, BrowserVersion.CHROME}) {
       browserMap.put(browser.getNickname(), browser);
     }
     return Collections.unmodifiableMap(browserMap);
@@ -263,7 +263,7 @@ public class RunStyleHtmlUnit extends RunStyle {
   public int initialize(String args) {
     if (args == null || args.length() == 0) {
       // If no browsers specified, default to Firefox 3.
-      args = "FF3";
+      args = "FF17";
     }
     Set<BrowserVersion> browserSet = new HashSet<BrowserVersion>();
     for (String browserName : args.split(",")) {

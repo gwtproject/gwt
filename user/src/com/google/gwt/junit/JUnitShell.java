edb5a1dc1c6e9f72a3e2e821068ab5351dfa57ab
jdramaix <julien.dramaix@gmail.com>
2013-09-05 16:06:13 +0200

    Re-enable -out options in JUnitShell
    Some tools like IntelliJ and GPE add automatically the -out option when it launches a GwtTestCase. JUnitShell throws an execption when you specify an unknown argument. Without this patch, it's now impossible to launch an  GwtTestCase with IntelliJ.
    
    Related patch : https://gwt-review.googlesource.com/#/c/4324/2
    
    Change-Id: If38e75f6148e48ef32e1494767911700bc7df176

--- user/src/com/google/gwt/junit/JUnitShell.java
+++ user/src/com/google/gwt/junit/JUnitShell.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -64,6 +64,7 @@ import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.junit.client.TimeoutException;
 import com.google.gwt.junit.client.impl.JUnitHost.TestInfo;
 import com.google.gwt.junit.client.impl.JUnitResult;
+import com.google.gwt.util.tools.ArgHandler;
 import com.google.gwt.util.tools.ArgHandlerFlag;
 import com.google.gwt.util.tools.ArgHandlerInt;
 import com.google.gwt.util.tools.ArgHandlerString;
@@ -95,26 +96,26 @@ import java.util.regex.Pattern;
 /**
  * This class is responsible for hosting JUnit test case execution. There are
  * three main pieces to the JUnit system.
- * 
+ *
  * <ul>
  * <li>Test environment</li>
  * <li>Client classes</li>
  * <li>Server classes</li>
  * </ul>
- * 
+ *
  * <p>
  * The test environment consists of this class and the non-translatable version
  * of {@link com.google.gwt.junit.client.GWTTestCase}. These two classes
  * integrate directly into the real JUnit test process.
  * </p>
- * 
+ *
  * <p>
  * The client classes consist of the translatable version of
  * {@link com.google.gwt.junit.client.GWTTestCase}, translatable JUnit classes,
  * and the user's own {@link com.google.gwt.junit.client.GWTTestCase}-derived
  * class. The client communicates to the server via RPC.
  * </p>
- * 
+ *
  * <p>
  * The server consists of {@link com.google.gwt.junit.server.JUnitHostImpl}, an
  * RPC servlet which communicates back to the test environment through a
@@ -174,7 +175,7 @@ public class JUnitShell extends DevMode {
 
       addTagValue("-notHeadless", true);
     }
-    
+
     @Override
     public String getPurposeSnippet() {
       return "Causes the log window and browser windows to be displayed; useful for debugging.";
@@ -549,6 +550,38 @@ public class JUnitShell extends DevMode {
           return true;
         }
       });
+
+      // deprecated -out argument. Still present for backward compatibility because tools using it.
+      // will be totally removed in a future release.
+      registerHandler(new ArgHandler() {
+        @Override
+        public String getPurpose() {
+          return "Don't use -out argument. This option is not used anymore and is still present " +
+              "for backward compatibility only. This option will be removed in future GWT " +
+              "release and will throw an error if it is still used. Please use -war option instead.";
+        }
+
+        @Override
+        public String getTag() {
+          return "-out";
+        }
+
+        @Override
+        public String[] getTagArgs() {
+          return new String[0];
+        }
+
+        @Override
+        public int handle(String[] args, int startIndex) {
+          System.err.println(getPurpose());
+          return 1;
+        }
+
+        @Override
+        public boolean isUndocumented() {
+          return true;
+        }
+      });
     }
 
     @Override
@@ -614,7 +647,7 @@ public class JUnitShell extends DevMode {
   /**
    * Called by {@link com.google.gwt.junit.server.JUnitHostImpl} to get an
    * interface into the test process.
-   * 
+   *
    * @return The {@link JUnitMessageQueue} interface that belongs to the
    *         singleton {@link JUnitShell}, or <code>null</code> if no such
    *         singleton exists.
@@ -629,7 +662,7 @@ public class JUnitShell extends DevMode {
   /**
    * Get the list of remote user agents to compile. This method returns null
    * until all clients have connected.
-   * 
+   *
    * @return the list of remote user agents
    */
   public static String[] getRemoteUserAgents() {
@@ -654,7 +687,7 @@ public class JUnitShell extends DevMode {
   /**
    * Checks if a testCase should not be executed. Currently, a test is either
    * executed on all clients (mentioned in this test) or on no clients.
-   * 
+   *
    * @param testInfo the test info to check
    * @return true iff the test should not be executed on any of the specified
    *         clients.
@@ -748,7 +781,7 @@ public class JUnitShell extends DevMode {
 
   /**
    * Returns the set of banned {@code Platform} for a test method.
-   * 
+   *
    * @param testClass the testClass
    * @param methodName the name of the test method
    */
@@ -1111,7 +1144,7 @@ public class JUnitShell extends DevMode {
     }
     return url;
   }
-  
+
   void maybeCompileForWebMode(ModuleDef module, String... userAgents)
       throws UnableToCompleteException {
     // Load any declared servlets.
@@ -1166,7 +1199,7 @@ public class JUnitShell extends DevMode {
 
   /**
    * Create the specified (or default) runStyle.
-   * 
+   *
    * @param runStyleName the argument passed to -runStyle
    * @return the number of clients, or -1 if initialization was unsuccessful
    */

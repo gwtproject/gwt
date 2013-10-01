2bf9027c7235818a345914252acd11863213af0f
John Stalcup <stalcup@google.com>
2013-09-30 17:31:42 -0700

    adds optional strictResources enforcement and the beginnings of a shared compilerContext object
    Change-Id: I9c6e905ceab2549ae4b03f153e0aa5747a1fa774
    Review-Link: https://gwt-review.googlesource.com/#/c/4750/

--- dev/codeserver/java/com/google/gwt/dev/codeserver/CompilerOptionsImpl.java
+++ dev/codeserver/java/com/google/gwt/dev/codeserver/CompilerOptionsImpl.java
@@ -20,9 +20,9 @@ import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.JsOutputOption;
 import com.google.gwt.dev.util.arg.OptionOptimize;
 import com.google.gwt.dev.util.arg.SourceLevel;
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,14 +30,22 @@ import java.util.List;
  * GWT apps. For now, these settings are hard-coded to reasonable defaults.
  */
 class CompilerOptionsImpl extends UnmodifiableCompilerOptions {
-  private final CompileDir compileDir;
-  private final String moduleName;
-  private final SourceLevel sourceLevel;
+  private CompileDir compileDir;
+  private List<String> moduleNames;
+  private SourceLevel sourceLevel = SourceLevel.DEFAULT_SOURCE_LEVEL;
+  private boolean strictResources;
 
-  CompilerOptionsImpl(CompileDir compileDir, String moduleName, SourceLevel sourceLevel) {
+  CompilerOptionsImpl(CompileDir compileDir, List<String> moduleNames, SourceLevel sourceLevel,
+      boolean strictResources) {
     this.compileDir = compileDir;
-    this.moduleName = moduleName;
+    this.moduleNames = Lists.newArrayList(moduleNames);
     this.sourceLevel = sourceLevel;
+    this.strictResources = strictResources;
+  }
+
+  @Override
+  public boolean enforceStrictResources() {
+    return strictResources;
   }
 
   @Override
@@ -85,7 +93,7 @@ class CompilerOptionsImpl extends UnmodifiableCompilerOptions {
 
   @Override
   public List<String> getModuleNames() {
-    return Arrays.asList(moduleName);
+    return moduleNames;
   }
 
   @Override

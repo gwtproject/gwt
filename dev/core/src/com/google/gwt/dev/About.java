f5e2fb8b17c343c3ab209f5bf16a6c7a542abd8c
Stephen Haberman <stephen.haberman@gmail.com>
2013-10-18 17:17:46 -0500

    Replace gwt.svnrev with gwt.gitsha.
    Change-Id: I3f28322bd6c127b5cfff968c178fa27d001679b8

--- dev/core/src/com/google/gwt/dev/About.java
+++ dev/core/src/com/google/gwt/dev/About.java
@@ -27,7 +27,7 @@ public class About {
    * Tag used for text replacement of the SVN version (split up to avoid
    * replacing it here).
    */ 
-  private static final String GWT_SVNREV_TAG = "@GWT_" + "SVNREV@";
+  private static final String GWT_GITSHA_TAG = "@GWT_" + "GITSHA@";
 
   /**
    * Tag used for text replacement of the GWT version (split up to avoid
@@ -36,7 +36,7 @@ public class About {
   private static final String GWT_VERSION_TAG = "@GWT_" + "VERSION@";
 
   private static final String gwtName = "Google Web Toolkit";
-  private static final String gwtSvnRev;
+  private static final String gwtGitSha;
   private static final GwtVersion gwtVersion;
 
   static {
@@ -49,12 +49,12 @@ public class About {
     }
 
     String tmp;
-    tmp = props.getProperty("gwt.svnrev");
+    tmp = props.getProperty("gwt.gitsha");
     // Check for null or sentinel value
-    if (tmp == null || tmp.equals(GWT_SVNREV_TAG)) {
-      gwtSvnRev = "unknown";
+    if (tmp == null || tmp.equals(GWT_GITSHA_TAG)) {
+      gwtGitSha = "unknown";
     } else {
-      gwtSvnRev = tmp;
+      gwtGitSha = tmp;
     }
 
     tmp = props.getProperty("gwt.version");
@@ -78,9 +78,20 @@ public class About {
    * 
    * @return the subversion revision or 'unknown' if the value couldn't be
    *         determined at build time.
+   * @deprecated See {@link #getGwtGitSha()}.
    */
   public static String getGwtSvnRev() {
-    return gwtSvnRev;
+    return gwtGitSha;
+  }
+
+  /**
+   * Returns the Git repository commit id.
+   * 
+   * @return the Git commit id or 'unknown' if the value couldn't be
+   *         determined at build time.
+   */
+  public static String getGwtGitSha() {
+    return gwtGitSha;
   }
 
   /**

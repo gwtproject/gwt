b22c4b0d9dd962525078d3c9e8f94067c28a2ed6
Roberto Lublinerman <rluble@google.com>
2013-10-22 17:57:01 -0700

    Refactor JForStatement construct.
    The third part of a JForStatement can only be a list of expressions.
    This patch changes its representation from a List<JExpressionStatement>
    to a JExpression (which in some cases will be a JMultiExpression).
    
    Some later passes, such as CompoundAssignmentNormalizer are simplified
    as a result (in a following patch).
    
    Change-Id: Ice90e323698ef5ec24ff744df04ee0c182d0e7e9

--- dev/core/src/com/google/gwt/dev/jjs/ast/js/JMultiExpression.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/js/JMultiExpression.java
@@ -21,33 +21,93 @@ import com.google.gwt.dev.jjs.ast.JExpression;
 import com.google.gwt.dev.jjs.ast.JPrimitiveType;
 import com.google.gwt.dev.jjs.ast.JType;
 import com.google.gwt.dev.jjs.ast.JVisitor;
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Represents multiple ordered expressions as a single compound expression.
  */
 public class JMultiExpression extends JExpression {
 
-  public ArrayList<JExpression> exprs = new ArrayList<JExpression>();
+  private List<JExpression> expressions = Lists.newArrayList();
 
+  /**
+   * Construct an empty multi expression.
+   */
   public JMultiExpression(SourceInfo info) {
     super(info);
   }
 
+  /**
+   * Construct a multi expression containing {@code expressions}.
+   */
+  public JMultiExpression(SourceInfo info, List<JExpression> expressions) {
+    super(info);
+    this.expressions.addAll(expressions);
+  }
+
+  /**
+   * Adds {@code }expressions} to the multi expression at the end.
+   */
+  public void addExpressions(JExpression... expressions) {
+    this.expressions.addAll(Arrays.asList(expressions));
+  }
+
+  /**
+   * Adds a list of expressions to the multi expression at the end.
+   */
+  public void addExpressions(List<JExpression> expressions) {
+    this.expressions.addAll(expressions);
+  }
+
+  /**
+   * Adds a list of expressions to the multi expression at position {@code index}.
+   */
+  public void addExpressions(int index, List<JExpression> expressions) {
+    this.expressions.addAll(expressions);
+  }
+
+  /**
+   * Returns the expression at {@code index}.
+   */
+  public JExpression getExpression(int index) {
+    return expressions.get(index);
+  }
+
+  /**
+   * Returns the list of expressions.
+   */
+  public List<JExpression> getExpressions() {
+    return Collections.unmodifiableList(expressions);
+  }
+
+  /**
+   * Returns the number of expressions directly included in the multi expression.
+   */
+  public int getNumberOfExpressions() {
+    return expressions.size();
+  }
+
+  /**
+   * Returns the multi expression type, i.e. the type of the last expression in the list or
+   * {@code void} if empty.
+   */
   public JType getType() {
-    int c = exprs.size();
+    int c = expressions.size();
     if (c == 0) {
       return JPrimitiveType.VOID;
     } else {
-      return exprs.get(c - 1).getType();
+      return expressions.get(c - 1).getType();
     }
   }
 
   @Override
   public boolean hasSideEffects() {
-    for (int i = 0; i < exprs.size(); ++i) {
-      JExpression expr = exprs.get(i);
+    for (int i = 0; i < expressions.size(); ++i) {
+      JExpression expr = expressions.get(i);
       if (expr.hasSideEffects()) {
         return true;
       }
@@ -55,11 +115,33 @@ public class JMultiExpression extends JExpression {
     return false;
   }
 
+  /**
+   * Returns {@code true} if the multi expression is empty.
+   */
+  public boolean isEmpty() {
+    return expressions.isEmpty();
+  }
+
+  /**
+   * Removes the expression at {@code index} from this multi expression.
+   */
+  public JExpression removeExpression(int index) {
+    return expressions.remove(index);
+  }
+
+  /**
+   * Replaces the expression at {@code index} by {@code expression}.
+   */
+  public void setExpression(int index, JExpression expression) {
+    this.expressions.set(index, expression);
+  }
+
+
+  @Override
   public void traverse(JVisitor visitor, Context ctx) {
     if (visitor.visit(this, ctx)) {
-      visitor.acceptWithInsertRemove(exprs);
+      visitor.acceptWithInsertRemove(expressions);
     }
     visitor.endVisit(this, ctx);
   }
-
 }

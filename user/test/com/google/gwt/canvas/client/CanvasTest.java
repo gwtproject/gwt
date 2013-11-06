8082d2be66b278404b909a88df8b1ac0dc0d0994
Daniel Kurka <dankurka@google.com>
2013-11-06 13:47:33 +0100

    Remove IE6 binding for Canvas
    Change-Id: Ia76065b43486978314d2abd733895c67b4501d08

--- user/test/com/google/gwt/canvas/client/CanvasTest.java
+++ user/test/com/google/gwt/canvas/client/CanvasTest.java
@@ -36,8 +36,9 @@ public class CanvasTest extends GWTTestCase {
     return (geckoVersion != -1) && (geckoVersion >= 1009001);
   }-*/;
 
-  private static native boolean isIE6() /*-{
-    return @com.google.gwt.dom.client.DOMImplIE6::isIE6()();
+  private static native boolean isIE8() /*-{
+    var ua = $wnd.navigator.userAgent.toLowerCase();
+    return (ua.indexOf('msie') != -1 && ($doc.documentMode == 8));
   }-*/;
 
   private static native boolean isWebkit525OrBefore() /*-{
@@ -177,7 +178,8 @@ public class CanvasTest extends GWTTestCase {
       assertTrue(Canvas.isSupported());
       assertTrue(Canvas.isSupported());
     }
-    if (isIE6()) {
+
+    if (isIE8()) {
       assertFalse(Canvas.isSupported());
       assertFalse(Canvas.isSupported());
     }

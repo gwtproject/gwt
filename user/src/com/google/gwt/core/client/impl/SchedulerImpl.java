76196dd745c4f844c212c6b600f75ef0fd390e69
nordligulv <a.korzhevskiy@gmail.com>
2013-11-17 04:45:02 +0400

    Remove arguments.callee in SchedulerImpl
    Change-Id: I243018c7e0d0303b3e75aef5eef2118f3e105075

--- user/src/com/google/gwt/core/client/impl/SchedulerImpl.java
+++ user/src/com/google/gwt/core/client/impl/SchedulerImpl.java
@@ -188,7 +188,7 @@ public class SchedulerImpl extends Scheduler {
 
   private static native void scheduleFixedDelayImpl(RepeatingCommand cmd,
       int delayMs) /*-{
-    @com.google.gwt.core.client.impl.Impl::setTimeout(Lcom/google/gwt/core/client/JavaScriptObject;I)(function() {
+    var fn = function() {
       // $entry takes care of uncaught exception handling
       var ret = $entry(@com.google.gwt.core.client.impl.SchedulerImpl::execute(Lcom/google/gwt/core/client/Scheduler$RepeatingCommand;))(cmd);
       if (!@com.google.gwt.core.client.GWT::isScript()()) {
@@ -196,9 +196,10 @@ public class SchedulerImpl extends Scheduler {
         ret = ret == true;
       }
       if (ret) {
-        @com.google.gwt.core.client.impl.Impl::setTimeout(Lcom/google/gwt/core/client/JavaScriptObject;I)(arguments.callee, delayMs);
+        @com.google.gwt.core.client.impl.Impl::setTimeout(Lcom/google/gwt/core/client/JavaScriptObject;I)(fn, delayMs);
       }
-    }, delayMs);
+    };
+    @com.google.gwt.core.client.impl.Impl::setTimeout(Lcom/google/gwt/core/client/JavaScriptObject;I)(fn, delayMs);
   }-*/;
 
   private static native void scheduleFixedPeriodImpl(RepeatingCommand cmd,
@@ -212,10 +213,10 @@ public class SchedulerImpl extends Scheduler {
       }
       if (!ret) {
         // Either canceled or threw an exception
-        @com.google.gwt.core.client.impl.Impl::clearInterval(I)(arguments.callee.token);
+        @com.google.gwt.core.client.impl.Impl::clearInterval(I)(token);
       }
-    };
-    fn.token = @com.google.gwt.core.client.impl.Impl::setInterval(Lcom/google/gwt/core/client/JavaScriptObject;I)(fn, delayMs);
+    },
+    token = @com.google.gwt.core.client.impl.Impl::setInterval(Lcom/google/gwt/core/client/JavaScriptObject;I)(fn, delayMs);
   }-*/;
 
   /**

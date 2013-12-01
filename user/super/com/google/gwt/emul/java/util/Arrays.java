a88967112a9f69d478b6a69dba6869869d262e74
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2013-12-01 19:17:04 +0400

    Emulate Arrays.copy, Arrays.copyOfRange
    Issue: 8473
    Change-Id: Iba827a0a2603a347f32c6368d2dc04a6c1ddcb45

--- user/super/com/google/gwt/emul/java/util/Arrays.java
+++ user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -385,6 +385,166 @@ public class Arrays {
     return -low - 1;
   }
 
+  @SuppressWarnings("unchecked")
+  public static <T> T[] copyOf(T[] original, int newLength) {
+    T[] copy = Array.createFrom(original, newLength);
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static byte[] copyOf(byte[] original, int newLength) {
+    byte[] copy = new byte[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static short[] copyOf(short[] original, int newLength) {
+    short[] copy = new short[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static int[] copyOf(int[] original, int newLength) {
+    int[] copy = new int[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static long[] copyOf(long[] original, int newLength) {
+    long[] copy = new long[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static char[] copyOf(char[] original, int newLength) {
+    char[] copy = new char[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static float[] copyOf(float[] original, int newLength) {
+    float[] copy = new float[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static double[] copyOf(double[] original, int newLength) {
+    double[] copy = new double[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  public static boolean[] copyOf(boolean[] original, int newLength) {
+    boolean[] copy = new boolean[newLength];
+    System.arraycopy(original, 0, copy, 0,
+        Math.min(original.length, newLength));
+    return copy;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> T[] copyOfRange(T[] original, int from, int to) {
+    if (from < to) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    return Array.cloneSubrange(original, from, to);
+  }
+
+  public static byte[] copyOfRange(byte[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    byte[] copy = new byte[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static short[] copyOfRange(short[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    short[] copy = new short[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static int[] copyOfRange(int[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    int[] copy = new int[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static long[] copyOfRange(long[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    long[] copy = new long[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static char[] copyOfRange(char[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    char[] copy = new char[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static float[] copyOfRange(float[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    float[] copy = new float[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static double[] copyOfRange(double[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    double[] copy = new double[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
+  public static boolean[] copyOfRange(boolean[] original, int from, int to) {
+    int newLength = to - from;
+    if (newLength < 0) {
+      throw new IllegalArgumentException(from + " > " + to);
+    }
+    boolean[] copy = new boolean[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
+
   public static boolean deepEquals(Object[] a1, Object[] a2) {
     if (a1 == a2) {
       return true;

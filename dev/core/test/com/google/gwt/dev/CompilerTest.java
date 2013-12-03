3c5423bcfbfbd2b2af0d2fa7821ebe536ab86f3a
Roberto Lublinerman <rluble@google.com>
2013-12-03 10:31:04 -0800

    Added -SourceLevel auto to match runtime vm spec version.
    Also -SourceLevel auto is now the default, otherwise devmode
    would choke on class files it itself compiled.
    
    Change-Id: Ife59a3f5137f0cdd51a9acae99aea4d3a5eae837

--- dev/core/test/com/google/gwt/dev/CompilerTest.java
+++ dev/core/test/com/google/gwt/dev/CompilerTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,6 +18,7 @@ package com.google.gwt.dev;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.jjs.JsOutputOption;
 import com.google.gwt.dev.util.arg.SourceLevel;
+import com.google.gwt.util.tools.Utility;
 
 import java.io.File;
 
@@ -91,5 +92,18 @@ public class CompilerTest extends ArgProcessorTestBase {
     assertProcessFailure(argProcessor, "-out", "www");
     assertProcessFailure(argProcessor, "-sourceLevel", "ssss");
     assertProcessFailure(argProcessor, "-sourceLevel", "1.5");
+  }
+
+  /**
+   * Tests ordering for emum {@link SourceLevel}.
+   */
+  public void testSourceLevelOrdering() {
+    SourceLevel[] sourceLevels = SourceLevel.values();
+    SourceLevel previousSourceLevel = sourceLevels[0];
+    for (int i = 1; i < sourceLevels.length; i++) {
+      assertTrue(Utility.versionCompare(previousSourceLevel.getStringValue(),
+          sourceLevels[i].getStringValue()) < 0);
+      previousSourceLevel = sourceLevels[i];
+    }
   }
 }

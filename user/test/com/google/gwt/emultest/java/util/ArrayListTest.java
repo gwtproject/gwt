5df8382ac86170b51472bd1043cce6e370d72af6
Chris Povirk <cpovirk@google.com>
2014-01-15 15:41:38 -0500

    listIterator().add() increments index after List.add() succeeds.
    This prevents a failed add() from corrupting the state for future
    users of the ListIterator. See equivalent JDK bug
    bugs.sun.com/bugdatabase/view_bug.do?bug_id=6533203
    
    Change-Id: I3bac7ed0f0be75bdeab2df3b3e32d8f0de54ec89
    Review-Link: https://gwt-review.googlesource.com/#/c/6100/

--- user/test/com/google/gwt/emultest/java/util/ArrayListTest.java
+++ user/test/com/google/gwt/emultest/java/util/ArrayListTest.java
@@ -15,8 +15,10 @@
  */
 package com.google.gwt.emultest.java.util;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Tests ArrayList class (and by extension, AbstractList).
@@ -29,6 +31,26 @@ public class ArrayListTest extends ListTestBase {
     public void removeRange(int fromIndex, int toIndex) {
       super.removeRange(fromIndex, toIndex);
     }
+  }
+
+  public void testAbstractListUnmodifiableFailedIteratorAddIndexCorruption() {
+    ListIterator<String> i = new AbstractList<String>() {
+      @Override
+      public int size() {
+        return 0;
+      }
+
+      @Override
+      public String get(int index) {
+        throw new IndexOutOfBoundsException();
+      }
+    }.listIterator();
+    try {
+      i.add("bar");
+      fail();
+    } catch (UnsupportedOperationException e) {
+    }
+    assertFalse(i.hasPrevious());
   }
 
   public void testRemoveRange() {

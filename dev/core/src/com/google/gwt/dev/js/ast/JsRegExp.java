3983a49df62ca1cefd61032afd8742713f7fa29d
Roberto Lublinerman <rluble@google.com>
2014-01-17 12:54:37 -0800

    Unified all interning logic into JsLiteralInterner.
    Now any JsLiteral that is a value is interned by a JsLiteralInterner.
    For now only string, regex, numbers and some object literals (from cast maps)
    are interned.
    
    A future compiler pass might mark other literals that have value semantics as
    internable.
    
    Change-Id: I163cac2622ee72c395bf8ae37ed67862ab74a082

--- dev/core/src/com/google/gwt/dev/js/ast/JsRegExp.java
+++ dev/core/src/com/google/gwt/dev/js/ast/JsRegExp.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2008 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -28,6 +28,14 @@ public final class JsRegExp extends JsValueLiteral {
     super(sourceInfo);
   }
 
+  @Override
+  public boolean equals(Object that) {
+    if (that == null || that.getClass() != this.getClass()) {
+      return false;
+    }
+    return flags.equals(((JsRegExp) that).flags) && pattern.equals(((JsRegExp) that).pattern);
+  }
+
   public String getFlags() {
     return flags;
   }
@@ -39,6 +47,11 @@ public final class JsRegExp extends JsValueLiteral {
 
   public String getPattern() {
     return pattern;
+  }
+
+  @Override
+  public int hashCode() {
+    return flags.hashCode() + 17 * pattern.hashCode();
   }
 
   @Override
@@ -59,6 +72,11 @@ public final class JsRegExp extends JsValueLiteral {
   @Override
   public boolean isDefinitelyNull() {
     return false;
+  }
+
+  @Override
+  public boolean isInternable() {
+    return true;
   }
 
   public void setFlags(String suffix) {

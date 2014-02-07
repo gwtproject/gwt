329d24a3381f1ff946676f51eb1598005797aea0
Jin Yi <jinyi@google.com>
2014-02-07 12:04:22 -0800

    fix System.nativeArraycopy and (native) String.valueOf's by coding around call stack limitations of Function.prototype.apply implementations.
    some context:
    http://stackoverflow.com/questions/18308700/chrome-how-to-solve-maximum-call-stack-size-exceeded-errors-on-math-max-apply
    
    Change-Id: I8d7ab5bc98976b27e6ab1db66882d26046b17c2b
    Review-Link: https://gwt-review.googlesource.com/#/c/6310/

--- user/test/com/google/gwt/emultest/java/lang/C.java
+++ user/test/com/google/gwt/emultest/java/lang/C.java
@@ -37,4 +37,18 @@ public class C {
   public static final String LONG_STRING = "1234567890123456";
   public static final int INT_VALUE = 123456789;
   public static final long LONG_VALUE = 1234567890123456L;
+
+  public static final char[] LARGE_CHAR_ARRAY_VALUE;
+  public static final String LARGE_CHAR_ARRAY_STRING;
+  static {
+    int inputCopies = 100000;
+    int inputSize = CHAR_ARRAY_VALUE.length;
+    LARGE_CHAR_ARRAY_VALUE = new char[inputCopies * inputSize];
+    for (int i = 0; i < inputCopies; ++i) {
+      for (int j = 0; j < inputSize; ++j) {
+        LARGE_CHAR_ARRAY_VALUE[i * inputSize + j] = CHAR_ARRAY_VALUE[j];
+      }
+    }
+    LARGE_CHAR_ARRAY_STRING = new String(new char[inputCopies]).replace("\0", CHAR_ARRAY_STRING);
+  }
 }

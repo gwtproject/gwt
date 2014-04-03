c63be02dc526f5f2cf6155300729d650c900e11c
Roberto Lublinerman <rluble@google.com>
2014-04-03 04:09:08 -0700

    Improvements and deprecations to Impl.getNameOf.
    Impl.getNameOf() (not a public API) provides a way to obtain the
    actual name of the JavaScript function that implements a Java method
    given a JSNI reference. Also allows the same for fields and types.
    
    (1) Impl.getNameOf now only accepts a proper JSNI reference as
    its string parameter.
    
    (2) Deprecated access to types just by name.
    
    (3) Deprecated use of binary names. All names must be
        source names.
    
    (4) Add limited support for pinning methods; methods that are
        pinned will not be inlined, statified or pruned. All methods
        referred to by Impl.getNameOf() are pinned.
    
    TODO in follow up cl, add support for pinning fields.
    
    Change-Id: Ia3fb6471318e3b9cb757d243578a5903b271e62c

---
+++ user/test/com/google/gwt/core/client/impl/ImplTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.core.client.impl;
+
+import com.google.gwt.junit.DoNotRunWith;
+import com.google.gwt.junit.Platform;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Tests for {@link Impl}.
+ */
+public class ImplTest extends GWTTestCase {
+
+  @Override
+  public String getModuleName() {
+    return "com.google.gwt.core.Core";
+  }
+
+  // A function that is not referenced and can be prunned very early in the compilation pipeline.
+  public static String prunnableFunction() {
+    return "Prunnable";
+  }
+
+  // A very simple function that will certainly be inlined away in optimized compiles.
+  public static String inlinableFunction() {
+    return "Inlinable";
+  }
+
+  private static final class Foo {
+    // A very simple function that will certainly be made static but not inlined.
+    // because it refers to its parameter twice.
+    public final String statifiableFunction(String o) {
+      return "Statifiable " + o + o;
+    }
+  }
+
+  @DoNotRunWith(Platform.Devel)
+  public void testPinnedByImpl_getNameOf() {
+    Foo foo = new Foo();
+    assertNotNull(foo.statifiableFunction("a" + "b"));
+    assertNotNull(inlinableFunction());
+    assertNotNullNorEmpty(
+        Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest::prunnableFunction()"));
+    assertNotNullNorEmpty(
+        Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest::inlinableFunction()"));
+    assertNotNullNorEmpty(
+        Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest.Foo::statifiableFunction(*)"));
+
+    if (areNamesPreserved()) {
+      String prunnableString =
+          Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest::prunnableFunction()");
+      assertTrue("Expecting 'prunnableFunction' got '" + prunnableString + "'",
+          prunnableString.contains("prunnableFunction"));
+
+      String inlineableString =
+          Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest::inlinableFunction()");
+      assertTrue("Expecting 'inlinableFunction' got '" + inlineableString + "'",
+          inlineableString.contains("inlinableFunction"));
+
+
+      String statisfiableString =
+          Impl.getNameOf("@com.google.gwt.core.client.impl.ImplTest.Foo::statifiableFunction(*)");
+      assertTrue("Expecting 'statifiableFunction' got '" + statisfiableString + "'",
+          statisfiableString.contains("statifiableFunction"));
+    }
+  }
+
+  private static native boolean areNamesPreserved() /*-{
+    try {
+    } finally {
+    }
+    return @ImplTest::areNamesPreserved().name.indexOf("areNamesPreserved") != -1;
+  }-*/;
+
+  private void assertNotNullNorEmpty(String s) {
+    assertNotNull(s);
+    assertFalse(s.isEmpty());
+  }
+}

f46ae1d3cfde754d725f2fb56601f37b377f4f9a
Ray Cromwell <cromwellian@google.com>
2014-04-10 15:28:13 -0700

    Adds @SpecializeMethod internal compiler hint.
    Allows compiler to tighten a method call into a specially written method based
    on calling context.
    
    Includes AbstractHashMap implementation.
    
    Change-Id: Ie8f825bff12535c10542e029107a1aa8188ca69d
    Note: Never to be used by end user apps, GWT SDK authors only. May be removed in future.
    Note: Does not rescue 'target method', the method must be live by other means
    Note: Does not currently implement covariant return matching on return type yet.

---
+++ dev/core/src/com/google/gwt/dev/jjs/impl/MethodCallSpecializer.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.dev.jjs.impl;
+
+import com.google.gwt.dev.jjs.ast.Context;
+import com.google.gwt.dev.jjs.ast.JMethod;
+import com.google.gwt.dev.jjs.ast.JMethodCall;
+import com.google.gwt.dev.jjs.ast.JModVisitor;
+import com.google.gwt.dev.jjs.ast.JNonNullType;
+import com.google.gwt.dev.jjs.ast.JNullType;
+import com.google.gwt.dev.jjs.ast.JProgram;
+import com.google.gwt.dev.jjs.ast.JType;
+import com.google.gwt.dev.util.log.speedtracer.CompilerEventType;
+import com.google.gwt.dev.util.log.speedtracer.SpeedTracerLogger;
+import com.google.gwt.dev.util.log.speedtracer.SpeedTracerLogger.Event;
+
+import java.util.List;
+
+/**
+ * For each JMethodCall that contains a specialization, retarget the method
+ * to the specialized call if possible. Depends on call tightening,
+ * staticizing, and finalizing having been successful on the
+ * method of interest.
+ */
+public class MethodCallSpecializer {
+
+  private class MethodCallSpecializingVisitor extends JModVisitor {
+
+    @Override
+    public void endVisit(JMethodCall x, Context ctx) {
+      // Don't do anything if the method binding isn't statically known
+      if (x.isVolatile() || x.canBePolymorphic() || x.getTarget().isAbstract()
+          || !x.getTarget().isFinal() || !x.getTarget().isStatic()) {
+        return;
+      }
+
+      JMethod method = x.getTarget();
+      JMethod.Specialization specialization = method.getSpecialization();
+
+      if (specialization == null) {
+        return;
+      }
+
+      List<JType> params = specialization.getParams();
+      // if the params exist, see if the arity matches
+      if (params != null) {
+        if (params.size() == x.getArgs().size()) {
+          for (int i = 0; i < params.size(); i++) {
+            JType argType = x.getArgs().get(i).getType();
+            if (argType instanceof JNonNullType) {
+              argType = ((JNonNullType) argType).getUnderlyingType();
+            }
+            if (argType instanceof JNullType) {
+              return;
+            }
+            // see if the args passed to the function can be cast to the
+            // specialization pattern
+            JType qType = params.get(i);
+            if (qType instanceof JNonNullType) {
+              qType = ((JNonNullType) qType).getUnderlyingType();
+            }
+            if (!program.typeOracle.canTriviallyCast(argType, qType)) {
+              // params don't match
+              return;
+            }
+          }
+        }
+
+        // TODO (cromwellian): implement in method flow detection of expected
+        // return type, and check against expected match (e.g. void.class
+        // for ignored return value)
+
+        // now try to look up the target instance method (hopefully not
+        // pruned)
+        JMethod targetMethod = specialization.getTargetMethod();
+        if (targetMethod != null) {
+          JMethodCall call = new JMethodCall(x.getSourceInfo(),
+              x.getInstance(), targetMethod);
+          call.addArgs(x.getArgs());
+          ctx.replaceMe(call);
+        }
+      }
+    }
+  }
+
+  public static final String NAME = MethodCallSpecializer.class.getSimpleName();
+
+  public static OptimizerStats exec(JProgram program) {
+    Event optimizeEvent = SpeedTracerLogger.start(CompilerEventType.OPTIMIZE, "optimizer", NAME);
+    OptimizerStats stats = new MethodCallSpecializer(program).execImpl();
+    optimizeEvent.end("didChange", "" + stats.didChange());
+    return stats;
+  }
+
+  private final JProgram program;
+
+  private MethodCallSpecializer(JProgram program) {
+    this.program = program;
+  }
+
+  private OptimizerStats execImpl() {
+    MethodCallSpecializingVisitor tightener = new MethodCallSpecializingVisitor();
+    tightener.accept(program);
+    return new OptimizerStats(NAME).recordModified(tightener.getNumMods());
+  }
+}

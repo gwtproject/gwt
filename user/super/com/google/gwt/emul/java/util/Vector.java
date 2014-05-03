34d0d499af8048cc91fd147ffea93963ad669a0d
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2014-05-03 15:34:53 +0200

    Let Vector/ArrayList emulation throw same exceptions as in Java.
    Vector emulation uses ArrayList which throws IndexOutOfBoundsException
    for several methods. However Vector is defined to throw
    ArrayIndexOutOfBoundsException instead.
    
    Additional adjustments:
    - Vector/ArrayList should throw IllegalArgumentException if trying to
    create with a negative initial capacity
    - Vector.firstElement() and Vector.lastElement() should
    throw NoSuchElementException instead of IndexOutOfBoundsException.
    
    Bug: issue 8663
    Change-Id: Ia2d6162262d31c243f95a3c9ac2253e693c367bc

--- user/super/com/google/gwt/emul/java/util/Vector.java
+++ user/super/com/google/gwt/emul/java/util/Vector.java
@@ -64,6 +64,9 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public void add(int index, E o) {
+    if (index < 0 || index > size()) {
+      throw new ArrayIndexOutOfBoundsException(index);
+    }
     arrayList.add(index, o);
   }
 
@@ -74,6 +77,9 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public boolean addAll(int index, Collection<? extends E> c) {
+    if (index < 0 || index > size()) {
+      throw new ArrayIndexOutOfBoundsException(index);
+    }
     return arrayList.addAll(index, c);
   }
 
@@ -125,11 +131,15 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
   }
 
   public E firstElement() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
     return get(0);
   }
 
   @Override
   public E get(int index) {
+    checkArrayIndex(index, size());
     return arrayList.get(index);
   }
 
@@ -161,10 +171,9 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
 
   public E lastElement() {
     if (isEmpty()) {
-      throw new IndexOutOfBoundsException("last");
-    } else {
-      return get(size() - 1);
+      throw new NoSuchElementException();
     }
+    return get(size() - 1);
   }
 
   @Override
@@ -181,6 +190,7 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public E remove(int index) {
+    checkArrayIndex(index, size());
     return arrayList.remove(index);
   }
 
@@ -203,6 +213,7 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
 
   @Override
   public E set(int index, E elem) {
+    checkArrayIndex(index, size());
     return arrayList.set(index, elem);
   }
 
@@ -252,5 +263,11 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
   @Override
   protected void removeRange(int fromIndex, int endIndex) {
     arrayList.removeRange(fromIndex, endIndex);
+  }
+
+  private static void checkArrayIndex(int index, int size) {
+    if (index < 0 || index >= size) {
+      throw new ArrayIndexOutOfBoundsException(index);
+    }
   }
 }

5bacf1e5b29b3643e928c18c72f30a299d3a0203
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-05-24 04:00:05 +0400

    Add csp compatible linker.
    Change-Id: Ib642a0267ae274c80a0ec70c9ab8d9ce6c8210f9

---
+++ dev/core/src/com/google/gwt/core/ext/linker/impl/installLocationMainWindowCsp.js
@@ -0,0 +1,19 @@
+// The GWT code can be installed anywhere, although an iFrame is the best
+// approach if you want both variable isolation (useful in general, but
+// critical if you want more than one GWT module on your page) and runAsync
+// which will need to be able to install additional chunks of code into that
+// isolated environment later on.
+
+// The items that must be provided in any install location are:
+// $wnd - the location where the bootstrap module is defined. Should also
+//        be the location where the __gwtStatsEvent is defined
+
+function getInstallLocationDoc() {
+  return window.document;
+}
+
+// This function is left for compatibility
+// and may be used by custom linkers
+function getInstallLocation() {
+  return getInstallLocationDoc().body;
+}

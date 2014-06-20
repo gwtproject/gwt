9b4a1249f74b6626bece1f8e5c9448f70a8e44bd
Ray Cromwell <cromwellian@google.com>
2014-06-20 12:26:47 -0700

    Double and Boolean implemented as unboxed raw types.
    Change-Id: I3a11e91be6ceb0d72b727a43c28581ebef6d70e3

--- dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Util.java
+++ dev/core/super/com/google/gwt/dev/jjs/intrinsic/com/google/gwt/lang/Util.java
@@ -17,6 +17,10 @@ package com.google.gwt.lang;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+import javaemul.internal.EmulatedCharset;
 import javaemul.internal.annotations.DoNotInline;
 
 /**
@@ -47,4 +51,123 @@ final class Util {
   public static String makeEnumName(String enumName) {
     return enumName;
   }
+
+  // CHECKSTYLE_OFF: Utility Methods for unboxed String, Double, and Boolean.
+
+  static native Double $createDouble(double x) /*-{
+    return x;
+  }-*/;
+
+  static Double $createDouble(String s) {
+    return $createDouble(Double.parseDouble(s));
+  }
+
+  static native Boolean $createBoolean(boolean x) /*-{
+    return x;
+  }-*/;
+
+  static Boolean $createBoolean(String x) {
+    return $createBoolean(Boolean.parseBoolean(x));
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString() {
+    return "";
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes) {
+    return $createString(bytes, 0, bytes.length);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes, int ofs, int len) {
+    return $createString(bytes, ofs, len, EmulatedCharset.UTF_8);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes, int ofs, int len, String charsetName)
+      throws UnsupportedEncodingException {
+    return $createString(bytes, ofs, len, String.getCharset(charsetName));
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes, int ofs, int len, Charset charset) {
+    return String.valueOf(((EmulatedCharset) charset).decodeString(bytes, ofs, len));
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes, String charsetName)
+      throws UnsupportedEncodingException {
+    return $createString(bytes, 0, bytes.length, charsetName);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(byte[] bytes, Charset charset)
+      throws UnsupportedEncodingException {
+    return $createString(bytes, 0, bytes.length, charset.name());
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(char value[]) {
+    return String.valueOf(value);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(char value[], int offset, int count) {
+    return String.valueOf(value, offset, count);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(int[] codePoints, int offset, int count) {
+    char[] chars = new char[count * 2];
+    int charIdx = 0;
+    while (count-- > 0) {
+      charIdx += Character.toChars(codePoints[offset++], chars, charIdx);
+    }
+    return String.valueOf(chars, 0, charIdx);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(String other) {
+    return other;
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(StringBuffer sb) {
+    return String.valueOf(sb);
+  }
+
+  /**
+   * @skip
+   */
+  static String $createString(StringBuilder sb) {
+    return String.valueOf(sb);
+  }
+  // CHECKSTYLE_ON: end utility methods
+
 }

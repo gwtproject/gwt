c445c61ff2b43c60d3db935e61389c54f28d2201
Ray Cromwell <cromwellian@google.com>
2014-06-20 12:26:47 -0700

    Double and Boolean implemented as unboxed raw types.
    Change-Id: I3a11e91be6ceb0d72b727a43c28581ebef6d70e3

--- dev/core/src/com/google/gwt/dev/jjs/impl/TypeCategory.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/TypeCategory.java
@@ -25,7 +25,7 @@ import com.google.gwt.dev.jjs.ast.JType;
  *
  * These are used in Cast checking and array implementation.
  */
-public  enum TypeCategory {
+public enum TypeCategory {
   /* Make sure this list is kept in sync with the one in Array.java */
 
   TYPE_JAVA_OBJECT,
@@ -33,6 +33,8 @@ public  enum TypeCategory {
   TYPE_JSO,
   TYPE_JAVA_LANG_OBJECT,
   TYPE_JAVA_LANG_STRING,
+  TYPE_JAVA_LANG_DOUBLE,
+  TYPE_JAVA_LANG_BOOLEAN,
   TYPE_JS_PROTOTYPE,
   TYPE_JS_FUNCTION,
   TYPE_PRIMITIVE_LONG,
@@ -57,8 +59,8 @@ public  enum TypeCategory {
     type = type.getUnderlyingType();
     if (type == program.getTypeJavaLangObject()) {
       return TypeCategory.TYPE_JAVA_LANG_OBJECT;
-    } else if (type == program.getTypeJavaLangString()) {
-      return TypeCategory.TYPE_JAVA_LANG_STRING;
+    } else if (program.getRepresentedAsNativeTypesDispatchMap().containsKey(type)) {
+      return program.getRepresentedAsNativeTypesDispatchMap().get(type).getTypeCategory();
     } else if (program.typeOracle.isEffectivelyJavaScriptObject(type)) {
       return TypeCategory.TYPE_JSO;
     } else if (program.typeOracle.isDualJsoInterface(type)

89118ad208086351d5416e4ac100e16e550b5ad9
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-07-06 03:14:25 +0400

    Use HashMap in ConstantMap.
    This change makes use of recent optimizations done to HashMap.
    
    Change-Id: Ie2fedb495ad9ebbba793892a3979cbf43c00b658

---
+++ user/src/com/google/gwt/i18n/client/impl/ConstantMapCreator.java
@@ -0,0 +1,23 @@
+package com.google.gwt.i18n.client.impl;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class is used to create <code>Constants</code> maps
+ * and is to be used only by the GWT code.
+ */
+public final class ConstantMapCreator {
+  private ConstantMapCreator() {}
+
+  public static Map<String, String> create(String keys[], String values[]) {
+    HashMap<String, String> map = new HashMap<String, String>();
+    for (int i = 0; i < keys.length; ++i) {
+      assert keys[i] != null;
+      assert values[i] != null;
+      map.put(keys[i], values[i]);
+    }
+    return Collections.unmodifiableMap(map);
+  }
+}

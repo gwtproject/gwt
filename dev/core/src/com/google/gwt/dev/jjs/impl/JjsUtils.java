116f38b4d1919fb4b0596a2bc6c6f9ba336b08e1
Roberto Lublinerman <rluble@google.com>
2014-07-10 18:51:13 -0700

    Make longs be handled more generally and interned by the JLiteralInterner.
    Longs were treated specially and interned at translation. The only drawback
    of this patch is that now the name of the intern variable won't help
    to know the value of the long.
    
    Change-Id: I1a7f03f4d8bf03ffd6823a52c0e5f6faf360ab47

--- dev/core/src/com/google/gwt/dev/jjs/impl/JjsUtils.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/JjsUtils.java
@@ -29,16 +29,14 @@ import com.google.gwt.dev.jjs.ast.JStringLiteral;
 import com.google.gwt.dev.js.ast.JsBooleanLiteral;
 import com.google.gwt.dev.js.ast.JsExpression;
 import com.google.gwt.dev.js.ast.JsLiteral;
-import com.google.gwt.dev.js.ast.JsNameRef;
+import com.google.gwt.dev.js.ast.JsName;
 import com.google.gwt.dev.js.ast.JsNullLiteral;
 import com.google.gwt.dev.js.ast.JsNumberLiteral;
 import com.google.gwt.dev.js.ast.JsObjectLiteral;
-import com.google.gwt.dev.js.ast.JsPropertyInitializer;
 import com.google.gwt.dev.js.ast.JsStringLiteral;
 import com.google.gwt.lang.LongLib;
 import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -97,19 +95,13 @@ public class JjsUtils {
       @Override
       JsLiteral translate(JExpression literal) {
         SourceInfo sourceInfo = literal.getSourceInfo();
-        int[] intArray = LongLib.getAsIntArray(((JLongLiteral) literal).getValue());
-        JsObjectLiteral objectLit = new JsObjectLiteral(sourceInfo);
-        List<JsPropertyInitializer> inits = objectLit.getPropertyInitializers();
-        JsExpression label0 = new JsNameRef(sourceInfo, "l");
-        JsExpression label1 = new JsNameRef(sourceInfo, "m");
-        JsExpression label2 = new JsNameRef(sourceInfo, "h");
-        JsExpression value0 = new JsNumberLiteral(sourceInfo, intArray[0]);
-        JsExpression value1 = new JsNumberLiteral(sourceInfo, intArray[1]);
-        JsExpression value2 = new JsNumberLiteral(sourceInfo, intArray[2]);
-        inits.add(new JsPropertyInitializer(sourceInfo, label0, value0));
-        inits.add(new JsPropertyInitializer(sourceInfo, label1, value1));
-        inits.add(new JsPropertyInitializer(sourceInfo, label2, value2));
-        return objectLit;
+        int[] values = LongLib.getAsIntArray(((JLongLiteral) literal).getValue());
+        JsObjectLiteral objectLiteral = new JsObjectLiteral(sourceInfo);
+        addPropertyToObject(sourceInfo, lName, values[0], objectLiteral);
+        addPropertyToObject(sourceInfo, mName, values[1], objectLiteral);
+        addPropertyToObject(sourceInfo, hName, values[2], objectLiteral);
+        objectLiteral.setInternable();
+        return objectLiteral;
       }
     },
     STRING_LITERAL_TRANSLATOR() {
@@ -125,7 +117,28 @@ public class JjsUtils {
       }
     };
 
+    private static final JsName lName;
+    private static final JsName hName;
+    private static final JsName mName;
+
+    static {
+      // Create unscoped, unobfuscatable names to avoid renaming.
+      lName = new JsName(null, "l", "l");
+      lName.setObfuscatable(false);
+      hName = new JsName(null, "h", "h");
+      hName.setObfuscatable(false);
+      mName = new JsName(null, "m", "m");
+      mName.setObfuscatable(false);
+    }
+
     abstract JsLiteral translate(JExpression literal);
+  }
+
+  private static void addPropertyToObject(SourceInfo sourceInfo, JsName propertyName,
+      int propertyValue, JsObjectLiteral objectLiteral) {
+    JsExpression label = propertyName.makeRef(sourceInfo);
+    JsExpression value = new JsNumberLiteral(sourceInfo, propertyValue);
+    objectLiteral.addProperty(sourceInfo, label, value);
   }
 
   private JjsUtils() {

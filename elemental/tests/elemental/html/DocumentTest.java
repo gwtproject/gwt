f61c054f2ca4c85e6eaf0a94de4b99faa71c4442
Manuel Carrasco Moñino <manolo@vaadin.com>
2014-08-06 16:12:26 +0200

    Fix elemental tests in production mode
    Change-Id: I1f2737d03a6854990edae84d5edc92d55e44ef86

--- elemental/tests/elemental/html/DocumentTest.java
+++ elemental/tests/elemental/html/DocumentTest.java
@@ -16,6 +16,8 @@
 
 package elemental.html;
 
+import com.google.gwt.junit.DoNotRunWith;
+import com.google.gwt.junit.Platform;
 import com.google.gwt.junit.client.GWTTestCase;
 
 import static elemental.client.Browser.getWindow;
@@ -32,6 +34,9 @@ public class DocumentTest extends GWTTestCase {
   /**
    * Tests {@link Document#write}.
    */
+  // For some reason it fails in HtmlUnit production mode. It also fails
+  // in real browsers because of pop up blocker.
+  @DoNotRunWith(Platform.Prod)
   public void testWrite() {
     final Window window = getWindow().open();
     final Document document = window.getDocument();

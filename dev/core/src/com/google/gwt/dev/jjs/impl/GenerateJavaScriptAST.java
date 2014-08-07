93d274c636f60d30c11a408389dafa47f8d0a4e4
Roberto Lublinerman <rluble@google.com>
2014-08-07 14:53:36 -0700

    Make polymorphic dispatch work in v8.
    Classes that ONLY override methods from parent classes might not
    be optimized in v8, due to a problem with the way hidden classes are
    built.
    
    V8 requires prototypes of different classes to differ in the set of
    properties in order to perform some optimizations. This patch makes
    every prototype have a unique field for that purpose.
    
    This optimization was attempted before with a different implementation
    at commit 340d85f5b30010bd4b6e358457711b08f2be2d86; but was rolled
    back due to performance regression in IE.
    
    Change-Id: I813a9e983d2abceddfda7484136d845d7a7881dd

--- dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaScriptAST.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaScriptAST.java
@@ -2274,6 +2274,7 @@ public class GenerateJavaScriptAST {
     private void generateClassSetup(JClassType x, List<JsStatement> globalStmts) {
       generateClassDefinition(x, globalStmts);
       generateVTables(x, globalStmts);
+      maybeGenerateUniqueIdAssignment(x, globalStmts);
 
       if (x == program.getTypeJavaLangObject()) {
         // special: setup a "toString" alias for java.lang.Object.toString()
@@ -2590,6 +2591,22 @@ public class GenerateJavaScriptAST {
       JsExprStmt polyAssignment = createAssignment(lhs, rhs).makeStmt();
       globalStmts.add(polyAssignment);
       vtableInitForMethodMap.put(polyAssignment, method);
+    }
+
+    /**
+     * Creates a unique field for each class (for Chrome optimization purposes).
+     */
+    private void maybeGenerateUniqueIdAssignment(JClassType type, List<JsStatement> globalStmts) {
+      JsScope classScope = classScopes.get(type);
+      SourceInfo sourceInfo = type.getSourceInfo();
+      JsName uniqueName =
+          classScope.declareName("_$GWT$_" + getRuntimeTypeReference(type).toString());
+      uniqueName.setObfuscatable(false);
+      JsNameRef lhs = uniqueName.makeRef(sourceInfo);
+      lhs.setQualifier(globalTemp.makeRef(sourceInfo));
+      JsExprStmt uniqueIdAssignment =
+          createAssignment(lhs, new JsNumberLiteral(sourceInfo,1)).makeStmt();
+      globalStmts.add(uniqueIdAssignment);
     }
 
     /**

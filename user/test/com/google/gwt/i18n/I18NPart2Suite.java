602527b67cd5ad124807520060c883ddd77f116c
Manuel Carrasco Moñino <manolo@vaadin.com>
2014-08-13 10:53:04 +0200

    Disable failing I18N_es_AR_RuntimeTest in I18N suite.
    - This test is failing with cldr-21 data, but passes with
      cldr-25 (I5d02c0587ed16dfedcfa64ad00a033808110d84b). Disabling
      it until new change is merged.
    
    Change-Id: Ida4306684008dbb28b8c9e5712376eed525e76a7

--- user/test/com/google/gwt/i18n/I18NPart2Suite.java
+++ user/test/com/google/gwt/i18n/I18NPart2Suite.java
@@ -58,7 +58,8 @@ public class I18NPart2Suite {
     suite.addTestSuite(I18N_iw_Test.class);
     suite.addTestSuite(I18N_es_MX_Test.class);
     suite.addTestSuite(I18N_es_MX_RuntimeTest.class);
-    suite.addTestSuite(I18N_es_AR_RuntimeTest.class);
+    // TODO(manolo): enable this test after cldr-25 update
+    // suite.addTestSuite(I18N_es_AR_RuntimeTest.class);
     suite.addTestSuite(I18N_en_US_Test.class);
     suite.addTestSuite(I18N_nb_Test.class);
     suite.addTestSuite(LocaleInfo_ar_Test.class);

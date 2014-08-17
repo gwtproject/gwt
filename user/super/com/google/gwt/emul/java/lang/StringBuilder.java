65a434d37b68b6b89238829b7ae763c011c8105b
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-08-17 05:44:05 +0400

    Make String API more JDK compliant.
    Change-Id: Ic5cf0cdf2366d7bee7e8f410dba0ab940e50c6b1

--- user/super/com/google/gwt/emul/java/lang/StringBuilder.java
+++ user/super/com/google/gwt/emul/java/lang/StringBuilder.java
@@ -73,7 +73,7 @@ public final class StringBuilder extends AbstractStringBuilder implements CharSe
 
   @Override
   public StringBuilder append(CharSequence x, int start, int end) {
-    append0(x, start, end);
+    string += String.__valueOf(x, start, end);
     return this;
   }
 
@@ -118,13 +118,12 @@ public final class StringBuilder extends AbstractStringBuilder implements CharSe
   }
 
   public StringBuilder delete(int start, int end) {
-    replace0(start, end, "");
-    return this;
+    return replace(start, end, "");
   }
 
   public StringBuilder deleteCharAt(int start) {
-    replace0(start, start + 1, "");
-    return this;
+    String.__checkIndex(start, length());
+    return replace(start, start + 1, "");
   }
 
   public StringBuilder insert(int index, boolean x) {
@@ -144,11 +143,11 @@ public final class StringBuilder extends AbstractStringBuilder implements CharSe
   }
 
   public StringBuilder insert(int index, CharSequence chars) {
-    return insert(index, chars.toString());
+    return insert(index, String.valueOf(chars));
   }
 
   public StringBuilder insert(int index, CharSequence chars, int start, int end) {
-    return insert(index, chars.subSequence(start, end).toString());
+    return insert(index, String.__valueOf(chars, start, end));
   }
 
   public StringBuilder insert(int index, double x) {
@@ -172,8 +171,7 @@ public final class StringBuilder extends AbstractStringBuilder implements CharSe
   }
 
   public StringBuilder insert(int index, String x) {
-    replace0(index, index, x);
-    return this;
+    return replace(index, index, x);
   }
 
   public StringBuilder replace(int start, int end, String toInsert) {

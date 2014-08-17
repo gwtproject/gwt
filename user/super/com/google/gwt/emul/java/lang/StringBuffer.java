b54a1ef5eed9e59dcc6e7c9df15743c00903d53e
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-08-17 05:44:05 +0400

    Make String API more JDK compliant.
    Change-Id: Ic5cf0cdf2366d7bee7e8f410dba0ab940e50c6b1

--- user/super/com/google/gwt/emul/java/lang/StringBuffer.java
+++ user/super/com/google/gwt/emul/java/lang/StringBuffer.java
@@ -118,13 +118,12 @@ public final class StringBuffer extends AbstractStringBuilder implements CharSeq
   }
 
   public StringBuffer delete(int start, int end) {
-    replace0(start, end, "");
-    return this;
+    return replace(start, end, "");
   }
 
   public StringBuffer deleteCharAt(int start) {
-    replace0(start, start + 1, "");
-    return this;
+    String.__checkIndex(start, length());
+    return delete(start, start + 1);
   }
 
   public StringBuffer insert(int index, boolean x) {
@@ -144,11 +143,11 @@ public final class StringBuffer extends AbstractStringBuilder implements CharSeq
   }
 
   public StringBuffer insert(int index, CharSequence chars) {
-    return insert(index, chars.toString());
+    return insert(index, String.valueOf(chars));
   }
 
   public StringBuffer insert(int index, CharSequence chars, int start, int end) {
-    return insert(index, chars.subSequence(start, end).toString());
+    return insert(index, String.valueOf(chars).substring(start, end));
   }
 
   public StringBuffer insert(int index, double x) {

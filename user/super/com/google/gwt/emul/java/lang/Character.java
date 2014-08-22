a3df7b67e8e55acc6e259a55bec141f153975870
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-08-23 03:09:54 +0400

    Refactor java.lang.Number.
    Change-Id: Ie966de7c36a976c402e6c22f1879e2e76eec3f34

--- user/super/com/google/gwt/emul/java/lang/Character.java
+++ user/super/com/google/gwt/emul/java/lang/Character.java
@@ -208,12 +208,7 @@ public final class Character implements Comparable<Character>, Serializable {
       return 0;
     }
 
-    final int baseTenMax = 10;
-    if (digit < baseTenMax) {
-      return (char) ('0' + digit);
-    } else {
-      return (char) ('a' + digit - baseTenMax);
-    }
+    return forDigit(digit);
   }
 
   /**
@@ -413,6 +408,20 @@ public final class Character implements Comparable<Character>, Serializable {
       return toCodePoint(highSurrogate, loSurrogate);
     }
     return loSurrogate;
+  }
+
+  /**
+   * Shared implementation with Long.toString
+   *
+   * @skip
+   */
+  static char forDigit(int digit) {
+    final int baseTenMax = 10;
+    if (digit < baseTenMax) {
+      return (char) ('0' + digit);
+    } else {
+      return (char) ('a' + digit - baseTenMax);
+    }
   }
 
   /**

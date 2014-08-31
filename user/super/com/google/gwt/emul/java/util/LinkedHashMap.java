51d2ca8b5794dd6ae92e7c648a0e1b7cd2c22255
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-08-31 20:53:39 +0400

    Refactor Map, Set implementations.
    - Implement LinkedHashMap.EntrySet.remove method
    - Implement clear and remove methods for
    AbstractMap's keySet() and values()
    - Remove initialization block in LinkedHashMap
    - Ignore capacity and loadFactor parameters in
      LinkedHashSet
    - Do not clear AbstractHashMap if it's empty
    
    Change-Id: Ie590954cb37356269489be385c14379313934df7

--- user/super/com/google/gwt/emul/java/util/LinkedHashMap.java
+++ user/super/com/google/gwt/emul/java/util/LinkedHashMap.java
@@ -147,8 +147,18 @@ public class LinkedHashMap<K, V> extends HashMap<K, V> implements Map<K, V> {
     }
 
     @Override
+    public boolean remove(Object entry) {
+      if (contains(entry)) {
+        Object key = ((Map.Entry<?, ?>) entry).getKey();
+        LinkedHashMap.this.remove(key);
+        return true;
+      }
+      return false;
+    }
+
+    @Override
     public int size() {
-      return map.size();
+      return LinkedHashMap.this.size();
     }
   }
 
@@ -172,35 +182,37 @@ public class LinkedHashMap<K, V> extends HashMap<K, V> implements Map<K, V> {
    */
   private final transient HashMap<K, ChainEntry> map = new HashMap<K, ChainEntry>();
 
-  {
-    // Initialize the empty linked list.
-    head.prev = head;
-    head.next = head;
-  }
-
   public LinkedHashMap() {
+    resetChainEntries();
   }
 
   public LinkedHashMap(int ignored) {
-    super(ignored);
+    this(ignored, 0);
   }
 
   public LinkedHashMap(int ignored, float alsoIgnored) {
     super(ignored, alsoIgnored);
+    resetChainEntries();
   }
 
   public LinkedHashMap(int ignored, float alsoIgnored, boolean accessOrder) {
     super(ignored, alsoIgnored);
     this.accessOrder = accessOrder;
+    resetChainEntries();
   }
 
   public LinkedHashMap(Map<? extends K, ? extends V> toBeCopied) {
+    resetChainEntries();
     this.putAll(toBeCopied);
   }
 
   @Override
   public void clear() {
     map.clear();
+    resetChainEntries();
+  }
+
+  private void resetChainEntries() {
     head.prev = head;
     head.next = head;
   }

48205482a8f3f35af96fedd3c628595ae73f17fe
Brian Slesinsky <skybrian@google.com>
2014-09-12 22:49:09 -0700

    Super Dev Mode: support standalone apps without precompile
    Standalone, static apps hosted entirely on the Super Dev Mode code server
    didn't work with -noprecompile because we didn't copy the public resources
    to the stub CompileDir and because binding properties weren't computed.
    
    It now copies the files and generates binding properties.
    
    Change-Id: Ib01b3783aef18e0041370067bd056f5c50339d61

--- dev/codeserver/java/com/google/gwt/dev/codeserver/nomodule.nocache.js
+++ dev/codeserver/java/com/google/gwt/dev/codeserver/nomodule.nocache.js
@@ -18,30 +18,66 @@
  * @fileoverview Stub for non-compiled modules.
  *
  * This script forces the proper reload + compilation in Super Dev Mode.
+ * Assumes "providers" and "values are already defined.
+ * This script will be wrapped in a function.
  */
+var scriptLocation;
+var getPropMap;
 
-(function() {
-  var moduleName = '__MODULE_NAME__';  // Replaced by actual module name.
+var key = '__gwtDevModeHook:' + moduleName;
+if (window.sessionStorage[key]) {
 
-  // Active Super Dev Mode is assumed.
-  var key = '__gwtDevModeHook:' + moduleName;
-  if (!window.sessionStorage[key]) {
-    alert('Unable to load Super Dev Mode version of ' + moduleName + '.');
-    return;
+  // already on
+  scriptLocation = window.sessionStorage[key];
+  getPropMap = null; // dev_mode_on.js will get it from __gwt_activeModules.
+
+} else {
+  // Someone included nocache.js directly from the codeserver.
+
+  // Find out own location. This script will be replaced after the compile is finished.
+  $wnd.__gwt_activeModules = $wnd.__gwt_activeModules || {};
+  $wnd.__gwt_activeModules[moduleName] = $wnd.__gwt_activeModules[moduleName] || {};
+  var scriptBase = computeScriptBase();
+  if (!scriptBase) {
+    return; // not found; already alerted.
   }
-  var scriptLocation = window.sessionStorage[key];
+  scriptLocation = scriptBase + "/" + moduleName + ".nocache.js";
 
-  // Get the Super Dev Mode Server URL: use the HTML a.href parsing.
-  var a = document.createElement('a');
-  a.href = scriptLocation;
-  var devServerUrl = a.protocol + '//' + a.host;
+  // There is no original app so we need to calculate the binding properties.
+  function computePropValue(propName){
+    var val = providers[propName]();
+    // sanity check
+    var allowedValuesMap = values[propName];
+    if (val in allowedValuesMap) {
+      return val;
+    } else {
+      console.log("provider for " + propName + " returned unexpected value: '" + val + "'");
+      throw "can't compute binding property value for " + propName;
+    }
+  }
 
-  // Load the bookmarklet.
-  window.__gwt_bookmarklet_params = {
-    'server_url' : devServerUrl + '/',
-    'module_name': moduleName
+  getPropMap = function () {
+    var result = {};
+    for (var key in values) {
+      if (values.hasOwnProperty(key)) {
+        result[key] = computePropValue(key);
+      }
+    }
+    return result;
   };
-  var script = document.createElement('script');
-  script.src = devServerUrl + '/dev_mode_on.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
-})();
+}
+
+// Get the Super Dev Mode Server URL: use the HTML a.href parsing.
+var a = document.createElement('a');
+a.href = scriptLocation;
+var devServerUrl = a.protocol + '//' + a.host;
+
+// Load the bookmarklet.
+window.__gwt_bookmarklet_params = {
+  'server_url' : devServerUrl + '/',
+  'module_name': moduleName,
+  'getPropMap': getPropMap
+};
+var script = document.createElement('script');
+script.src = devServerUrl + '/dev_mode_on.js';
+document.getElementsByTagName('head')[0].appendChild(script);

f7d4d3a2f51a8b04217ae47e22235b2ef42f4bcf
Roberto Lublinerman <rluble@google.com>
2014-10-22 17:17:19 -0700

    Adds ArgHandlerEnum, an argument handler backed by an enum.
    Adds an abstract base class that makes it easy to define a
    handlers for otion type where the option values are enum values.
    
    Refactors the code in arg handlres for JsInterop and
    MethodNameDisplayMode.
    
    Change-Id: I0d2465f6947824070bbb1cd582847c4b3b9f3b98

---
+++ dev/core/src/com/google/gwt/util/tools/ArgHandlerEnum.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.util.tools;
+
+import com.google.gwt.thirdparty.guava.common.base.Joiner;
+
+/**
+ * A generic arg handler for options defined by enums.
+ *
+ * @param <T> enum type providing option values.
+ */
+public abstract  class ArgHandlerEnum<T extends Enum<T>> extends ArgHandlerString {
+  private final Class<T> optionsEnum;
+  private final int defaultEnumIndex;
+
+  public ArgHandlerEnum(Class<T> optionsEnum) {
+    this(optionsEnum, 0);
+  }
+
+    public ArgHandlerEnum(Class<T> optionsEnum, int defaultEnumIndex) {
+    this.optionsEnum = optionsEnum;
+    this.defaultEnumIndex = defaultEnumIndex;
+  }
+
+  @Override
+  public String[] getTagArgs() {
+    return new String[]{"[" + Joiner.on(", ").skipNulls().join(
+        optionsEnum.getEnumConstants()) + "]"};
+  }
+
+  @Override
+  public final boolean setString(String value) {
+    T mode = null;
+    try {
+      mode = Enum.valueOf(optionsEnum, value.trim().toUpperCase());
+    } catch (IllegalArgumentException e) {
+      System.err.println(value + " is not a valid option for " + getTag());
+      System.err.println(getTag() + " value must be one of [" +
+          Joiner.on(", ").skipNulls().join(optionsEnum.getEnumConstants()) + "].");
+      return false;
+    }
+
+    setValue(mode);
+    return true;
+  }
+
+  /**
+   * Override to handle the setting of an enum value.
+   */
+  public abstract void setValue(T value);
+}

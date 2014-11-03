6207550280ea7e2972b79607bf91e72466d1621e
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-11-03 23:12:13 +0300

    Add missing null-checks to PriorityQueue.
    Change-Id: I88f008a597aed0065d925f0c3cadd483baccb318

--- user/src/com/google/gwt/core/shared/impl/InternalPreconditions.java
+++ user/src/com/google/gwt/core/shared/impl/InternalPreconditions.java
@@ -164,10 +164,7 @@ public class InternalPreconditions {
    * Ensures that an object reference passed as a parameter to the calling method is not null.
    */
   public static <T> T checkNotNull(T reference) {
-    if (reference == null) {
-      throw new NullPointerException();
-    }
-    return reference;
+    return checkCriticalNotNull(reference);
   }
 
   /**
@@ -177,6 +174,16 @@ public class InternalPreconditions {
     if (reference == null) {
       throw new NullPointerException(String.valueOf(errorMessage));
     }
+  }
+
+  /**
+   * Ensures that an object reference passed as a parameter to the calling method is not null.
+   */
+  public static <T> T checkCriticalNotNull(T reference) {
+    if (reference == null) {
+      throw new NullPointerException();
+    }
+    return reference;
   }
 
   /**

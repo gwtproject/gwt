9e7ad4f47cec7c14915b5a0ee1733e7148186672
John Stalcup <stalcup@google.com>
2014-11-04 16:14:26 -0800

    Adds recursive file change listen support.
    Accumulated file changes will later be useful to shortcut
    ResourceOracle rescans.
    
    Change-Id: I10ee002c5752084a715e9c116aff8d46aaf33945

---
+++ dev/core/test/com/google/gwt/dev/resource/impl/ChangedFileAccumulatorTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.dev.resource.impl;
+
+import com.google.gwt.thirdparty.guava.common.io.Files;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Tests for ChangedFileAccumulator.
+ */
+public class ChangedFileAccumulatorTest extends TestCase {
+
+  public void testAddFile() throws IOException, InterruptedException {
+    File rootDirectory = Files.createTempDir();
+    File subDirectory = createDirectoryIn("subdir", rootDirectory);
+
+    ChangedFileAccumulator changedFileAccumulator =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+
+    assertTrue(changedFileAccumulator.getAndClearModifiedFiles().isEmpty());
+
+    createFileIn("New.java", subDirectory);
+    waitForFileEvents();
+
+    List<File> modifiedFiles = changedFileAccumulator.getAndClearModifiedFiles();
+    assertEquals(1, modifiedFiles.size());
+    assertTrue(modifiedFiles.get(0).getPath().endsWith("New.java"));
+
+    changedFileAccumulator.shutdown();
+  }
+
+  public void testDeleteFile() throws IOException, InterruptedException {
+    File rootDirectory = Files.createTempDir();
+    File subDirectory = createDirectoryIn("subdir", rootDirectory);
+    File originalFile = createFileIn("SomeFile.java", subDirectory);
+
+    ChangedFileAccumulator changedFileAccumulator =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+
+    assertTrue(changedFileAccumulator.getAndClearModifiedFiles().isEmpty());
+
+    originalFile.delete();
+    waitForFileEvents();
+
+    List<File> modifiedFiles = changedFileAccumulator.getAndClearModifiedFiles();
+    assertEquals(1, modifiedFiles.size());
+    assertTrue(modifiedFiles.get(0).getPath().endsWith("SomeFile.java"));
+
+    changedFileAccumulator.shutdown();
+  }
+
+  public void testListensInNewDirectories() throws IOException, InterruptedException {
+    File rootDirectory = Files.createTempDir();
+
+    ChangedFileAccumulator changedFileAccumulator =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+
+    assertTrue(changedFileAccumulator.getAndClearModifiedFiles().isEmpty());
+
+    // Create a new directory and contained file AFTER the root directory has started being listened
+    // to.
+    File subDirectory = createDirectoryIn("subdir", rootDirectory);
+    createFileIn("New.java", subDirectory);
+    waitForFileEvents();
+
+    List<File> modifiedFiles = changedFileAccumulator.getAndClearModifiedFiles();
+    assertEquals(2, modifiedFiles.size());
+    assertTrue(modifiedFiles.get(0).getPath().endsWith("subdir"));
+    assertTrue(modifiedFiles.get(1).getPath().endsWith("New.java"));
+
+    changedFileAccumulator.shutdown();
+  }
+
+  public void testMultipleListeners() throws IOException, InterruptedException {
+    File rootDirectory = Files.createTempDir();
+    File subDirectory = createDirectoryIn("subdir", rootDirectory);
+
+    ChangedFileAccumulator changedFileAccumulator1 =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+    ChangedFileAccumulator changedFileAccumulator2 =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+
+    assertTrue(changedFileAccumulator1.getAndClearModifiedFiles().isEmpty());
+    assertTrue(changedFileAccumulator2.getAndClearModifiedFiles().isEmpty());
+
+    createFileIn("New.java", subDirectory);
+    waitForFileEvents();
+
+    List<File> modifiedFiles1 = changedFileAccumulator1.getAndClearModifiedFiles();
+    assertEquals(1, modifiedFiles1.size());
+    assertTrue(modifiedFiles1.get(0).getPath().endsWith("New.java"));
+
+    List<File> modifiedFiles2 = changedFileAccumulator2.getAndClearModifiedFiles();
+    assertEquals(1, modifiedFiles2.size());
+    assertTrue(modifiedFiles2.get(0).getPath().endsWith("New.java"));
+
+    changedFileAccumulator1.shutdown();
+    changedFileAccumulator2.shutdown();
+  }
+
+  public void testRenameFile() throws IOException, InterruptedException {
+    File rootDirectory = Files.createTempDir();
+    File subDirectory = createDirectoryIn("subdir", rootDirectory);
+    File originalFile = createFileIn("OriginalName.java", subDirectory);
+    File renamedFile = new File(subDirectory, "Renamed.java");
+
+    ChangedFileAccumulator changedFileAccumulator =
+        new ChangedFileAccumulator(rootDirectory.toPath());
+
+    assertTrue(changedFileAccumulator.getAndClearModifiedFiles().isEmpty());
+
+    originalFile.renameTo(renamedFile);
+    waitForFileEvents();
+
+    List<File> modifiedFiles = changedFileAccumulator.getAndClearModifiedFiles();
+    assertEquals(2, modifiedFiles.size());
+    assertTrue(modifiedFiles.get(0).getPath().endsWith("OriginalName.java"));
+    assertTrue(modifiedFiles.get(1).getPath().endsWith("Renamed.java"));
+
+    changedFileAccumulator.shutdown();
+  }
+
+  private File createDirectoryIn(String fileName, File inDirectory) {
+    File newDirectory = new File(inDirectory, fileName);
+    newDirectory.mkdir();
+    return newDirectory;
+  }
+
+  private File createFileIn(String fileName, File inDirectory) throws IOException {
+    File newFile = new File(inDirectory, fileName);
+    newFile.createNewFile();
+    return newFile;
+  }
+
+  private void waitForFileEvents() throws InterruptedException {
+    Thread.sleep(10);
+  }
+}

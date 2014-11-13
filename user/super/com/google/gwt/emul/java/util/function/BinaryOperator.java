f5d2d014a87faa4d0cda826ebc6e748bd1456863
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2014-11-14 00:44:25 +0100

    Emulation of java.util.function
    Change-Id: I8f2cfde31da7ca2fd6dfa1807b2024b53934210f

---
+++ user/super/com/google/gwt/emul/java/util/function/BinaryOperator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package java.util.function;
+
+import com.google.gwt.core.shared.impl.InternalPreconditions;
+
+import java.util.Comparator;
+
+/**
+ * See <a href="https://docs.oracle.com/javase/8/docs/api/java/util/function/BinaryOperator.html">
+ * the official Java API doc</a> for details.
+ *
+ * @param <T> type of both operands and the result
+ */
+@FunctionalInterface
+public interface BinaryOperator<T> extends BiFunction<T, T, T> {
+
+  static <T> BinaryOperator<T> maxBy(Comparator<? super T> comparator) {
+    InternalPreconditions.checkNotNull(comparator);
+    return (t, u) -> comparator.compare(t, u) <= 0 ? u : t;
+  }
+
+  static <T> BinaryOperator<T> minBy(Comparator<? super T> comparator) {
+    InternalPreconditions.checkNotNull(comparator);
+    return (t, u) -> comparator.compare(t, u) <= 0 ? t : u;
+  }
+
+}

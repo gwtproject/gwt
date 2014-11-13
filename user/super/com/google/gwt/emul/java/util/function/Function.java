7bcd576e8a1ab9ad2852a61c3567e1cfcf183584
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2014-11-14 00:44:25 +0100

    Emulation of java.util.function
    Change-Id: I8f2cfde31da7ca2fd6dfa1807b2024b53934210f

---
+++ user/super/com/google/gwt/emul/java/util/function/Function.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package java.util.function;
+
+import com.google.gwt.core.shared.impl.InternalPreconditions;
+
+/**
+ * See <a href="https://docs.oracle.com/javase/8/docs/api/java/util/function/Function.html">
+ * the official Java API doc</a> for details.
+ *
+ * @param <T> type of the argument
+ * @param <R> type of the return value
+ */
+@FunctionalInterface
+public interface Function<T, R> {
+
+  static <T> Function<T, T> identity() {
+    return t -> t;
+  }
+
+  R apply(T t);
+
+  default <V> Function<T, V> andThen(Function<? super R, ? extends V> after) {
+    InternalPreconditions.checkNotNull(after);
+    return t -> after.apply(apply(t));
+  }
+
+  default <V> Function<V, R> compose(Function<? super V, ? extends T> before) {
+    InternalPreconditions.checkNotNull(before);
+    return t -> apply(before.apply(t));
+  }
+
+}

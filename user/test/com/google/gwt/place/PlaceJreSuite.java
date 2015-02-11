db88503b2e6de339fd5da289dbd6eae8f46c469e
Goktug Gokdogan <goktug@google.com>
2015-02-10 18:33:24 -0800

    Adds missing suite and corrects naming for JRE only suites.
    Naming JRE only suites correctly ensures that they are only
    executed once instead of per browser.
    
    Change-Id: I7da49d168b0076a3910a5c690bc469ef93450519

--- user/test/com/google/gwt/place/PlaceJreSuite.java
+++ user/test/com/google/gwt/place/PlaceJreSuite.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010 Google Inc.
- *
+ * Copyright 2015 Google Inc.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,8 +15,10 @@
  */
 package com.google.gwt.place;
 
-import com.google.gwt.junit.tools.GWTTestSuite;
-import com.google.gwt.place.impl.PlaceHistoryMapperGeneratorTest;
+import com.google.gwt.place.rebind.PlaceHistoryGeneratorContextTest;
+import com.google.gwt.place.shared.PlaceChangeRequestEventTest;
+import com.google.gwt.place.shared.PlaceControllerTest;
+import com.google.gwt.place.shared.PlaceHistoryHandlerTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -24,11 +26,14 @@ import junit.framework.TestSuite;
 /**
  * Tests of the place package that doesn't require browser.
  */
-public class PlaceSuite {
+public class PlaceJreSuite {
   public static Test suite() {
-    TestSuite suite = new GWTTestSuite("JRE Tests of the place package");
+    TestSuite suite = new TestSuite("Tests of the place package");
 
-    suite.addTestSuite(PlaceHistoryMapperGeneratorTest.class);
+    suite.addTestSuite(PlaceControllerTest.class);
+    suite.addTestSuite(PlaceChangeRequestEventTest.class);
+    suite.addTestSuite(PlaceHistoryGeneratorContextTest.class);
+    suite.addTestSuite(PlaceHistoryHandlerTest.class);
 
     return suite;
   }

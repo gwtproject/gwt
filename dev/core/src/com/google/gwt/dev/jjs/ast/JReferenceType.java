111cdfdcf172e62b61ef016a7bd1fd8b47da301e
Goktug Gokdogan <goktug@google.com>
2015-03-11 20:21:38 -0700

    Enhances TypeTightener with ability to lock into exact types.
    Being able to lock to an exact type helps us to mark even more
    methods as non-polymorphic which provides new optimization
    opportunities.
    
    Earlier, for example, if there was any reference to LinkedHashMap
    in the app, we were not able to apply method specializations in
    HashMap because all specialized method was potentially polymorphic.
    With the new optimization if the variable is only assigned to
    HashMap instance, now we potentially identify that and specialize
    the methods. Same is true for any optimizations that exploit the
    fact that a method doesn't require polymorphic dispatch
    (like inlining).
    
    The implementation introduces refactors JNonNullType into
    JAnalysisDecoratedType whichn now can represent a non null subtype
    (or itself, tagged with NOT_NULL) or a specific type (no subclass)
    that is also not null (tagged with EXACT).
    
    For example, if a JAnalysisType refers to HashMap and is tagged with
    EXACT, the type cannot be any subtype but the HashMap itself.
    The only places where the an EXACT JAnalysisType is a result of
    newing a type (when a type is instantiated, tts precise type is known)
    and then it is propagated mostly via TypeTightener.
    
    Change-Id: Iad95138d336bbda66bbdbcc6d007b1b722d1c604
    Review-Link: https://gwt-review.googlesource.com/#/c/10932/

--- dev/core/src/com/google/gwt/dev/jjs/ast/JReferenceType.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/JReferenceType.java
@@ -22,20 +22,140 @@ import com.google.gwt.dev.jjs.SourceInfo;
  */
 public abstract class JReferenceType extends JType implements CanBeAbstract {
 
-  private transient JNonNullType nonNullType;
+  private transient AnalysisDecoratedTypeSingletons analysisDecoratedTypeSingletons = null;
+  /**
+   * A reference type decorated with the result of static analysis. Only two analysis properties
+   * are computed (mostly during type propagation in TypeTightener: nullness and exactness.
+   */
+  private static class JAnalysisDecoratedType extends JReferenceType {
+
+
+    private final boolean canBeSubclass;
+    private final boolean canBeNull;
+    private final JReferenceType ref;
+
+    private JAnalysisDecoratedType(JReferenceType ref, boolean canBeSubclass, boolean canBeNull) {
+      super(ref.getSourceInfo(), ref.getName());
+      assert ref.getUnderlyingType().canBeSubclass() != canBeSubclass ||
+          ref.getUnderlyingType().canBeNull() != canBeNull : "An analysis type for " + ref +
+          " should not have been constructed as it is equivalent to the original type";
+      assert !(ref instanceof JNullType);
+      this.ref = ref.getUnderlyingType();
+      this.canBeSubclass = canBeSubclass;
+      this.canBeNull = canBeNull;
+    }
+
+    @Override
+    public boolean canBeNull() {
+      return canBeNull;
+    }
+
+    @Override
+    public boolean canBeSubclass() {
+      return canBeSubclass;
+    }
+
+    @Override
+    public String getJavahSignatureName() {
+      return ref.getJavahSignatureName();
+    }
+
+    @Override
+    public String getJsniSignatureName() {
+      return ref.getJsniSignatureName();
+    }
+
+    @Override
+    public JEnumType isEnumOrSubclass() {
+      return ref.isEnumOrSubclass();
+    }
+
+    @Override
+    public JReferenceType getUnderlyingType() {
+      return ref;
+    }
+
+    @Override
+    public boolean isAbstract() {
+      return ref.isAbstract();
+    }
+
+    @Override
+    public boolean isExternal() {
+      return ref.isExternal();
+    }
+
+    @Override
+    public boolean isFinal() {
+      return ref.isFinal();
+    }
+
+    @Override
+    public void traverse(JVisitor visitor, Context ctx) {
+      visitor.accept(ref);
+    }
+
+    private Object readResolve() {
+      // Reuse the instance stored in the ref type to make sure there is only one analysis result
+      // per type.
+      return ref.getAnalysisDecoratedTypeSingletons().getAnalysisDecoratedType(ref, ref.canBeNull(),
+          ref.canBeSubclass());
+    }
+
+    @Override
+    public String toString() {
+      return (!canBeSubclass ? "(exact)" : "") + (!canBeNull ? "(non-null)" : "") +
+          super.toString();
+    }
+  }
+
+  /**
+   * Each {@link JReferenceType} has access to the corresponding singletons (one per type of
+   * analysis result).
+   */
+  static class AnalysisDecoratedTypeSingletons {
+    private static final int NUM_PROPERTIES = 2;
+    private final JAnalysisDecoratedType[] decoratedAnalysisTypeSingletons =
+        new JAnalysisDecoratedType[2 * NUM_PROPERTIES];
+
+    private int propertyIndex(boolean canBeNull, boolean canBeSubclass) {
+      return (canBeNull ? 0 : 1) + (canBeSubclass ? 0 : 2);
+    }
+    public JReferenceType getAnalysisDecoratedType(
+        JReferenceType type, boolean canBeNull, boolean canBeSubclass) {
+      JReferenceType underlyingType = type.getUnderlyingType();
+      if (underlyingType.canBeNull() == canBeNull &&
+          underlyingType.canBeSubclass() == canBeSubclass) {
+        return underlyingType;
+      }
+      int index = propertyIndex(canBeNull, canBeSubclass);
+      JAnalysisDecoratedType result = decoratedAnalysisTypeSingletons[index];
+      if (result == null) {
+        result = decoratedAnalysisTypeSingletons[index] =
+            new JAnalysisDecoratedType(type.getUnderlyingType(), canBeSubclass, canBeNull);
+      }
+      return result;
+    }
+  }
+
 
   public JReferenceType(SourceInfo info, String name) {
     super(info, name);
   }
 
-  /**
-   * Returns <code>true</code> if it's possible for this type to be
-   * <code>null</code>.
-   *
-   * @see JNonNullType
-   */
+  JReferenceType(SourceInfo info, String name,
+      AnalysisDecoratedTypeSingletons analysisDecoratedTypeSingletons) {
+    super(info, name);
+    this.analysisDecoratedTypeSingletons = analysisDecoratedTypeSingletons;
+  }
+  @Override
   public boolean canBeNull() {
     return true;
+  }
+
+  @Override
+  public boolean canBeSubclass() {
+    return !isFinal();
   }
 
   @Override
@@ -53,11 +173,20 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
     return "L" + name.replace('.', '/') + ';';
   }
 
-  public JNonNullType getNonNull() {
-    if (nonNullType == null) {
-      nonNullType = new JNonNullType(this);
-    }
-    return nonNullType;
+  public JReferenceType weakenToNullable() {
+    return weakenToNullable(this);
+  }
+
+  public JReferenceType weakenToNonExact() {
+    return weakenToNonExact(this);
+  }
+
+  public JReferenceType strengthenToNonNull() {
+    return strengthenToNonNull(this);
+  }
+
+  public JReferenceType stengthenToExact() {
+    return stengthenToExact(this);
   }
 
   /**
@@ -71,5 +200,52 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
   public boolean replaces(JType originalType) {
     return super.replaces(originalType)
         && canBeNull() == ((JReferenceType) originalType).canBeNull();
+  }
+
+  protected AnalysisDecoratedTypeSingletons getAnalysisDecoratedTypeSingletons() {
+    if (analysisDecoratedTypeSingletons == null) {
+      analysisDecoratedTypeSingletons = new AnalysisDecoratedTypeSingletons();
+    }
+    return analysisDecoratedTypeSingletons;
+  }
+
+  private static JReferenceType weakenToNonExact(JReferenceType type) {
+    if (type.canBeSubclass() || !type.getUnderlyingType().canBeSubclass()) {
+      // If the underlying type is already exact (e.g. a final class, int[], null), we return it.
+      // While this might be seen as an optimization, many parts of the compiler assume that the
+      // underlying type is always the weakest form. So here we ensure that we don't weaken beyond
+      // the underlying type.
+      return type;
+    }
+    return
+        type.getAnalysisDecoratedTypeSingletons().getAnalysisDecoratedType(type, type.canBeNull(),
+            true);
+  }
+
+  private static JReferenceType weakenToNullable(JReferenceType type) {
+    if (type.canBeNull()) {
+      return type;
+    }
+
+    return type.getAnalysisDecoratedTypeSingletons().getAnalysisDecoratedType(type, true,
+        type.canBeSubclass());
+  }
+
+  private static JReferenceType stengthenToExact(JReferenceType type) {
+    if (!type.canBeSubclass()) {
+      return type;
+    }
+    return
+        type.getAnalysisDecoratedTypeSingletons().getAnalysisDecoratedType(type, type.canBeNull(),
+            false);
+  }
+
+  private static JReferenceType strengthenToNonNull(JReferenceType type) {
+    assert !(type.getUnderlyingType() instanceof JNullType);
+    if (!type.canBeNull()) {
+      return type;
+    }
+    return type.getAnalysisDecoratedTypeSingletons().getAnalysisDecoratedType(type, false,
+        type.canBeSubclass());
   }
 }

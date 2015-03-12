843fc550685619f2f1500a99eca027dc46122ca4
Goktug Gokdogan <goktug@google.com>
2015-03-11 20:21:38 -0700

    Refactor TypeTightener.
    Change-Id: Iad95138d336bbda66bbdbcc6d007b1b722d1c604

--- dev/core/src/com/google/gwt/dev/jjs/ast/JMethod.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/JMethod.java
@@ -291,7 +291,7 @@ public class JMethod extends JNode implements JMember, CanBeAbstract, CanBeNativ
   }
 
   public static final JMethod NULL_METHOD = new JMethod(SourceOrigin.UNKNOWN, "nullMethod", null,
-      JNullType.INSTANCE, false, false, true, AccessModifier.PUBLIC);
+      JReferenceType.NULL_TYPE, false, false, true, AccessModifier.PUBLIC);
 
   static {
     NULL_METHOD.setSynthetic();

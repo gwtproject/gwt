aff278c76b30f64836bab8c5048678eba285f261
Ray Cromwell <cromwellian@google.com>
2015-03-13 11:39:25 -0700

    Make Closure output more idiomatic
    The following changes result in an 11% improvement in Inbox when
    compiling with closure.
    
    1. defineClass returns a function as well as setting _
    2. In Closure mode, the defineClass result is assigned to a global
    and all non-static methods are hung off it's prototype.
    
    This is because when the Closure compiler sees every method assigned
    as _.method = expr;, it has no idea what '_' and how to associate
    a given method with a given constructor. By changing this to
    Foo.prototype.method = expr, the compiler can now associate
    instantiations of Foo with liveness of methods assigned to it.
    
    3. The GWT compiler now has responsibility for generating all
    goog.provide statements and setting up empty functions on
    goog.provided interface types.
    
    The reason this is done is several fold. The closure compiler
    will error if any goog.provide happens more than once, and it
    will error if a goog.provide or a namespace is setup out of
    order (enclosed type before enclosing type). Previously, a linker
    woud generate all of the goog.provide statements as well as
    setup stub functions for interfaces. However since linkers can't
    (easily) insert code in the right order (like setting up a empty
    interface stub function) in the topologically correct order with
    those setup by the GWT compiler itself, pushing this reponsibility
    to the compiler removes errors from bad sorting.
    
    Also, double assigning any namespace causes deoptimization.
    Previously the linker would pre-pend a bunch of empty stub
    constructors which are written over by the main GWT module.
    
    Finally, this is a step along the way to getting rid of the
    linker, and having GWT output @JsDoc'ed class definitions.
    
    Change-Id: I66aae7a745bb36059f1f0670d2820861527f2fc9

---
+++ dev/core/src/com/google/gwt/dev/jjs/impl/JsInteropExportsGenerator.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.dev.jjs.impl;
+
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.dev.jjs.SourceInfo;
+import com.google.gwt.dev.jjs.ast.HasName;
+import com.google.gwt.dev.jjs.ast.JDeclaredType;
+import com.google.gwt.dev.jjs.ast.JMember;
+import com.google.gwt.dev.js.ast.JsBinaryOperation;
+import com.google.gwt.dev.js.ast.JsBinaryOperator;
+import com.google.gwt.dev.js.ast.JsExpression;
+import com.google.gwt.dev.js.ast.JsFunction;
+import com.google.gwt.dev.js.ast.JsName;
+import com.google.gwt.dev.js.ast.JsNameRef;
+import com.google.gwt.dev.js.ast.JsProgram;
+import com.google.gwt.dev.js.ast.JsStatement;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Responsible for handling @JsExport code generation for non-Closure formatted code.
+ */
+public abstract class JsInteropExportsGenerator {
+
+  public static JsInteropExportsGenerator create(boolean closureCompilerFormatEnabled, JsProgram
+      jsProgram, JsName globalTemp, List<JsStatement> exportStmts,
+      Map<String, JsFunction> indexedFunctions,
+      Map<HasName, JsName> names) {
+    if (closureCompilerFormatEnabled) {
+      return new ClosureJsInteropExportsGeneratorImpl(jsProgram, globalTemp, exportStmts,
+          indexedFunctions, names);
+    } else {
+      return new DefaultJsInteropExportsGeneratorImpl(jsProgram, globalTemp, exportStmts,
+          indexedFunctions, names);
+    }
+  }
+
+  /*
+   * Exports a member as
+   * {goog.|JCHSU.}provide('foo.bar.ClassCtorNameOrClassSimpleName')
+   */
+  public abstract void exportMember(JDeclaredType x, JMember member, TreeLogger logger);
+
+
+  /**
+   * Ensures that, when needed, types with no exported members have namespaces.
+   */
+  public abstract boolean maybeExportTypeWithNoExportedMembers(JDeclaredType x, TreeLogger branch);
+
+  /**
+   * Given a string namespace qualifier such as 'foo.bar.Baz', creates a chain of JsNameRef's
+   * representing this namespace, optionally prefixed by '$wnd' qualifier.
+   */
+  static JsNameRef createQualifier(String namespace, SourceInfo sourceInfo, boolean qualifyWithWnd) {
+    JsNameRef ref = null;
+    if (namespace.isEmpty() || qualifyWithWnd) {
+      ref = new JsNameRef(sourceInfo, "$wnd");
+    }
+
+    for (String part : namespace.split("\\.")) {
+      JsNameRef newRef = new JsNameRef(sourceInfo, part);
+      if (ref != null) {
+        newRef.setQualifier(ref);
+      }
+      ref = newRef;
+    }
+    return ref;
+  }
+
+  static JsExpression createAssignment(JsExpression lhs, JsExpression rhs) {
+    return new JsBinaryOperation(lhs.getSourceInfo(), JsBinaryOperator.ASG, lhs, rhs);
+  }
+}

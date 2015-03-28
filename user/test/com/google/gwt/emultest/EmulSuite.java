3922886e9010306d60a0bcf5ea1dff08e4c3b49e
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2015-03-28 18:50:43 +0100

    Emulate Optional<T> and its int, long, double variants
    Change-Id: Id69894008c48a4e3dca06f9f739f7ed2cd5e76e1

--- user/test/com/google/gwt/emultest/EmulSuite.java
+++ user/test/com/google/gwt/emultest/EmulSuite.java
@@ -37,6 +37,10 @@ import com.google.gwt.emultest.java.security.MessageDigestTest;
 import com.google.gwt.emultest.java.sql.SqlDateTest;
 import com.google.gwt.emultest.java.sql.SqlTimeTest;
 import com.google.gwt.emultest.java.sql.SqlTimestampTest;
+import com.google.gwt.emultest.java.util.OptionalDoubleTest;
+import com.google.gwt.emultest.java.util.OptionalIntTest;
+import com.google.gwt.emultest.java.util.OptionalLongTest;
+import com.google.gwt.emultest.java.util.OptionalTest;
 import com.google.gwt.junit.tools.GWTTestSuite;
 
 import junit.framework.Test;
@@ -83,6 +87,12 @@ public class EmulSuite {
     suite.addTestSuite(SqlDateTest.class);
     suite.addTestSuite(SqlTimeTest.class);
     suite.addTestSuite(SqlTimestampTest.class);
+
+    //-- java.util
+    suite.addTestSuite(OptionalTest.class);
+    suite.addTestSuite(OptionalIntTest.class);
+    suite.addTestSuite(OptionalLongTest.class);
+    suite.addTestSuite(OptionalDoubleTest.class);
     // $JUnit-END$
 
     return suite;

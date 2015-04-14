rstudio/rstudio

commit 63520645ec65a2fb5c5f13851111f2661ec812c1

Move isJSO() from JTypeOracle to JType to fix an exact type bug.
Previous to this patch a final JSO was considered an EXACT type, which
was not correct as JSOs can cross cast to other JSOs regardless of
the final attribute.

Change-Id: Ide5826effbd561426743a78aa35f714132eeae1f

dev/core/src/com/google/gwt/dev/jjs/ast/JClassType.java
@@ -39,6 +39,7 @@ public class JClassType extends JDeclaredType {
 
   private final boolean isAbstract;
   private final boolean isFinal;
+  private boolean isJso;
   private JClassType superClass;
   private boolean isJsPrototypeStub;
 
@@ -46,6 +47,7 @@ public class JClassType extends JDeclaredType {
     super(info, name);
     this.isAbstract = isAbstract;
     this.isFinal = isFinal;
+    this.isJso = name.equals(JProgram.JAVASCRIPTOBJECT);
   }
 
   /**
@@ -55,6 +57,7 @@ public class JClassType extends JDeclaredType {
     super(SourceOrigin.UNKNOWN, name);
     isAbstract = false;
     isFinal = false;
+    isJso = name.equals(JProgram.JAVASCRIPTOBJECT);
     setExternal(true);
   }
 
@@ -81,11 +84,19 @@ public class JClassType extends JDeclaredType {
     return isFinal;
   }
 
+  @Override
+  public boolean isJsoType() {
+    return isJso;
+  }
+
   /**
    * Sets this type's super class.
    */
   public final void setSuperClass(JClassType superClass) {
+    // Super class can only be set once.
+    assert this.superClass == null;
     this.superClass = superClass;
+    this.isJso |= superClass != null && superClass.isJso;
   }
 
   public boolean isJsPrototypeStub() {

dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
@@ -368,8 +368,6 @@ public final class JavaToJavaScriptCompiler {
       // Now that the AST has stopped mutating update with the final references.
       maybeRecordReferencesAndControlFlow(true);
 
-      jprogram.typeOracle.recomputeAfterOptimizations(jprogram.getDeclaredTypes());
-
       javaEvent.end();
 
       Event javaScriptEvent = SpeedTracerLogger.start(CompilerEventType.PERMUTATION_JAVASCRIPT);
@@ -519,6 +517,8 @@ public final class JavaToJavaScriptCompiler {
       if (shouldOptimize()) {
         RemoveSpecializations.exec(jprogram);
         Pruner.exec(jprogram, false);
+        // Last Java optimization step, update type oracle accordingly.
+        jprogram.typeOracle.recomputeAfterOptimizations(jprogram.getDeclaredTypes());
       }
       ReplaceGetClassOverrides.exec(jprogram);
     } finally {
@@ -1216,7 +1216,7 @@ public final class JavaToJavaScriptCompiler {
 
   private CompilationState constructJavaAst(RebindPermutationOracle rpo,
       String[] entryPointTypeNames, String[] additionalRootTypes)
-      throws UnableToCompleteException {
+  throws UnableToCompleteException {
     Set<String> allRootTypes = Sets.newTreeSet();
     CompilationState compilationState = rpo.getCompilationState();
     Memory.maybeDumpMemory("CompStateBuilt");

dev/core/src/com/google/gwt/dev/jjs/ast/JArrayType.java
@@ -85,6 +85,11 @@ public class JArrayType extends JReferenceType {
   }
 
   @Override
+  public boolean isJsoType() {
+    return false;
+  }
+
+  @Override
   public boolean replaces(JType originalType) {
     return (originalType instanceof JArrayType)
         && elementType.replaces(((JArrayType) originalType).getElementType());

dev/core/src/com/google/gwt/dev/jjs/ast/JInterfaceType.java
@@ -63,6 +63,10 @@ public class JInterfaceType extends JDeclaredType {
   public boolean isFinal() {
     return false;
   }
+  @Override
+  public boolean isJsoType() {
+    return false;
+  }
 
   @Override
   public void traverse(JVisitor visitor, Context ctx) {

dev/core/src/com/google/gwt/dev/jjs/ast/JPrimitiveType.java
@@ -103,6 +103,10 @@ public class JPrimitiveType extends JType {
   }
 
   @Override
+  public boolean isJsoType() {
+    return false;
+  }
+  @Override
   public void traverse(JVisitor visitor, Context ctx) {
     if (visitor.visit(this, ctx)) {
     }

dev/core/src/com/google/gwt/dev/jjs/ast/JProgram.java
@@ -696,7 +696,7 @@ public class JProgram extends JNode implements ArrayTypeCreator {
 
   public JField getClassLiteralField(JType type) {
     return classLiteralFieldsByType.get(
-        typeOracle.isJavaScriptObject(type) ? getJavaScriptObject() : type);
+        type.isJsoType() ? getJavaScriptObject() : type);
   }
 
   public List<JDeclaredType> getDeclaredTypes() {
@@ -1056,7 +1056,7 @@ public class JProgram extends JNode implements ArrayTypeCreator {
           ((JArrayType) type).getDims());
     }
 
-    if (typeOracle.isJavaScriptObject(type)) {
+    if (type.isJsoType()) {
       return getJavaScriptObject();
     }
     return type;

dev/core/src/com/google/gwt/dev/jjs/ast/JReferenceType.java
@@ -51,6 +51,12 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
         }
 
         @Override
+        public boolean isJsoType() {
+          // TODO(rluble): check why it is needed that the NULL_TYPE is considered a Jso.
+          return true;
+        }
+
+        @Override
         public boolean isNullType() {
           return true;
         }
@@ -133,6 +139,11 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
     @Override
     public boolean isAbstract() {
       return ref.isAbstract();
+    }
+
+    @Override
+    public boolean isJsoType() {
+      return ref.isJsoType();
     }
 
     @Override
@@ -289,6 +300,7 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
   /**
    * If this type is a non-null type, returns the underlying (original) type.
    */
+  @Override
   public JReferenceType getUnderlyingType() {
     return this;
   }
@@ -307,7 +319,7 @@ public abstract class JReferenceType extends JType implements CanBeAbstract {
   }
 
   AnalysisResult getAnalysisResult() {
-    if (isFinal()) {
+    if (isFinal() && !isJsoType()) {
       return AnalysisResult.NULLABLE_EXACT;
     }
     return AnalysisResult.NULLABLE_NOT_EXACT;

dev/core/src/com/google/gwt/dev/jjs/ast/JType.java
@@ -70,6 +70,10 @@ public abstract class JType extends JNode implements HasName, CanBeFinal {
   }
 
   /**
+   * Returns {@code true} if this is a JavaScriptObject type.
+   */
+  public abstract boolean isJsoType();
+  /**
    * Returns <code>true</code> if it's possible for this type to be
    * a subclass of the type denoted with this type.
    *

gwtproject/gwt

commit aedbff8363215626997f0934e528740545619ec0

Fix an incorrect assertion in JClassType.
The assertion was introduced by commit
63520645ec65a2fb5c5f13851111f2661ec812c1.

Change-Id: Ic1cec88ea75004040a61af0dbc3d4f627ca12af4

dev/core/src/com/google/gwt/dev/jjs/ast/JClassType.java
@@ -93,10 +93,11 @@ public class JClassType extends JDeclaredType {
    * Sets this type's super class.
    */
   public final void setSuperClass(JClassType superClass) {
-    // Super class can only be set once.
-    assert this.superClass == null;
     this.superClass = superClass;
-    this.isJso |= superClass != null && superClass.isJso;
+
+    if (!name.equals(JProgram.JAVASCRIPTOBJECT) && superClass != null) {
+      this.isJso = superClass.isJso;
+    }
   }
 
   public boolean isJsPrototypeStub() {
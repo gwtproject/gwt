29060fc4d08127de2a5f9e073a37afbcb128c348
Goktug Gokdogan <goktug@google.com>
2015-05-25 00:33:18 -0700

    Enhances JRE emul to utilize ES6 Map.
    All major browsers added support for ES6 Map with the exception
    of IE11 (which includes Map but missing the essential iteration
    methods).
    A polyfill similar to existing implementation is provided for
    older browsers.
    
    In Chrome, GWT benchmarks shows that String put operations are
    ~2.5x faster and Object put operations are ~1.2x faster but there
    is a slight slow down (~15%) for iteration which I think will be
    fixed by Chrome later.
    
    [New]
    HashmapInsertString - 5400
    HashmapContainsString - 6200
    HashmapIterationString - 6100
    HashMapInsertObject - 661
    
    [Old]
    HashmapInsertString - 2200
    HashmapContainsString - 6100
    HashmapIterationString - 7100
    HashMapInsertObject - 529
    
    Interestingly, although jsperf benchmark in [1] is in line with
    improvements seen in GWT benchmarks, my other benchmarks [2] that
    are testing different patterns shows significant slow downs.
    
    [1] Put/set/iteration with fixed key length which might collide.
     http://jsperf.com/es6-map-vs-object-properties/10
    
    [2] Different benchmarks showing performance characteristics with
    different key scenarions (random lenghth, all collide, non-collide
    etc.):
    http://jsperf.com/iterate-es6-map-vs-object-properties
    http://jsperf.com/put-es6-map-vs-object-properties
    http://jsperf.com/put-collide-es6-map-vs-object-properties
    http://jsperf.com/gwt-es6-map-vs-object-properties
    http://jsperf.com/get-miss-es6-map-vs-object-properties
    
    Change-Id: I276fb0b57f6852bd8763d66cc8c0fbdead641dbb
    Review-Link: https://gwt-review.googlesource.com/#/c/11320/

--- user/src/com/google/gwt/junit/JUnitShell.java
+++ user/src/com/google/gwt/junit/JUnitShell.java
@@ -51,6 +51,7 @@ import com.google.gwt.dev.util.arg.ArgHandlerDisableRunAsync;
 import com.google.gwt.dev.util.arg.ArgHandlerDisableUpdateCheck;
 import com.google.gwt.dev.util.arg.ArgHandlerDraftCompile;
 import com.google.gwt.dev.util.arg.ArgHandlerEnableAssertions;
+import com.google.gwt.dev.util.arg.ArgHandlerEnableClosureCompiler;
 import com.google.gwt.dev.util.arg.ArgHandlerExtraDir;
 import com.google.gwt.dev.util.arg.ArgHandlerGenDir;
 import com.google.gwt.dev.util.arg.ArgHandlerIncrementalCompile;
@@ -284,6 +285,7 @@ public class JUnitShell extends DevMode {
       registerHandler(new ArgHandlerDisableRunAsync(options));
       registerHandler(new ArgHandlerDisableUpdateCheck(options));
       registerHandler(new ArgHandlerDraftCompile(options));
+      registerHandler(new ArgHandlerEnableClosureCompiler(options));
       registerHandler(new ArgHandlerMaxPermsPerPrecompile(options));
       registerHandler(new ArgHandlerLocalWorkers(options));
       registerHandler(new ArgHandlerNamespace(options));

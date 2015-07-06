2a79b1ba31ac59f0d497e7f47e96fb3ab5097783
Stephen Haberman <stephen.haberman@gmail.com>
2015-07-06 09:46:51 -0500

    Add more Locale constants.
    Change-Id: Ica79b80b0059734302c9cbc3a51d74ecad59dc78

--- user/super/com/google/gwt/emul/java/util/Locale.java
+++ user/super/com/google/gwt/emul/java/util/Locale.java
@@ -38,12 +38,131 @@ public class Locale {
     }
   };
 
+  public static final Locale FRENCH = new Locale() {
+    @Override
+    public String toString() {
+      return "fr";
+    }
+  };
+
+  public static final Locale GERMAN = new Locale() {
+    @Override
+    public String toString() {
+      return "de";
+    }
+  };
+
+  public static final Locale ITALIAN = new Locale() {
+    @Override
+    public String toString() {
+      return "it";
+    }
+  };
+
+  public static final Locale JAPANESE = new Locale() {
+    @Override
+    public String toString() {
+      return "ja";
+    }
+  };
+
+  public static final Locale KOREAN = new Locale() {
+    @Override
+    public String toString() {
+      return "ko";
+    }
+  };
+
+  public static final Locale CHINESE = new Locale() {
+    @Override
+    public String toString() {
+      return "zh";
+    }
+  };
+
+  public static final Locale SIMPLIFIED_CHINESE = new Locale() {
+    @Override
+    public String toString() {
+      return "zh_CN";
+    }
+  };
+
+  public static final Locale TRADITIONAL_CHINESE = new Locale() {
+    @Override
+    public String toString() {
+      return "zh_TW";
+    }
+  };
+
+  public static final Locale FRANCE = new Locale() {
+    @Override
+    public String toString() {
+      return "fr_FR";
+    }
+  };
+
+  public static final Locale GERMANY = new Locale() {
+    @Override
+    public String toString() {
+      return "de_DE";
+    }
+  };
+
+  public static final Locale GERMANY = new Locale() {
+    @Override
+    public String toString() {
+      return "it_IT";
+    }
+  };
+
+  public static final Locale JAPAN = new Locale() {
+    @Override
+    public String toString() {
+      return "ja_JP";
+    }
+  };
+
+  public static final Locale KOREA = new Locale() {
+    @Override
+    public String toString() {
+      return "ko_KR";
+    }
+  };
+
+  public static final Locale CHINA = SIMPLIFIED_CHINESE;
+
+  public static final Locale PRC = SIMPLIFIED_CHINESE;
+
+  public static final Locale TAIWAN = TRADITIONAL_CHINESE;
+
+  public static final Locale UK = new Locale() {
+    @Override
+    public String toString() {
+      return "en_GB";
+    }
+  };
+
   public static final Locale US = new Locale() {
     @Override
     public String toString() {
       return "en_US";
     }
   };
+
+  public static final Locale CANADA = new Locale() {
+    @Override
+    public String toString() {
+      return "en_CA";
+    }
+  };
+
+  public static final Locale CANADA_FRENCH = new Locale() {
+    @Override
+    public String toString() {
+      return "fr_CA";
+    }
+  };
+
 
   private static Locale defaultLocale = new Locale() {
     @Override

a5bd5af212a9cbb4ad6ac156fce3cdd78f1ced74
Daniel Kurka <dankurka@google.com>
2015-07-18 21:32:33 +0200

    Move shared methods off of String.
    GWT's String class had a couple of shared methods that were
    used in StringBuilder and Long. Moved these to a
    StringHelper class.
    
    Change-Id: I912746c2024a6528b8c362554e1093a1ca10a830

---
+++ dev/core/super/javaemul/internal/StringHelper.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package javaemul.internal;
+
+/**
+ * StringHelper contains utility methods for String processing that are used by multiple classes
+ * in GWT's emul.
+ */
+public class StringHelper {
+  /**
+   * Checks that bounds are correct.
+   *
+   * @param legalCount the end of the legal range
+   * @param start must be >= 0
+   * @param end must be <= legalCount and must be >= start
+   * @throw StringIndexOutOfBoundsException if the range is not legal
+   * @skip
+   */
+  public static void checkBounds(int legalCount, int start, int end) {
+    if (start < 0) {
+      throw new StringIndexOutOfBoundsException(start);
+    }
+    if (end < start) {
+      throw new StringIndexOutOfBoundsException(end - start);
+    }
+    if (end > legalCount) {
+      throw new StringIndexOutOfBoundsException(end);
+    }
+  }
+
+  public static String valueOf(char x[], int start, int end) {
+    return valueOf(x, start, end, ArrayHelper.ARRAY_PROCESS_BATCH_SIZE);
+  }
+
+  private static native String valueOf(char x[], int start, int end, int batchSize) /*-{
+    // Work around function.prototype.apply call stack size limits:
+    // https://code.google.com/p/v8/issues/detail?id=2896
+    // Performance: http://jsperf.com/string-fromcharcode-test/13
+    var s = "";
+    for (var batchStart = start; batchStart < end;) { // increment in block
+      var batchEnd = Math.min(batchStart + batchSize, end);
+      s += String.fromCharCode.apply(null, x.slice(batchStart, batchEnd));
+      batchStart = batchEnd;
+    }
+    return s;
+  }-*/;
+}
+

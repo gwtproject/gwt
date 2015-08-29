2daf998ed6085a5cc568ba08aea90f2954d8230f
Goktug Gokdogan <goktug@google.com>
2015-08-28 19:59:52 -0700

    Simplifies Long.toString.
    It should slightly improve the performance as well.
    Adapted from the algorithm in android-sdk.
    
    Change-Id: I10a896c52c625be4a147b0dc0e6ecad3ab8353a9

--- user/super/com/google/gwt/emul/java/lang/Long.java
+++ user/super/com/google/gwt/emul/java/lang/Long.java
@@ -164,36 +164,42 @@ public final class Long extends Number implements Comparable<Long> {
   }
 
   public static String toString(long value, int intRadix) {
-    if (intRadix == 10 || intRadix < Character.MIN_RADIX
-        || intRadix > Character.MAX_RADIX) {
+    if (intRadix == 10 || intRadix < Character.MIN_RADIX || intRadix > Character.MAX_RADIX) {
       return String.valueOf(value);
     }
 
-    if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE) {
-      return Integer.toString((int) value, intRadix);
+    int intValue = (int) value;
+    if (intValue == value) {
+      return Integer.toString(intValue, intRadix);
     }
 
-    final int bufSize = 65;
-    char[] buf = new char[bufSize];
-    int pos = bufSize - 1;
+    /*
+     * If v is positive, negate it. This is the opposite of what one might expect. It is necessary
+     * because the range of the negative values is strictly larger than that of the positive values:
+     * there is no positive value corresponding to Long.MIN_VALUE.
+     */
+    boolean negative = value < 0;
+    if (!negative) {
+      value = -value;
+    }
+
+    int bufLen = intRadix < 8 ? 65 : 23; // Max chars in result (conservative)
+    char[] buf = new char[bufLen];
+    int cursor = bufLen;
+
     // Cache a converted version for performance (pure long ops are faster).
     long radix = intRadix;
-    if (value >= 0) {
-      while (value >= radix) {
-        buf[pos--] = Character.forDigit((int) (value % radix));
-        value /= radix;
-      }
-      buf[pos] = Character.forDigit((int) value);
-    } else {
-      long negRadix = -radix;
-      while (value <= negRadix) {
-        buf[pos--] = Character.forDigit(-((int) (value % radix)));
-        value /= radix;
-      }
-      buf[pos--] = Character.forDigit(-((int) value));
-      buf[pos] = '-';
+    do {
+      long q = value / radix;
+      buf[--cursor] = Character.forDigit((int) (radix * q - value));
+      value = q;
+    } while (value != 0);
+
+    if (negative) {
+      buf[--cursor] = '-';
     }
-    return StringHelper.valueOf(buf, pos, bufSize);
+
+    return String.valueOf(buf, cursor, bufLen - cursor);
   }
 
   public static Long valueOf(long i) {

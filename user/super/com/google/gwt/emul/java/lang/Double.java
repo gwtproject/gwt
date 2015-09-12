ae292edc8a9616768177f02f2c87fc006f2ae4f0
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2015-09-13 02:33:35 +0300

    Add some java.lang Java 8 API
    Change-Id: I5f2ca3069de73ad8d8801d8e7bca7ebb1ed36e43

--- user/super/com/google/gwt/emul/java/lang/Double.java
+++ user/super/com/google/gwt/emul/java/lang/Double.java
@@ -35,6 +35,7 @@ public final class Double extends Number implements Comparable<Double> {
   public static final double NEGATIVE_INFINITY = -1d / 0d;
   public static final double POSITIVE_INFINITY = 1d / 0d;
   public static final int SIZE = 64;
+  public static final int BYTES = SIZE / Byte.SIZE;
   public static final Class<Double> TYPE = double.class;
 
   // 2^512, 2^-512
@@ -194,20 +195,21 @@ public final class Double extends Number implements Comparable<Double> {
     return (ihi << 32) | ilo;
   }
 
-  /**
-   * @skip Here for shared implementation with Arrays.hashCode
-   */
   public static int hashCode(double d) {
     return (int) d;
   }
 
-  public static boolean isInfinite(double x) {
-    return x == JsUtils.getInfinity() || x == -JsUtils.getInfinity();
+  public static boolean isFinite(double x) {
+    return NEGATIVE_INFINITY < x && x < POSITIVE_INFINITY;
   }
 
-  public static native boolean isNaN(double x) /*-{
-    return isNaN(x);
-  }-*/;
+  public static boolean isInfinite(double x) {
+    return x == NEGATIVE_INFINITY || x == POSITIVE_INFINITY;
+  }
+
+  public static boolean isNaN(double x) {
+    return JsUtils.isNaN(x);
+  }
 
   public static double longBitsToDouble(long bits) {
     long ihi = (long) (bits >> 32);
@@ -261,8 +263,20 @@ public final class Double extends Number implements Comparable<Double> {
     return negative ? -d : d;
   }
 
+  public static double max(double a, double b) {
+    return Math.max(a, b);
+  }
+
+  public static double min(double a, double b) {
+    return Math.min(a, b);
+  }
+
   public static double parseDouble(String s) throws NumberFormatException {
     return __parseAndValidateDouble(s);
+  }
+
+  public static double sum(double a, double b) {
+    return a + b;
   }
 
   public static String toString(double b) {

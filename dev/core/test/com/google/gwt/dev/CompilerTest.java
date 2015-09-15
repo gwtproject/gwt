rstudio/rstudio

commit e261a009786b4e855da569c257c12f3d838c65ca

Double and Boolean implemented as unboxed raw types.
Change-Id: I3a11e91be6ceb0d72b727a43c28581ebef6d70e3

dev/core/src/com/google/gwt/dev/jjs/ast/JProgram.java
@@ -22,6 +22,7 @@ import com.google.gwt.dev.jjs.InternalCompilerException;
 import com.google.gwt.dev.jjs.SourceInfo;
 import com.google.gwt.dev.jjs.SourceOrigin;
 import com.google.gwt.dev.jjs.impl.GwtAstBuilder;
+import com.google.gwt.dev.jjs.impl.TypeCategory;
 import com.google.gwt.dev.jjs.impl.codesplitter.FragmentPartitioningResult;
 import com.google.gwt.dev.util.StringInterner;
 import com.google.gwt.dev.util.log.speedtracer.CompilerEventType;
@@ -32,6 +33,7 @@ import com.google.gwt.thirdparty.guava.common.collect.BiMap;
 import com.google.gwt.thirdparty.guava.common.collect.Collections2;
 import com.google.gwt.thirdparty.guava.common.collect.HashBiMap;
 import com.google.gwt.thirdparty.guava.common.collect.ImmutableList;
+import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
 import com.google.gwt.thirdparty.guava.common.collect.Lists;
 import com.google.gwt.thirdparty.guava.common.collect.Maps;
 import com.google.gwt.thirdparty.guava.common.collect.Sets;
@@ -43,6 +45,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -53,6 +56,68 @@ import java.util.Set;
  * Root for the AST representing an entire Java program.
  */
 public class JProgram extends JNode implements ArrayTypeCreator {
+
+  /**
+   * Encapsulates all information necessary to deal with native represented types in an
+   * generic fashion used throughout GWT. This can be extended later to deal with say, unboxed
+   * Integer if desired.
+   */
+  public enum DispatchType {
+    // These this list can be extended by creating the appropriate fields/methods on Cast,
+    // as well as extending the TypeCategory enum and updating EqualityNormalizer.
+    STRING(true),
+    DOUBLE(true),
+    BOOLEAN(true),
+
+    // non-native represented type values.
+    HAS_JAVA_VIRTUAL_DISPATCH(false), JAVA_ARRAY(false), JSO(false);
+
+    private final String instanceOfMethod;
+    private final String castMapField;
+    private final TypeCategory typeCategory;
+    private final String dynamicCastMethod;
+
+    DispatchType(boolean nativeType) {
+      if (nativeType) {
+        // These field are initialized to methods that are by-convention
+        // The conventions are:
+        // Cast.isJava[BoxedTypeName] for instanceof checks
+        // Cast.[boxedTypeName]CastMap for cast map fields
+        // Cast.dynamicCastTo[BoxedTypeName] for cast checks
+        // TypedCategory.TYPE_JAVA_LANG_[BoxedTypeName]
+        // If Cast or TypeCategory is edited, update this constructor
+        this.instanceOfMethod = "Cast.isJava" + camelCase(name());
+        this.castMapField = "Cast." + name().toLowerCase() + "CastMap";
+        this.dynamicCastMethod = "Cast.dynamicCastTo" + camelCase(name());
+        this.typeCategory = TypeCategory.valueOf("TYPE_JAVA_LANG_" + name());
+      } else {
+        instanceOfMethod = null;
+        castMapField = null;
+        typeCategory = null;
+        dynamicCastMethod = null;
+      }
+    }
+
+    private static String camelCase(String name) {
+      return Character.toUpperCase(name.charAt(0)) + name.toLowerCase().substring(1);
+    }
+
+    public String getInstanceOfMethod() {
+      return instanceOfMethod;
+    }
+
+    public String getCastMapField() {
+      return castMapField;
+    }
+
+    public TypeCategory getTypeCategory() {
+      return typeCategory;
+    }
+
+    public String getDynamicCastMethod() {
+      return dynamicCastMethod;
+    }
+  }
 
   /**
    * Returns whether a class is a synthetic Prototype class generated by APT or user.
@@ -288,7 +353,13 @@ public class JProgram extends JNode implements ArrayTypeCreator {
 
   private JClassType typeString;
 
+  private JClassType typeDouble;
+
+  private JClassType typeBoolean;
+
   private FragmentPartitioningResult fragmentPartitioningResult;
+
+  private Map<JClassType, DispatchType> nativeType2DispatchType;
 
   /**
    * Add a pinned method.
@@ -353,6 +424,12 @@ public class JProgram extends JNode implements ArrayTypeCreator {
       case "java.lang.String":
         typeString = (JClassType) type;
         break;
+      case "java.lang.Double":
+        typeDouble = (JClassType) type;
+        break;
+      case "java.lang.Boolean":
+        typeBoolean = (JClassType) type;
+        break;
       case "java.lang.Class":
         typeClass = (JClassType) type;
         break;
@@ -363,6 +440,59 @@ public class JProgram extends JNode implements ArrayTypeCreator {
         typeSpecialClassLiteralHolder = (JClassType) type;
         break;
     }
+  }
+
+  public boolean isRepresentedAsNativeJsPrimitive(JType type) {
+    return getRepresentedAsNativeTypes().contains(type);
+  }
+
+  public Set<JClassType> getRepresentedAsNativeTypes() {
+    return getRepresentedAsNativeTypesDispatchMap().keySet();
+  }
+
+  public Map<JClassType, DispatchType> getRepresentedAsNativeTypesDispatchMap() {
+     if (nativeType2DispatchType == null) {
+       // these are returned in the reverse order in which they are checked
+       // because devirtualizer constructs the nested expressions from the inside out.
+       nativeType2DispatchType = ImmutableMap.of(
+           typeBoolean, DispatchType.BOOLEAN,
+           typeDouble, DispatchType.DOUBLE,
+           typeString, DispatchType.STRING);
+     }
+    return nativeType2DispatchType;
+  }
+
+  public EnumSet<DispatchType> getDispatchType(JReferenceType type) {
+    if (!typeOracle.isInstantiatedType(type)) {
+      return EnumSet.noneOf(DispatchType.class);
+    }
+
+    // Object methods can be dispatched to all four possible classes.
+    if (type == getTypeJavaLangObject()) {
+      return EnumSet.allOf(DispatchType.class);
+    }
+
+    EnumSet<DispatchType> dispatchSet = EnumSet.noneOf(DispatchType.class);
+    DispatchType dispatchType = getRepresentedAsNativeTypesDispatchMap().get(type);
+    if (dispatchType != null) {
+      dispatchSet = EnumSet.of(dispatchType);
+    } else if (typeOracle.isDualJsoInterface(type)) {
+      // If it is an interface implemented both by JSOs and regular Java Objects;
+      dispatchSet = EnumSet.of(DispatchType.HAS_JAVA_VIRTUAL_DISPATCH, DispatchType.JSO);
+    } else if (typeOracle.isSingleJsoImpl(type) || type.isJsoType()) {
+      // If it is either an interface implemented by JSOs or JavaScriptObject or one of its
+      // subclasses.
+      dispatchSet = EnumSet.of(DispatchType.JSO);
+    }
+
+    for (JDeclaredType potentialNativeDispatchType : getRepresentedAsNativeTypes()) {
+      if (typeOracle.isInstantiatedType(potentialNativeDispatchType) &&
+          typeOracle.castSucceedsTrivially(potentialNativeDispatchType, type)) {
+        dispatchSet.add(getRepresentedAsNativeTypesDispatchMap().get(potentialNativeDispatchType));
+        dispatchSet.add(DispatchType.HAS_JAVA_VIRTUAL_DISPATCH);
+      }
+    }
+    return dispatchSet;
   }
 
   /**

user/test/com/google/gwt/dev/jjs/test/CompilerTest.java
@@ -363,6 +363,42 @@ public class CompilerTest extends GWTTestCase {
     byte[] bytes = new byte[10];
     bytes[0] = (byte) '1';
     assertEquals(49, bytes[0]);
+
+    Object[] disguisedApple = apple;
+    expectArrayStoreException(disguisedApple, "Hello");
+    expectArrayStoreException(disguisedApple, true);
+    expectArrayStoreException(disguisedApple, 42.0);
+  }
+
+  private void expectArrayStoreException(Object[] array, Object o) {
+    try {
+      array[0] = o;
+      fail("Expected ArrayStoreException");
+    } catch (ArrayStoreException e) {
+    }
+  }
+
+  public void testUnboxedArrays() {
+    Double[] doubleArray = new Double[1];
+    doubleArray[0] = 42.0;
+
+    expectArrayStoreException(doubleArray, true);
+    expectArrayStoreException(doubleArray, "Hello");
+    expectArrayStoreException(doubleArray, new Integer(23));
+
+    Boolean[] booleanArray = new Boolean[1];
+    booleanArray[0] = true;
+
+    expectArrayStoreException(booleanArray, 42.0);
+    expectArrayStoreException(booleanArray, "Hello");
+    expectArrayStoreException(booleanArray, new Integer(23));
+
+    String[] stringArray = new String[1];
+    stringArray[0] = "Hello";
+
+    expectArrayStoreException(stringArray, 42.0);
+    expectArrayStoreException(stringArray, true);
+    expectArrayStoreException(stringArray, new Integer(23));
   }
 
   /**

dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
@@ -113,6 +113,7 @@ import com.google.gwt.dev.jjs.impl.ResolveRuntimeTypeReferences.IntTypeMapper;
 import com.google.gwt.dev.jjs.impl.ResolveRuntimeTypeReferences.StringTypeMapper;
 import com.google.gwt.dev.jjs.impl.ResolveRuntimeTypeReferences.TypeMapper;
 import com.google.gwt.dev.jjs.impl.ResolveRuntimeTypeReferences.TypeOrder;
+import com.google.gwt.dev.jjs.impl.RewriteConstructorCallsForUnboxedTypes;
 import com.google.gwt.dev.jjs.impl.SameParameterValueOptimizer;
 import com.google.gwt.dev.jjs.impl.SourceInfoCorrelator;
 import com.google.gwt.dev.jjs.impl.TypeCoercionNormalizer;
@@ -324,6 +325,9 @@ public final class JavaToJavaScriptCompiler {
 
       // TODO(stalcup): hide metrics gathering in a callback or subclass
       logger.log(TreeLogger.INFO, "Compiling permutation " + permutationId + "...");
+
+      // Rewrite calls to from boxed constructor types to specialized unboxed methods
+      RewriteConstructorCallsForUnboxedTypes.exec(jprogram);
 
       // (2) Transform unresolved Java AST to resolved Java AST
       ResolvePermutationDependentValues

dev/core/src/com/google/gwt/dev/jjs/impl/ControlFlowAnalyzer.java
@@ -413,7 +413,6 @@ public class ControlFlowAnalyzer {
       }
 
       rescueArgumentsIfParametersCanBeRead(call);
-
       return false;
     }
 
@@ -514,7 +513,8 @@ public class ControlFlowAnalyzer {
     private boolean canBeInstantiatedInJavaScript(JType type) {
       // Technically, JsType/JsFunction are also instantiatable in JavaScript but we don't track
       // them using similar to JSO as if we do that then after cast normalization, they got pruned.
-      if (program.typeOracle.canBeJavaScriptObject(type) || program.isJavaLangString(type)) {
+      if (program.typeOracle.canBeJavaScriptObject(type)
+          || program.isRepresentedAsNativeJsPrimitive(type)) {
         return true;
       }
 

gwtproject/gwt

commit 1e0871712bd370c447cfee077c227bbdc1f63157

Fix compiler exception in SuperDevMode.
Commit e261a009786 (Double and Booleans as native types) introduced
an error that causes the compiler to abort during UnifyAST due
to having only partially processed types like Double that require
devirtualization.

Change-Id: Id8bb52ae7566e8e11b4ec901b38443e3a8b9d6c3

dev/core/src/com/google/gwt/dev/jjs/ast/JProgram.java
@@ -29,8 +29,10 @@ import com.google.gwt.dev.util.log.speedtracer.CompilerEventType;
 import com.google.gwt.dev.util.log.speedtracer.SpeedTracerLogger;
 import com.google.gwt.dev.util.log.speedtracer.SpeedTracerLogger.Event;
 import com.google.gwt.thirdparty.guava.common.base.Function;
+import com.google.gwt.thirdparty.guava.common.base.Predicates;
 import com.google.gwt.thirdparty.guava.common.collect.BiMap;
 import com.google.gwt.thirdparty.guava.common.collect.Collections2;
+import com.google.gwt.thirdparty.guava.common.collect.FluentIterable;
 import com.google.gwt.thirdparty.guava.common.collect.HashBiMap;
 import com.google.gwt.thirdparty.guava.common.collect.ImmutableList;
 import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
@@ -65,9 +67,11 @@ public class JProgram extends JNode implements ArrayTypeCreator {
   public enum DispatchType {
     // These this list can be extended by creating the appropriate fields/methods on Cast,
     // as well as extending the TypeCategory enum and updating EqualityNormalizer.
-    STRING(true),
-    DOUBLE(true),
+    // The order in which these native types appear is the inverse as the way they are
+    // checked by devirtualized method.
     BOOLEAN(true),
+    DOUBLE(true),
+    STRING(true),
 
     // non-native represented type values.
     HAS_JAVA_VIRTUAL_DISPATCH(false), JAVA_ARRAY(false), JSO(false);
@@ -76,6 +80,7 @@ public class JProgram extends JNode implements ArrayTypeCreator {
     private final String castMapField;
     private final TypeCategory typeCategory;
     private final String dynamicCastMethod;
+    private final String className;
 
     DispatchType(boolean nativeType) {
       if (nativeType) {
@@ -90,11 +95,13 @@ public class JProgram extends JNode implements ArrayTypeCreator {
         this.castMapField = "Cast." + name().toLowerCase() + "CastMap";
         this.dynamicCastMethod = "Cast.dynamicCastTo" + camelCase(name());
         this.typeCategory = TypeCategory.valueOf("TYPE_JAVA_LANG_" + name());
+        this.className = "java.lang." + camelCase(name());
       } else {
-        instanceOfMethod = null;
-        castMapField = null;
-        typeCategory = null;
-        dynamicCastMethod = null;
+        this.instanceOfMethod = null;
+        this.castMapField = null;
+        this.typeCategory = null;
+        this.dynamicCastMethod = null;
+        this.className = null;
       }
     }
 
@@ -116,6 +123,10 @@ public class JProgram extends JNode implements ArrayTypeCreator {
 
     public String getDynamicCastMethod() {
       return dynamicCastMethod;
+    }
+
+    public String getclassName() {
+      return className;
     }
   }
 
@@ -372,7 +383,7 @@ public class JProgram extends JNode implements ArrayTypeCreator {
 
   private FragmentPartitioningResult fragmentPartitioningResult;
 
-  private Map<JClassType, DispatchType> nativeType2DispatchType;
+  private Map<JClassType, DispatchType> dispatchTypeByNativeType;
 
   /**
    * Add a pinned method.
@@ -455,6 +466,16 @@ public class JProgram extends JNode implements ArrayTypeCreator {
     }
   }
 
+  public static Set<String> getRepresentedAsNativeClassNames() {
+    return FluentIterable.from(Arrays.asList(DispatchType.values())).transform(
+        new Function<DispatchType, String>() {
+          @Override
+          public String apply(DispatchType dispatchType) {
+            return dispatchType.getclassName();
+          }
+        }).filter(Predicates.notNull()).toSet();
+  }
+
   public boolean isRepresentedAsNativeJsPrimitive(JType type) {
     return getRepresentedAsNativeTypes().contains(type);
   }
@@ -464,15 +485,18 @@ public class JProgram extends JNode implements ArrayTypeCreator {
   }
 
   public Map<JClassType, DispatchType> getRepresentedAsNativeTypesDispatchMap() {
-     if (nativeType2DispatchType == null) {
-       // these are returned in the reverse order in which they are checked
-       // because devirtualizer constructs the nested expressions from the inside out.
-       nativeType2DispatchType = ImmutableMap.of(
-           typeBoolean, DispatchType.BOOLEAN,
-           typeDouble, DispatchType.DOUBLE,
-           typeString, DispatchType.STRING);
+     if (dispatchTypeByNativeType == null) {
+       ImmutableMap.Builder builder = new ImmutableMap.Builder();
+       for (DispatchType dispatchType : DispatchType.values()) {
+         if (dispatchType.getclassName() == null) {
+           continue;
+         }
+         JClassType classType = (JClassType) getFromTypeMap(dispatchType.getclassName());
+         builder.put(classType, dispatchType);
+       }
+       dispatchTypeByNativeType = builder.build();
      }
-    return nativeType2DispatchType;
+    return dispatchTypeByNativeType;
   }
 
   public EnumSet<DispatchType> getDispatchType(JReferenceType type) {

dev/core/src/com/google/gwt/dev/jjs/impl/UnifyAst.java
@@ -1354,7 +1354,8 @@ public class UnifyAst {
 
   private boolean requiresDevirtualization(JDeclaredType type) {
     // NOTE: these types are the ones {@link Devirtualizer} handles.
-    return isJso(type) || type == program.getTypeJavaLangString();
+    return isJso(type) ||
+        JProgram.getRepresentedAsNativeClassNames().contains(type.getName());
   }
 
   private boolean isJso(JDeclaredType type) {

dev/core/test/com/google/gwt/dev/CompilerTest.java
@@ -1262,6 +1262,14 @@ public class CompilerTest extends ArgProcessorTestBase {
     checkIncrementalRecompile_devirtualizeString(JsOutputOption.DETAILED);
   }
 
+  public void testIncrementalRecompile_devirtualizeComparable()
+      throws UnableToCompleteException, IOException, InterruptedException {
+    // Tests that Doublecalls through interfaces are correctly devirtualized when compiling per
+    // file and neither String nor CharSequence interface are stale.
+    checkIncrementalRecompile_devirtualizeComparable(JsOutputOption.OBFUSCATED);
+    checkIncrementalRecompile_devirtualizeComparable(JsOutputOption.DETAILED);
+  }
+
   public void testIncrementalRecompile_multipleClassGenerator()
       throws UnableToCompleteException, IOException, InterruptedException {
     // Tests that a Generated type that is not directly referenced from the rebound GWT.create()
@@ -1626,14 +1634,14 @@ public class CompilerTest extends ArgProcessorTestBase {
     File relinkApplicationDir = Files.createTempDir();
 
     String originalJs = compileToJs(relinkApplicationDir, "com.foo.SimpleModule", Lists
-        .newArrayList(simpleModuleResource, simpleModelEntryPointResource, simpleModelResource,
-            constantsModelResource),
+            .newArrayList(simpleModuleResource, simpleModelEntryPointResource, simpleModelResource,
+                constantsModelResource),
         relinkMinimalRebuildCache, emptySet, output);
 
     // Compile again with the same source but a new date stamp on SimpleModel and reusing the
     // minimalRebuildCache.
     String relinkedJs = compileToJs(relinkApplicationDir, "com.foo.SimpleModule",
-        Lists.<MockResource> newArrayList(simpleModelResource), relinkMinimalRebuildCache,
+        Lists.<MockResource>newArrayList(simpleModelResource), relinkMinimalRebuildCache,
         stringSet("com.foo.TestEntryPoint", "com.foo.SimpleModel"), output);
 
     assertTrue(originalJs.equals(relinkedJs));
@@ -1807,6 +1815,39 @@ public class CompilerTest extends ArgProcessorTestBase {
         devirtualizeStringEntryPointResource, devirtualizeStringEntryPointResource,
         stringSet("com.foo.DevirtualizeStringEntryPoint",
             getEntryMethodHolderTypeName("com.foo.DevirtualizeStringModule")),
+        output);
+  }
+
+  private void checkIncrementalRecompile_devirtualizeComparable(JsOutputOption output)
+      throws UnableToCompleteException, IOException, InterruptedException {
+
+    final MockResource devirtualizeComparableModuleResource =
+        JavaResourceBase.createMockResource("com/foo/DevirtualizeComparableModule.gwt.xml",
+            "<module>",
+            "<source path=''/>",
+            "<entry-point class='com.foo.DevirtualizeComparableEntryPoint'/>",
+            "</module>");
+
+    final MockJavaResource devirtualizeComparableEntryPointResource =
+        JavaResourceBase.createMockJavaResource("com.foo.DevirtualizeComparableEntryPoint",
+            "package com.foo;",
+            "import com.google.gwt.core.client.EntryPoint;",
+            "public class DevirtualizeComparableEntryPoint implements EntryPoint {",
+            "  @Override",
+            "  public void onModuleLoad() {",
+            "    Comparable c = (Double) 0.1;",
+            "    c.compareTo(c);",
+            "  }",
+            "}");
+
+    CompilerOptions compilerOptions = new CompilerOptionsImpl();
+    compilerOptions.setUseDetailedTypeIds(true);
+
+    checkRecompiledModifiedApp(compilerOptions, "com.foo.DevirtualizeComparableModule",
+        Lists.newArrayList(devirtualizeComparableModuleResource),
+        devirtualizeComparableEntryPointResource, devirtualizeComparableEntryPointResource,
+        stringSet("com.foo.DevirtualizeComparableEntryPoint",
+            getEntryMethodHolderTypeName("com.foo.DevirtualizeComparableModule")),
         output);
   }
 
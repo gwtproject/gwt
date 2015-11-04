84a71d249c52f001035c1c7d5406d4a9ceebdadc
Goktug Gokdogan <goktug@google.com>
2015-11-03 19:13:27 -0800

    Refactor property/member identificaiton for JsInterop.
    Change-Id: I7e4d2bc0675bb08f013c69993c1fd602f12a8ca1

--- dev/core/src/com/google/gwt/dev/jjs/ast/JField.java
+++ dev/core/src/com/google/gwt/dev/jjs/ast/JField.java
@@ -80,6 +80,7 @@ public class JField extends JVariable implements JMember {
   public static final JField NULL_FIELD = new JField(SourceOrigin.UNKNOWN, "nullField", null,
       JReferenceType.NULL_TYPE, false, Disposition.FINAL);
 
+  private JsMemberType jsMembertype = JsMemberType.NONE;
   private String jsName;
   private String jsNamespace;
   private boolean exported;
@@ -94,6 +95,7 @@ public class JField extends JVariable implements JMember {
    * The access modifier; stored as an int to reduce memory / serialization footprint.
    */
   private final int access;
+
 
   public JField(SourceInfo info, String name, JDeclaredType enclosingType, JType type,
       boolean isStatic, Disposition disposition, AccessModifier access) {
@@ -131,16 +133,24 @@ public class JField extends JVariable implements JMember {
   }
 
   @Override
-  public void setJsMemberInfo(String namespace, String name, boolean exported) {
-    this.jsName = name;
+  public void setJsMemberInfo(
+      JsMemberType jsMembertype, String namespace, String name, boolean exported) {
+    this.jsMembertype = jsMembertype;
+    this.jsName = name != null ? name : jsMembertype.computeName(this);
     this.jsNamespace = namespace;
     this.exported = exported;
+  }
+
+  @Override
+  public JsMemberType getJsMemberType() {
+    return jsMembertype;
   }
 
   public boolean isJsInteropEntryPoint() {
     return exported && isStatic();
   }
 
+  @Override
   public boolean canBeReferencedExternally() {
     return exported;
   }
@@ -159,10 +169,6 @@ public class JField extends JVariable implements JMember {
   @Override
   public boolean isAbstract() {
     return false;
-  }
-
-  public boolean isJsProperty() {
-    return jsName != null;
   }
 
   @Override

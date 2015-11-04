d19ad74a291806ef33017c45d57020e0ee6eed71
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2015-11-04 19:27:54 +0300

    Add new Java 8 annotations.
    Change-Id: I9e98504f886d96e42c4160dcb7506e3279d36a5b

---
+++ user/super/com/google/gwt/emul/java/lang/annotation/Repeatable.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package java.lang.annotation;
+
+/**
+ * See <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/Repeatable.html">
+ * the official Java API doc</a> for details.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface Repeatable {
+  Class<? extends Annotation> value();
+}

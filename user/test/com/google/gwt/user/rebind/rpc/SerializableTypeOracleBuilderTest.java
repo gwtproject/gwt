0c97605a8dedeb64a50dcc0b38b7785e9b7d6e66
Rene Hangstrup Møller <rhmoller@gmail.com>
2015-11-16 13:14:54 -0800

    Fix problem where pending instantiable types were erronerously marked not instantiable
    Bug-Link: https://github.com/gwtproject/gwt/issues/7247
    Change-Id: I2660762e490fc04e1c3c5f3f78652b59b01262be

--- user/test/com/google/gwt/user/rebind/rpc/SerializableTypeOracleBuilderTest.java
+++ user/test/com/google/gwt/user/rebind/rpc/SerializableTypeOracleBuilderTest.java
@@ -2422,6 +2422,99 @@ public class SerializableTypeOracleBuilderTest extends TestCase {
     assertNotInstantiableOrFieldSerializable(so, a.getRawType());
   }
 
+  public void testPendingInstantiable() throws NotFoundException, UnableToCompleteException {
+    // When running the GwtRandomBug test suite attached to https://github.com/gwtproject/gwt/issues/7247#issue-87058425
+    // computeTypeInstantiability() is called in the this order:
+    //     Level1, Root, Parameter, Level2, AnInterface
+    // Use LinkedHashSet to mimic this order
+    // Unfortunately this test case still visits them in this order:
+    //     Root, Level1, Level2, Parameter, AnInterface
+    // this may be why this test passes without the fixed SerializableTypeOracleBuilder
+    Set<Resource> resources = new LinkedHashSet<>();
+    addStandardClasses(resources);
+
+    {
+      StringBuilder code = new StringBuilder();
+      code.append("package com.stibo.gwtrandombug.client.components;\n");
+      code.append("import com.stibo.gwtrandombug.client.AnInterface;\n");
+      code.append("public class Level1 implements AnInterface {\n");
+      code.append("    private Level2 container;\n");
+      code.append("}");
+      resources.add(new StaticJavaResource("com.stibo.gwtrandombug.client.components.Level1", code));
+    }
+
+    {
+      StringBuilder code = new StringBuilder();
+      code.append("package com.stibo.gwtrandombug.client.components;\n");
+      code.append("import com.stibo.gwtrandombug.client.AnInterface;\n");
+      code.append("import com.google.gwt.user.client.rpc.IsSerializable;\n");
+      code.append("public class Root implements IsSerializable {\n");
+      code.append("    private Level1 three;\n");
+      code.append("    private AnInterface anInterface;\n");
+      code.append("}"
+      );
+      resources.add(new StaticJavaResource("com.stibo.gwtrandombug.client.components.Root", code));
+    }
+
+    {
+      StringBuilder code = new StringBuilder();
+      code.append("package com.stibo.gwtrandombug.client;\n");
+      code.append("import java.io.Serializable;\n");
+      code.append("public class Parameter<E extends Serializable> implements Serializable {\n");
+      code.append("    private E e;\n");
+      code.append("    public Parameter() {\n");
+      code.append("    }\n");
+      code.append("    public Parameter(E e) {\n");
+      code.append("        this.e = e;\n");
+      code.append("    }\n");
+      code.append("    public E getE() {\n");
+      code.append("        return e;\n");
+      code.append("    }\n");
+      code.append("}");
+      resources.add(new StaticJavaResource("com.stibo.gwtrandombug.client.Parameter", code));
+    }
+
+    {
+      StringBuilder code = new StringBuilder();
+      code.append("package com.stibo.gwtrandombug.client.components;\n");
+      code.append("import com.google.gwt.user.client.rpc.IsSerializable;\n");
+      code.append("import com.stibo.gwtrandombug.client.AnInterface;\n");
+      code.append("import com.stibo.gwtrandombug.client.Parameter;\n");
+      code.append("public class Level2 implements IsSerializable {\n");
+      code.append("    private Parameter parameter;\n");
+      code.append("    private AnInterface anInterface;\n");
+      code.append("}");
+      resources.add(new StaticJavaResource("com.stibo.gwtrandombug.client.components.Level2", code));
+    }
+
+    {
+      StringBuilder code = new StringBuilder();
+      code.append("package com.stibo.gwtrandombug.client;\n");
+      code.append("import com.google.gwt.user.client.rpc.IsSerializable;\n");
+      code.append("public interface AnInterface extends IsSerializable {\n");
+      code.append("}");
+      resources.add(new StaticJavaResource("com.stibo.gwtrandombug.client.AnInterface", code));
+    }
+
+    TreeLogger logger = createLogger();
+    TypeOracle to = TypeOracleTestingUtils.buildTypeOracle(logger, resources);
+    SerializableTypeOracleBuilder sob = createSerializableTypeOracleBuilder(logger, to);
+
+    JClassType level1 = to.getType("com.stibo.gwtrandombug.client.components.Level1");
+    JClassType root = to.getType("com.stibo.gwtrandombug.client.components.Root");
+    JClassType parameter = to.getType("com.stibo.gwtrandombug.client.Parameter");
+    JClassType level2 = to.getType("com.stibo.gwtrandombug.client.components.Level2");
+
+    sob.addRootType(logger, root);
+    SerializableTypeOracle so = sob.build(logger);
+
+    assertInstantiable(so, root);
+    assertInstantiable(so, level1);
+    assertInstantiable(so, level2);
+
+    assertSerializableTypes(so, level1, root, parameter, level2);
+  }
+
   /**
    * Checks that a type is serializable when searching from the given roots.
    * Also, check that the root order doesn't matter.

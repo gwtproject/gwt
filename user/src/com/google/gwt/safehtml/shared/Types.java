238ee8add858c4cb085c14789de8b00c7939efde
Julian Bangert <bangert@google.com>
2015-11-18 14:44:49 -0800

    Adding SafeHTML annotations to gwt-user-bare.
    Change-Id: I8cb4b112571a4f58f4439cea5c74a4e3decb35c5
    Review-Link: https://gwt-review.googlesource.com/#/c/13991/

---
+++ user/src/com/google/gwt/safehtml/shared/Types.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.safehtml.shared;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+//import com.google.errorprone.annotations.LatticeType;
+//import com.google.errorprone.annotations.LatticeType.AllowCast;
+//import com.google.errorprone.annotations.TypeLattice;
+//import com.google.errorprone.annotations.TypeLattice.MaturityLevel;
+//import com.google.errorprone.annotations.TypeLattice.SeverityLevel;
+//import com.google.errorprone.annotations.TypeLattice.Suppressibility;
+
+//TODO: Move this to its own package once I figure out the build system
+import java.lang.annotation.Target;
+
+/**
+ * Annotations to mark properly escaped HTML.
+ */
+public class Types {
+  /**
+   * A type annotation that represents values that are safe to use in a HTML context.
+   *
+   * The annotated type {@code @Html String} and the type {@code SafeHtml} are semantically 
+   * equivalent.
+   */
+  @Target({PARAMETER, METHOD, FIELD, LOCAL_VARIABLE})
+  public @interface IsSafeHtml {
+  }
+}

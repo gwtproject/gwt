acebf25f06c9e5f76be8efbec34039046ec22dde
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2016-04-04 11:08:57 +0300

    Optimize out cast path in System.arraycopy when type checks are disabled.
    Change-Id: I521685fc22ca5d2a8d96eac6e616e511a8abc20f

--- user/super/com/google/gwt/emul/javaemul/internal/InternalPreconditions.java
+++ user/super/com/google/gwt/emul/javaemul/internal/InternalPreconditions.java
@@ -49,6 +49,10 @@ public final class InternalPreconditions {
     }
   }
 
+  public static boolean isArrayTypeChecked() {
+    return TYPE_CHECK || CHECKED_MODE;
+  }
+
   /**
    * Ensures the truth of an expression that verifies array type.
    */

b9db9f6caabca5e95652611dfc20a1818e4035f3
Michele Spagnuolo <mikispag@google.com>
2016-05-05 20:52:34 +0200

    Make GWT SuperDevMode Javascript file dev_mode_on.js CSP-friendly by propagating nonces to dynamically created scripts and avoid using javascript: URIs, normally blocked by CSP.
    Change-Id: I04696c5084cf70e70a0b902de5f54f183bfebc78

--- dev/codeserver/java/com/google/gwt/dev/codeserver/dev_mode_on.js
+++ dev/codeserver/java/com/google/gwt/dev/codeserver/dev_mode_on.js
@@ -29,6 +29,13 @@
  * parameters on window and include this script. That way, users don't have
  * to delete and replace their bookmarklets when we change this code.
  */
+
+window.nonce = "";
+var noncedScript = document.querySelector('script[nonce]');
+if (noncedScript) {
+  window.nonce = noncedScript.getAttribute('nonce');
+}
+
 (function() {
   var $doc = document;
 
@@ -115,27 +122,30 @@
     return dialog;
   }
 
-  function makeBookmarklet(name, javascript) {
+  function makeBookmarklet(name, codeserver_url, module_name) {
     var result = makeTextElt('a', '12pt', name);
     result.style.fontFamily = 'sans';
     result.style.textDecoration = 'none';
     result.style.background = '#ddd';
     result.style.border = '2px outset #ddd';
     result.style.padding = '3pt';
-    result.setAttribute('href', 'javascript:' + encodeURIComponent(javascript));
+    result.setAttribute('href', '#');
     result.title = 'Tip: drag this button to the bookmark bar';
+    result.onclick = function() {
+      window.__gwt_bookmarklet_params = {
+        server_url: codeserver_url,
+        module_name: module_name
+      };
+      var s = document.createElement('script');
+      s.src = codeserver_url + 'dev_mode_on.js';
+      s.setAttribute('nonce', window.nonce);
+      document.getElementsByTagName('head')[0].appendChild(s);
+    };
     return result;
   }
 
   function makeCompileBookmarklet(codeserver_url, module_name) {
-    var bookmarklets_js = codeserver_url + 'dev_mode_on.js';
-    var javascript = '{ window.__gwt_bookmarklet_params = {'
-        + 'server_url:\'' + codeserver_url + '\','
-        + 'module_name:\'' + module_name + '\'};'
-        + ' var s = document.createElement(\'script\');'
-        + ' s.src = \'' + bookmarklets_js + '\';'
-        + ' void(document.getElementsByTagName(\'head\')[0].appendChild(s));}';
-    return makeBookmarklet('Compile', javascript);
+    return makeBookmarklet('Compile', codeserver_url, module_name);
   }
 
   /**

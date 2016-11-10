7fab53c07f20ba86af12e3e64a06fe3b9d4ac11b
Roberto Lublinerman <rluble@gmail.com>
2016-11-10 00:31:31 -0800

    Fix bad rewriting of default methods.
    ReplaceDefenderMethodRefernces rewrites default methods into static
    methods and rerouting all the explicit callsites. The callsite rewrite
    was incorrectly assuming that calls would always have JThisRef as a
    qualifier, which is true initially but not in the midst of the
    rewriting process.
    
    A second issue was the rewriting of callsites might have been
    incomplete as new methods are created by this process and might or
    might not be themselves processed to rewrite default method dispatch.
    
    Bug: #9453
    Bug-Link: https://github.com/gwtproject/gwt/issues/9453
    Change-Id: I4ee3b340ce780cd0458920c9951b442fc25f1523

--- dev/core/src/com/google/gwt/dev/jjs/impl/ReplaceDefenderMethodReferences.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/ReplaceDefenderMethodReferences.java
@@ -30,35 +30,42 @@ import com.google.gwt.dev.jjs.ast.JThisRef;
  * References to I.super.m() are replaced by creating a static version of this method on the
  * interface, and then delegating to it instead.
  */
-public class ReplaceDefenderMethodReferences extends JModVisitor {
+public class ReplaceDefenderMethodReferences {
 
-  private final MakeCallsStatic.CreateStaticImplsVisitor staticImplCreator;
-  private JProgram program;
+  public static void exec(final JProgram program) {
+    final MakeCallsStatic.CreateStaticImplsVisitor staticImplCreator =
+        new MakeCallsStatic.CreateStaticImplsVisitor(program);
+    // 1. create the static implementations.
+    // NOTE: the process needs to be done in two steps because the creation of static implementation
+    // might introduce call sites in types that were already processed and hence would not be
+    // rewritten (bug #9453).
+    new JModVisitor() {
+      @Override
+      public boolean visit(JMethod x, Context ctx) {
+        if (x.isDefaultMethod()) {
+          staticImplCreator.getOrCreateStaticImpl(program, x);
+        }
+        return false;
+      }
+    }.accept(program);
 
-  public static void exec(JProgram program) {
-    ReplaceDefenderMethodReferences visitor =
-        new ReplaceDefenderMethodReferences(program);
-    visitor.accept(program);
-  }
+    // 2. Rewrite call sites.
+    new JModVisitor() {
+      @Override
+      public void endVisit(JMethodCall x, Context ctx) {
+        JMethod targetMethod = x.getTarget();
+        if (targetMethod.isDefaultMethod() && x.isStaticDispatchOnly()) {
+          assert x.getInstance() != null;
 
-  private ReplaceDefenderMethodReferences(JProgram program) {
-    this.program = program;
-    this.staticImplCreator = new MakeCallsStatic.CreateStaticImplsVisitor(program);
-  }
-
-  @Override
-  public void endVisit(JMethodCall x, Context ctx) {
-    JMethod targetMethod = x.getTarget();
-    if (targetMethod.isDefaultMethod() && x.isStaticDispatchOnly()) {
-      assert x.getInstance() instanceof JThisRef;
-
-      JMethod staticMethod = staticImplCreator.getOrCreateStaticImpl(program, targetMethod);
-      // Cannot use setStaticDispatchOnly() here because interfaces don't have prototypes
-      JMethodCall callStaticMethod = new JMethodCall(x.getSourceInfo(), null, staticMethod);
-      // add 'this' as first parameter
-      callStaticMethod.addArg(new JThisRef(x.getSourceInfo(), targetMethod.getEnclosingType()));
-      callStaticMethod.addArgs(x.getArgs());
-      ctx.replaceMe(callStaticMethod);
-    }
+          JMethod staticMethod = program.getStaticImpl(targetMethod);
+          // Cannot use setStaticDispatchOnly() here because interfaces don't have prototypes
+          JMethodCall callStaticMethod = new JMethodCall(x.getSourceInfo(), null, staticMethod);
+          // add 'this' as first parameter
+          callStaticMethod.addArg(x.getInstance());
+          callStaticMethod.addArgs(x.getArgs());
+          ctx.replaceMe(callStaticMethod);
+        }
+      }
+    }.accept(program);
   }
 }

6b0e77ff442b0b1eb8e8b2e1e3e9be34bd0e21f1
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2016-12-05 12:00:06 +0200

    Small optimization for Objects.deepEquals.
    Change-Id: I7e1cdaae70f713af6071bb81f410fc25f5239864

--- user/super/com/google/gwt/emul/java/util/Objects.java
+++ user/super/com/google/gwt/emul/java/util/Objects.java
@@ -37,9 +37,6 @@ public final class Objects {
     if (a == null || b == null) {
       return false;
     }
-    if (a.equals(b)) {
-      return true;
-    }
 
     // We have to test and see if these are two arrays of the same type,
     // then see what types of arrays they are and dispatch to the
@@ -48,7 +45,7 @@ public final class Objects {
     Class<?> class1 = a.getClass();
     Class<?> class2 = b.getClass();
     if (!class1.isArray() || !class1.equals(class2)) {
-      return false;
+      return a.equals(b);
     }
 
     if (a instanceof Object[]) {
@@ -79,7 +76,8 @@ public final class Objects {
       return Arrays.equals((double[]) a, (double[]) b);
     }
 
-    return true;
+    // it should never go this path
+    return false;
   }
 
   public static boolean equals(Object a, Object b) {

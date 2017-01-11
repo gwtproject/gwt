3032483a3d5aac819e7fbe362b02a0326567b95d
Graham Rogers <grahamrogers@google.com>
2017-01-11 19:09:11 +0000

    Add SafeUri overloads for Style.setBackgroundImage.
    The @IsSafeUri String overload cannot be used as the string actually
    needs to be of the form "url('http://example.com/img.png')" which is
    impossible to pass as a @IsSafeUri parameter.
    
    Change-Id: I7e868bb64d51cfea12b3701b0ba33424f5566f37
    Review-Link: https://gwt-review.googlesource.com/#/c/17560/

--- user/src/com/google/gwt/dom/client/Style.java
+++ user/src/com/google/gwt/dom/client/Style.java
@@ -16,7 +16,12 @@
 package com.google.gwt.dom.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.safehtml.shared.annotations.IsSafeUri;
+import com.google.gwt.safehtml.shared.SafeUri;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 41af0ee9a8fe390371aa633cde7fe5b9a0c9d60d
+import java.util.Collection;
 
 /**
  * Provides programmatic access to properties of the style object.
@@ -1877,8 +1882,38 @@ public class Style extends JavaScriptObject {
 
   /**
    * Set the background-image css property.
+   *
+   * <p>Note that this method is not safe for setting urls as the background-image, use
+   * {@link #setBackgroundImage(SafeUri)} or {@link #setBackgroundImage(Collection)} instead.
    */
-  public final void setBackgroundImage(@IsSafeUri String value) {
+  public final void setBackgroundImage(String value) {
+    setProperty(STYLE_BACKGROUND_IMAGE, value);
+  }
+
+  /**
+   * Set the background-image css property to a list of urls or "none" if no SafeUris are provided.
+   */
+  public final void setBackgroundImage(SafeUri uri) {
+    setProperty(STYLE_BACKGROUND_IMAGE, "url(" + uri.asString() + ")");
+  }
+
+  /**
+   * Set the background-image css property to a list of urls or "none" if no SafeUris are provided.
+   */
+  public final void setBackgroundImage(Collection<? extends SafeUri> uris) {
+    String value;
+    if (uris.isEmpty()) {
+      value = "none";
+    } else {
+      StringBuilder valueBuilder = new StringBuilder();
+      for (SafeUri uri : uris) {
+        if (valueBuilder.length() > 0) {
+          valueBuilder.append(", ");
+        }
+        valueBuilder.append("url(").append(uri.asString()).append(")");
+      }
+      value = valueBuilder.toString();
+    }
     setProperty(STYLE_BACKGROUND_IMAGE, value);
   }
 
@@ -2274,3 +2309,4 @@ public class Style extends JavaScriptObject {
     this[name] = value;
   }-*/;
 }
+

2aa2ad801856c5b1dd149c3893594849c446bb79
Daniel Kurka <dankurka@google.com>
2017-07-07 13:19:26 -0700

    Forward window.onerror into the UncaughtExceptionHandler.
    This change allows GWT to trap window.onerror and forward
    errors to the uncaught exception handler. This is necessary
    for libraries like elemental2 so that users do not constantly
    have to call the entry function manually.
    Right now this is still disabled and a subsequent change will
    enable it by default.
    
    For this change there are three modes:
     - Trap window on error at all times and forward
     - Only trap window.onerror if there is no current handler
     - Do not trap anything (old behavior)
    
    With this change some applications will see more calls
    to their UncaughtExceptionHandler since these would get
    ignored beforehand, but we strongly suggest that this is
    still the right way forward and users should fix those errors.
    For the transition period we added the above switches.
    
    Change-Id: I54f979c1887f44791f5bd2f32190b692f421fcce
    Review-Link: https://gwt-review.googlesource.com/#/c/18540/

---
+++ user/test/com/google/gwt/core/client/WindowOnErrorTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.core.client;
+
+import com.google.gwt.core.client.GWT.UncaughtExceptionHandler;
+import com.google.gwt.junit.DoNotRunWith;
+import com.google.gwt.junit.Platform;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/** Test for window.onerror reporting to {@link UncaughtExceptionHandler}. */
+public class WindowOnErrorTest extends GWTTestCase {
+
+  private int exceptionCount;
+
+  @Override
+  public String getModuleName() {
+    return "com.google.gwt.core.WindowOnError";
+  }
+
+  @Override
+  protected void reportUncaughtException(Throwable ex) {
+    // We need to distinguis here between the exceptions that we are deliberately creating
+    // and ones that should make the test fail.
+    // All our exceptions will contain "from_js" in their message
+    if (ex.getMessage().contains("from_js")) {
+      // Do not let the test fail
+      return;
+    }
+    super.reportUncaughtException(ex);
+  }
+
+  // Does not work in dev mode, since JNSI code for setting up window.onerror needs Throwable.of
+  // from super sourced code.
+  @DoNotRunWith({Platform.Devel})
+  public void testFailViaWindowOnError() {
+    delayTestFinish(3000);
+    exceptionCount = 0;
+
+    GWT.setUncaughtExceptionHandler(
+        new UncaughtExceptionHandler() {
+          @Override
+          public void onUncaughtException(Throwable e) {
+            exceptionCount++;
+            // Not using assert here since we would just be throwing again
+            // causing yet another window.onerror to happen.
+            // Instead we let the test timeout.
+            if (e.getMessage().contains("from_js")) {
+              delayFinish();
+            }
+          }
+        });
+    throwInNonEntryMethod();
+  }
+
+  private native void delayFinish() /*-{
+    var that = this;
+    $wnd.setTimeout(function() {
+      that.@WindowOnErrorTest::maybeFinish()();
+    }, 1000);
+  }-*/;
+
+  private void maybeFinish() {
+    if (exceptionCount == 1) {
+      finishTest();
+      return;
+    }
+    // We do not throw any exception here since these would just trigger our
+    // uncaught exception handler again through window.onerror
+  }
+
+  private native void throwInNonEntryMethod() /*-{
+    $wnd.setTimeout(function() {
+      throw new Error("from_js");
+    }, 0);
+  }-*/;
+}

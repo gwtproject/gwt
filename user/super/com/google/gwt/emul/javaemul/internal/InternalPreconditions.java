0155e07759295e8b6aa1963a6b6f6fc2d3fe9ccf
Goktug Gokdogan <goktug@google.com>
2017-07-20 16:47:58 -0700

    Replace String.equals with == in InternalPreconditions.
    Due to cycle between java.lang.String and InternalPreconditions
    it requires more parsses to turn precidition flags into static
    constants.
    This also prevents equals to be fully optimized as it results in a
    checken-egg problem in J2CL (equals needs checkNotNull to be removed
    to be evaluated but checkNotNull cannot be removed before the equals
    calls in preconditions evaluated).
    GWT doesn't hit this problems because String.equals is special cased
    for optimization purposes.
    
    Change-Id: Ib32bb141f98477cde7b8d56689a86829e1cf0a94

--- user/super/com/google/gwt/emul/javaemul/internal/InternalPreconditions.java
+++ user/super/com/google/gwt/emul/javaemul/internal/InternalPreconditions.java
@@ -78,13 +78,13 @@ public final class InternalPreconditions {
 
   // NORMAL
   private static final boolean LEVEL_NORMAL_OR_HIGHER =
-      getProperty("jre.checks.checkLevel").equals("NORMAL");
+      getProperty("jre.checks.checkLevel") == "NORMAL";
   // NORMAL or OPTIMIZED
   private static final boolean LEVEL_OPT_OR_HIGHER =
-      getProperty("jre.checks.checkLevel").equals("OPTIMIZED") || LEVEL_NORMAL_OR_HIGHER;
+      getProperty("jre.checks.checkLevel") == "OPTIMIZED" || LEVEL_NORMAL_OR_HIGHER;
   // NORMAL or OPTIMIZED or MINIMAL
   private static final boolean LEVEL_MINIMAL_OR_HIGHER =
-      getProperty("jre.checks.checkLevel").equals("MINIMAL") || LEVEL_OPT_OR_HIGHER;
+      getProperty("jre.checks.checkLevel") == "MINIMAL" || LEVEL_OPT_OR_HIGHER;
 
   static {
     if (!LEVEL_MINIMAL_OR_HIGHER) {
@@ -93,15 +93,15 @@ public final class InternalPreconditions {
   }
 
   private static final boolean IS_TYPE_CHECKED =
-      (CHECK_TYPE.equals("AUTO") && LEVEL_OPT_OR_HIGHER) || CHECK_TYPE.equals("ENABLED");
+      CHECK_TYPE == "AUTO" && LEVEL_OPT_OR_HIGHER || CHECK_TYPE == "ENABLED";
   private static final boolean IS_BOUNDS_CHECKED =
-      (CHECK_BOUNDS.equals("AUTO") && LEVEL_NORMAL_OR_HIGHER) || CHECK_BOUNDS.equals("ENABLED");
+      CHECK_BOUNDS == "AUTO" && LEVEL_NORMAL_OR_HIGHER || CHECK_BOUNDS == "ENABLED";
   private static final boolean IS_API_CHECKED =
-      (CHECK_API.equals("AUTO") && LEVEL_NORMAL_OR_HIGHER) || CHECK_API.equals("ENABLED");
+      CHECK_API == "AUTO" && LEVEL_NORMAL_OR_HIGHER || CHECK_API == "ENABLED";
   private static final boolean IS_NUMERIC_CHECKED =
-      (CHECK_NUMERIC.equals("AUTO") && LEVEL_NORMAL_OR_HIGHER) || CHECK_NUMERIC.equals("ENABLED");
+      CHECK_NUMERIC == "AUTO" && LEVEL_NORMAL_OR_HIGHER || CHECK_NUMERIC == "ENABLED";
 
-  private static final boolean IS_ASSERTED = getProperty("jre.checkedMode").equals("ENABLED");
+  private static final boolean IS_ASSERTED = getProperty("jre.checkedMode") == "ENABLED";
 
   /**
    * This method reports if the code is compiled with type checks.

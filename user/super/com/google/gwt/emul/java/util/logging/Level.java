822f52dbc33bbc9c65d3cab4f581099dddbc4e43
Goktug Gokdogan <goktug@google.com>
2017-09-08 13:16:14 -0700

    Makes java.util.logging.Logger optimize faster.
    Patch reduces indirection on flag processing so they could
    get their final value sooner and static eval earlier in the
    optimization loop.
    
    Change-Id: I409e0211433375c0248d47458efbaab82c54f783

--- user/super/com/google/gwt/emul/java/util/logging/Level.java
+++ user/super/com/google/gwt/emul/java/util/logging/Level.java
@@ -82,8 +82,7 @@ public class Level implements Serializable {
   }
 
   public static Level parse(String name) {
-    Logger.assertLoggingValues();
-    if (System.getProperty("jre.logging.logLevel").equals("OFF")) {
+    if (System.getProperty("jre.logging.logLevel") == "OFF") {
       return null;
     }
 

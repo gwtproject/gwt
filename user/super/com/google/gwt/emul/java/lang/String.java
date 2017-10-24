1ff1323922c001afbe0174348b98a5cc9e79a5d1
Goktug Gokdogan <goktug@google.com>
2017-10-24 16:59:41 -0700

    Take shortcut for String.charAt when safe
    Change-Id: Ib5051391abb2c11d566db4f1ecbe0e3f56c098de

--- user/super/com/google/gwt/emul/java/lang/String.java
+++ user/super/com/google/gwt/emul/java/lang/String.java
@@ -27,16 +27,13 @@ import java.nio.charset.UnsupportedCharsetException;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.StringJoiner;
-
-import javax.annotation.Nonnull;
-
 import javaemul.internal.ArrayHelper;
 import javaemul.internal.EmulatedCharset;
 import javaemul.internal.HashCodes;
 import javaemul.internal.JsUtils;
 import javaemul.internal.NativeRegExp;
 import javaemul.internal.annotations.DoNotInline;
-
+import javax.annotation.Nonnull;
 import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
@@ -205,9 +202,8 @@ public final class String implements Comparable<String>, CharSequence,
   private static String translateReplaceString(String replaceStr) {
     int pos = 0;
     while (0 <= (pos = replaceStr.indexOf("\\", pos))) {
-      if (replaceStr.charAt(pos + 1) == '$') {
-        replaceStr = replaceStr.substring(0, pos) + "$"
-            + replaceStr.substring(++pos);
+      if (replaceStr.asNativeString().charCodeAt(pos + 1) == '$') {
+        replaceStr = replaceStr.substring(0, pos) + "$" + replaceStr.substring(++pos);
       } else {
         replaceStr = replaceStr.substring(0, pos) + replaceStr.substring(++pos);
       }
@@ -451,7 +447,7 @@ public final class String implements Comparable<String>, CharSequence,
 
   private void getChars0(int srcBegin, int srcEnd, char[] dst, int dstBegin) {
     while (srcBegin < srcEnd) {
-      dst[dstBegin++] = charAt(srcBegin++);
+      dst[dstBegin++] = asNativeString().charCodeAt(srcBegin++);
     }
   }
 
@@ -741,11 +737,11 @@ public final class String implements Comparable<String>, CharSequence,
   public String trim() {
     int length = length();
     int start = 0;
-    while (start < length && charAt(start) <= ' ') {
+    while (start < length && asNativeString().charCodeAt(start) <= ' ') {
       start++;
     }
     int end = length;
-    while (end > start && charAt(end - 1) <= ' ') {
+    while (end > start && asNativeString().charCodeAt(end - 1) <= ' ') {
       end--;
     }
     return start > 0 || end < length ? substring(start, end) : this;

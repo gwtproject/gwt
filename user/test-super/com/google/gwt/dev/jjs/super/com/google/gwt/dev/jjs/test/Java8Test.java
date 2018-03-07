aa3b2a8fa8985a672a9ea936764e7c9851da375a
Roberto Lublinerman <rluble@google.com>
2018-03-07 09:26:10 -0800

    Fix for bad method resolution in GwtAstBuilder.
    Bug: #9598
    Bug-Link: https://github.com/gwtproject/gwt/issues/9598
    Change-Id: Ie6a5b485eb524436d9f5fe4f7dc758d35fcc4d60

--- user/test-super/com/google/gwt/dev/jjs/super/com/google/gwt/dev/jjs/test/Java8Test.java
+++ user/test-super/com/google/gwt/dev/jjs/super/com/google/gwt/dev/jjs/test/Java8Test.java
@@ -27,6 +27,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.IntFunction;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -2037,5 +2038,25 @@ public class Java8Test extends GWTTestCase {
       }
     };
     assertEquals("hello", helloSupplier.get());
+  }
+
+  interface Selector extends Predicate<String> {
+    @Override
+    boolean test(String object);
+
+    default Selector trueSelector() {
+      // Unused variable that creates a lambda with a bridge for the method test. The bug #9598
+      // was caused by GwtAstBuilder associating the bridge method Lambda.test(Object) on the
+      // lambda below to the method Predicate.test(Object), causing the method resolution in the
+      // code that refers to the Predicate.test(Object) in the test below to refer to
+      // Lambda.test(Object) which is the wrong method.
+      return receiver -> true;
+    }
+  }
+
+  // Regression tests for #9598
+  public void testImproperMethodResolution() {
+    Predicate p = o -> true;
+    assertTrue(p.test(null));
   }
 }

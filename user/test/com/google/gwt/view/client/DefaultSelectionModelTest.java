316596bb4634c96e02fc94cf97ed6c33d3308f21
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2018-07-23 23:11:46 +0200

    Make some of the c.g.g.view.client.*SelectionModelTest tests async.
    SelectionModel fires selection events using a finally command and
    some tests using SelectionChangeEvent.Handler were not async.
    In addition these tests had an implementation that let the test fail
    (but ANT never noticed because tests were sync).
    
    This CL makes sure all tests using SelectionChangeEvent.Handler are
    async and updates implementation slightly to correclty assert event
    behavior.
    
    Bug: #9622
    Bug-Link: https://github.com/gwtproject/gwt/issues/9622
    Change-Id: I66ba1170d2a1335d3ad921346017b5b5bc1accf8

--- user/test/com/google/gwt/view/client/DefaultSelectionModelTest.java
+++ user/test/com/google/gwt/view/client/DefaultSelectionModelTest.java
@@ -16,6 +16,9 @@
 package com.google.gwt.view.client;
 
 
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.Timer;
+
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -65,18 +68,32 @@ public class DefaultSelectionModelTest extends AbstractSelectionModelTest {
   }
 
   public void testNoDuplicateChangeEvent() {
-    DefaultSelectionModel<String> model = createSelectionModel(null);
-    SelectionChangeEvent.Handler handler = new SelectionChangeEvent.Handler() {
-      @Override
-      public void onSelectionChange(SelectionChangeEvent event) {
-        fail();
-      }
-    };
+    delayTestFinish(2000);
+    final DefaultSelectionModel<String> model = createSelectionModel(null);
+    final MockSelectionChangeHandler handler = new AssertOneSelectionChangeEventOnlyHandler();
 
-    model.setSelected("selected999", false);
     model.addSelectionChangeHandler(handler);
-    model.setSelected("selected999", false); // Should not fire change event
-    model.setSelected("selected999", false); // Should not fire change event
+    model.setSelected("selected999", false);
+    // selection events fire at the end of current event loop (finally command)
+    handler.assertEventFired(false);
+
+    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+      @Override
+      public void execute() {
+        handler.assertEventFired(true);
+        // No further selection events should be fired
+        model.addSelectionChangeHandler(new FailingSelectionChangeEventHandler());
+        model.setSelected("selected999", false);
+        model.setSelected("selected999", false);
+      }
+    });
+
+    new Timer() {
+      @Override
+      public void run() {
+        finishTest();
+      }
+    }.schedule(1000);
   }
 
   public void testSetSelectedDefault() {

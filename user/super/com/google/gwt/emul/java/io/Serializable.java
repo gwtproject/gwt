554e25d2b21346df51b45815c2bc0cacefb465a9
Roberto Lublinerman <rluble@google.com>
2018-10-15 09:01:03 -0700

    Adds helpers to Serializable for J2CL.
    Change-Id: I6931a2ef62e8e7c0939bb7da3f31f1655fb5067f

--- user/super/com/google/gwt/emul/java/io/Serializable.java
+++ user/super/com/google/gwt/emul/java/io/Serializable.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,10 +15,28 @@
  */
 package java.io;
 
+import javaemul.internal.JsUtils;
+import jsinterop.annotations.JsMethod;
+
 /**
  * Provided for interoperability; RPC treats this interface synonymously with
  * {@link com.google.gwt.user.client.rpc.IsSerializable IsSerializable}.
  * The Java serialization protocol is explicitly not supported.
  */
 public interface Serializable {
+  // CHECKSTYLE_OFF: Utility methods.
+  @JsMethod
+  static boolean $isInstance(HasSerializableTypeMarker instance) {
+    if (instance == null) {
+      return false;
+    }
+
+    String type = JsUtils.typeOf(instance);
+    return type.equals("boolean")
+        || type.equals("number")
+        || type.equals("string")
+        || instance.getTypeMarker()
+        || instance.getClass().isArray();
+  }
+  // CHECKSTYLE_ON: end utility methods
 }

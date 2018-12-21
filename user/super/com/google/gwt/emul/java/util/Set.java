3bf432cc43eea272836e3b9f6c60769fa20144f9
Colin Alworth <niloc132@gmail.com>
2018-12-21 15:44:42 -0600

    Updated collections to support changes made in Java 9
    Bug: #9547
    Bug-Link: https://github.com/gwtproject/gwt/issues/9547
    Change-Id: I0ce4a6fa92806a41661056b53db3f27701506f28

--- user/super/com/google/gwt/emul/java/util/Set.java
+++ user/super/com/google/gwt/emul/java/util/Set.java
@@ -15,6 +15,9 @@
  */
 package java.util;
 
+import static javaemul.internal.InternalPreconditions.checkArgument;
+import static javaemul.internal.InternalPreconditions.checkNotNull;
+
 import jsinterop.annotations.JsIgnore;
 import jsinterop.annotations.JsType;
 
@@ -26,6 +29,196 @@ import jsinterop.annotations.JsType;
  */
 @JsType
 public interface Set<E> extends Collection<E> {
+  @JsIgnore
+  static <E> Set<E> of() {
+    return Collections.emptySet();
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(E e1) {
+    return Collections.singleton(checkNotNull(e1));
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5,
+      E e6
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    checkArgument(set.add(checkNotNull(e6)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5,
+      E e6,
+      E e7
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    checkArgument(set.add(checkNotNull(e6)));
+    checkArgument(set.add(checkNotNull(e7)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5,
+      E e6,
+      E e7,
+      E e8
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    checkArgument(set.add(checkNotNull(e6)));
+    checkArgument(set.add(checkNotNull(e7)));
+    checkArgument(set.add(checkNotNull(e8)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5,
+      E e6,
+      E e7,
+      E e8,
+      E e9
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    checkArgument(set.add(checkNotNull(e6)));
+    checkArgument(set.add(checkNotNull(e7)));
+    checkArgument(set.add(checkNotNull(e8)));
+    checkArgument(set.add(checkNotNull(e9)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(
+      E e1,
+      E e2,
+      E e3,
+      E e4,
+      E e5,
+      E e6,
+      E e7,
+      E e8,
+      E e9,
+      E e10
+  ) {
+    Set<E> set = new HashSet<E>(2);
+    set.add(checkNotNull(e1));
+    checkArgument(set.add(checkNotNull(e2)));
+    checkArgument(set.add(checkNotNull(e3)));
+    checkArgument(set.add(checkNotNull(e4)));
+    checkArgument(set.add(checkNotNull(e5)));
+    checkArgument(set.add(checkNotNull(e6)));
+    checkArgument(set.add(checkNotNull(e7)));
+    checkArgument(set.add(checkNotNull(e8)));
+    checkArgument(set.add(checkNotNull(e9)));
+    checkArgument(set.add(checkNotNull(e10)));
+    return Collections.unmodifiableSet(set);
+  }
+
+  @JsIgnore
+  static <E> Set<E> of(E... elements) {
+    HashSet<E> set = new HashSet<>();
+    for (int i = 0; i < elements.length; i++) {
+      checkArgument(set.add(checkNotNull(elements[i])));
+    }
+    return Collections.unmodifiableSet(set);
+  }
+
   @JsIgnore
   @Override
   default Spliterator<E> spliterator() {

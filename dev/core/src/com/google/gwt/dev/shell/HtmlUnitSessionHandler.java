a1eb60255e94c710ebb0b81f28287896f601ae77
Colin Alworth <niloc132@gmail.com>
2020-06-02 21:09:04 -0500

    Upgrade HtmlUnit to 2.55.0 and Jetty to 9.4.44
    Several old tests previously not able to run in HtmlUnit are now
    enabled, and two tests are disabled, linked to the appropriate bug
    report.
    
    The jaspi security/authentication module has been disabled in the dev
    mode server. It is no longer included in the base jetty install due to
    licensing reasons, and is disabled by default here for the same reason.
    
    Many small changes were necessary across various parts of testing and
    dev mode infrastructure:
     * HtmlUnit has introduced a proxy for window and document, legacy dev
       mode needs to treat those objects as if they were the real instance.
     * HtmlUnit has additional protection to avoid letting a page interact
       with a real Java object. This had to be relaxed in order to let
       legacy dev mode tests continue to work.
     * The custom classloader used to run a webapp within dev mode now
       overrides loadClass instead of findClass, to correctly delegate to
       Jetty's own implementation to handle startup issues. Additionally,
       isSystemClass and isServerClass are relocated and deprecated.
    
    Bug: #9720
    Bug-Link: https://github.com/gwtproject/gwt/issues/9720
    Co-authored-by: Mihai Stanciu <mihaisdm@gmail.com>
    Change-Id: Ie14cb33cb0edea57c121de943f2964a8cf0540ee

--- dev/core/src/com/google/gwt/dev/shell/HtmlUnitSessionHandler.java
+++ dev/core/src/com/google/gwt/dev/shell/HtmlUnitSessionHandler.java
@@ -15,6 +15,7 @@
  */
 package com.google.gwt.dev.shell;
 
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.dev.shell.BrowserChannel.JavaObjectRef;
 import com.google.gwt.dev.shell.BrowserChannel.JsObjectRef;
@@ -108,9 +109,11 @@ public class HtmlUnitSessionHandler extends SessionHandlerClient {
   private final ToStringMethod toStringMethod = new ToStringMethod();
 
   private final Window window;
+  private final WebClient webClient;
 
-  HtmlUnitSessionHandler(Window window, JavaScriptEngine jsEngine) {
+  HtmlUnitSessionHandler(Window window, JavaScriptEngine jsEngine, WebClient webClient) {
     this.window = window;
+    this.webClient = webClient;
     logger.setMaxDetail(TreeLogger.ERROR);
     this.jsEngine = jsEngine;
     htmlPage = (HtmlPage) this.window.getWebWindow().getEnclosedPage();
@@ -161,7 +164,7 @@ public class HtmlUnitSessionHandler extends SessionHandlerClient {
   @Override
   public String getUserAgent() {
     return "HtmlUnit-"
-        + jsEngine.getWebClient().getBrowserVersion().getUserAgent();
+        + webClient.getBrowserVersion().getUserAgent();
   }
 
   @SuppressWarnings("unchecked")
@@ -286,6 +289,15 @@ public class HtmlUnitSessionHandler extends SessionHandlerClient {
       return returnVal;
     }
     if (value instanceof Scriptable) {
+      if (value instanceof SimpleScriptableProxy) {
+        // HtmlUnit will return proxies to java for the window/document objects,
+        // so that those objects can work after navigating away from the page.
+        // However, GWTTestCase operates inside a single page session, so we
+        // can unwrap these proxies to get the real instance. Without doing
+        // this, the refToJsObject mapping would indicate that an object might
+        // not equal itself
+        value = ((SimpleScriptableProxy<?>) value).getDelegee();
+      }
       if (value instanceof ScriptableObject) {
         /*
          * HACK: check for native types like NativeString. NativeString is

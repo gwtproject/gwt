1a39bb5f983786a876f94d32b7707376f29269d2
Gaurav Ghildiyal <gauravkg@google.com>
2021-08-13 03:03:15 +0530

    Add dummy classes in order to make the newer javax_validation compile.
    Context: We are upgrading the `javax_validation` version present in
    third party (cl/377574983). `javax_validation` is used by GWT and it's
    dependencies and many builds seem to be failing ([TAP Global
    Presubmit](https://test.corp.google.com/ui#id=OCL:377574983:BASE:389211742:1628302603988:fcb7a9a8))
    because GWT does not emulate:
    * `java.lang.reflect.Constructor<T>`
    * `java.lang.reflect.Method`
    * `java.time.Clock`
    
    Hence, I'm making these classes part of the GWT emulator. Since they
    aren't being used anywhere, empty classes seem to be working fine.
    
    Change-Id: Ic47f52d7762b82c35e04f52a2c135a7603bd3625

---
+++ user/super/com/google/gwt/emul/java/lang/reflect/Constructor.java
@@ -0,0 +1,7 @@
+package java.lang.reflect;
+
+/**
+ * * The Constructor class has not been fully implemented since it's not being used anywhere but its
+ * * presence is required to make the dependencies of javax_validation version 2.0.1.Final compile.
+ */
+public final class Constructor<T> {}

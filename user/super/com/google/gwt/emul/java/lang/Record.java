4df780e650827e82f6e950996900ccabf5f2da89
Colin Alworth <niloc132@gmail.com>
2021-10-30 16:01:07 -0500

    Cleaner tostring, and hashcode, equals, and refactored field accessors
    Change-Id: Icd5177fd72d8e16a04596f7abe1218ac392c243f

--- user/super/com/google/gwt/emul/java/lang/Record.java
+++ user/super/com/google/gwt/emul/java/lang/Record.java
@@ -11,30 +11,4 @@ public abstract class Record {
     public abstract boolean equals(Object other);
 
     public abstract String toString();
-
-
-    /**
-     * Internal and non-standard helper to generate a readable tostring as required by
-     * the Record spec.
-     *
-     * This method probably shouldn't exist, but be inlined into the generated toString
-     * itself.
-     *
-     * @param typeName the name of the type, may be obfuscated
-     * @param labeledComponents labeled values, already converted to a string
-     * @return a string with the typename and labeled componenents
-     */
-    protected static String __toString(String typeName, String... labeledComponents) {
-        StringBuilder sb = new StringBuilder(typeName);
-        sb.append(" { ");
-
-        for (int i = 0; i < labeledComponents.length; i++) {
-            if (i != 0) {
-                sb.append(", ");
-            }
-            sb.append(labeledComponents);
-        }
-
-        return sb.append(" }").toString();
-    }
 }

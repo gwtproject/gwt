d0369052ae0469d964aa4ef47fe0affb86cbdd41
Colin Alworth <niloc132@gmail.com>
2021-12-16 11:52:55 -0600

    Distinct relocation poms and script for gwtproject vs Google
    The old script, being already used in Google-managed CI, continues to
    deploy poms with com.google groupId. A new script,
    maven/push-gwt-ogg.sh, has the same contents as the old one used to
    have, and deploys the org.gwtproject poms.
    
    Change-Id: I0f582304faf9546ac37616a78bc7735f8beeba26

--- maven/push-gwtproject.sh
+++ maven/push-gwtproject.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
-# Pushes GWT poms with "com.google" groupIds to a local (the default) or remote maven repository.
-# These poms are relocation poms, and point to the new "org.gwtproject" poms, rather than being
-# deployed with any artifacts.
+# Pushes GWT artifacts to a local (the default) or remote maven repository
 # To push remote, set 2 env variables: GWT_MAVEN_REPO_URL and GWT_MAVEN_REPO_ID
 #
 # GWT_MAVEN_REPO_ID = a server id in your .m2/settings.xml with remote repo username and password
@@ -11,11 +9,11 @@
 #   GWT_MAVEN_REPO_URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
 #
 # Sonatype Google SNAPSHOTs repo (can only deploy SNAPSHOTs here, and they are immediately public)
-#   GWT_MAVEN_REPO_URL=https://oss.sonatype.org/content/repositories/google-snapshots/
+#   GWT_MAVEN_REPO_URL=https://oss.sonatype.org/content/repositories/snapshots/
 
 pushd $(dirname $0) >/dev/null 2>&1
 
-export pomDir=./google-poms
+export pomDir=./poms
 
 source lib-gwt.sh
 
@@ -29,6 +27,9 @@ repoId=${GWT_MAVEN_REPO_ID:=none}
 # use GWT_DIST_FILE to specify the default distribution file
 gwtTrunk=$(dirname $(pwd))
 gwtPathDefault=${GWT_DIST_FILE:=$(ls -t1 ${gwtTrunk}/build/dist/gwt-*.zip 2>/dev/null | head -1)}
+if [[ -f "$gwtPathDefault" ]]; then
+  gwtPathPrompt="($gwtPathDefault)"
+fi
 
 VERSION_REGEX='[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*-*.*'
 
@@ -40,6 +41,13 @@ read -e -p"GWT version for Maven (${gwtVersionDefault:-ex: HEAD-SNAPSHOT}): " gw
 gwtVersion=${gwtVersion:=$gwtVersionDefault}
 if test "$gwtVersion" != "HEAD-SNAPSHOT" && ! expr "$gwtVersion" : "$VERSION_REGEX" >/dev/null; then
   echo "Please enter a version of the form x.y.z or x.y.z-abc"
+  exit 1
+fi
+
+read -e -p"Path to GWT distro zip $gwtPathPrompt: " gwtPath
+gwtPath=${gwtPath:=$gwtPathDefault}
+if [[ ! -f  $gwtPath ]]; then
+  echo "ERROR: Cannot find file at \"$gwtPath\""
   exit 1
 fi
 
@@ -56,39 +64,9 @@ fi
 read -p"GPG passphrase for jar signing (may skip for local deployment): " gpgPassphrase
 gpgPassphrase=${gpgPassphrase:=$GWT_GPG_PASS}
 
-# A simplified maven-gwt is inlined here, since only poms are templated and deployed
-
-gwtMavenVersion="$gwtVersion"
-mavenRepoUrl="$repoUrl"
-mavenRepoId="$repoId"
-
-# Generate POMs with correct version
-for template in `find $pomDir -name pom-template.xml`
-do
-  dir=`dirname $template`
-  pushd $dir > /dev/null
-  sed -e "s|\${gwtVersion}|$gwtMavenVersion|g" pom-template.xml >pom.xml
-  popd > /dev/null
-done
-
-# push parent poms
-maven-deploy-file $mavenRepoUrl $mavenRepoId $pomDir/gwt/pom.xml $pomDir/gwt/pom.xml || die
-
-# push artifact relocation poms for gwt
-for i in dev user servlet codeserver
-do
-  gwtPomFile=$pomDir/gwt/gwt-$i/pom.xml
-  maven-deploy-file $mavenRepoUrl $mavenRepoId $gwtPomFile $gwtPomFile || die
-
-done
-
-# Deploy RequestFactory jars
-maven-deploy-file $mavenRepoUrl $mavenRepoId $pomDir/requestfactory/pom.xml $pomDir/requestfactory/pom.xml || die
-
-for i in client server apt
-do
-  rfPomFile=$pomDir/requestfactory/${i}/pom.xml
-  maven-deploy-file $mavenRepoUrl $mavenRepoId $rfPomFile $rfPomFile || die
-done
+maven-gwt "$gwtVersion" \
+          "$gwtPath" \
+          "$repoUrl" \
+          "$repoId"
 
 popd >/dev/null 2>&1

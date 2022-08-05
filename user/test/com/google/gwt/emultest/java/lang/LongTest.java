gwtproject/gwt

commit 7616ceb5fbfe693514116829aa7a5d33c22ef054

Fix broken Long octal/hex/binaryToString
Existing fast path that was using int was incorrect and wasn't
handling negative values correctly.

PiperOrigin-RevId: 373840498
Change-Id: I3eac012c271c2b9e6553453565c23d90789c41c0

user/super/com/google/gwt/emul/java/lang/Long.java
@@ -228,7 +228,9 @@ public final class Long extends Number implements Comparable<Long> {
 
   private static String toPowerOfTwoUnsignedString(long value, int shift) {
     final int radix = 1 << shift;
-    if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE) {
+
+    int highBits = LongUtils.getHighBits(value);
+    if (highBits == 0) {
       return Integer.toString((int) value, radix);
     }
 

user/test/com/google/gwt/emultest/java/lang/LongTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -87,7 +87,7 @@ public class LongTest extends GWTTestCase {
     assertEquals(63, Long.numberOfTrailingZeros(Long.MIN_VALUE));
     assertEquals(20, Long.numberOfTrailingZeros(-0x7ff00000L));
   }
- 
+
   public void testParse() {
     assertEquals(0L, Long.parseLong("0"));
     assertEquals(100000000000L, Long.parseLong("100000000000"));
@@ -251,6 +251,9 @@ public class LongTest extends GWTTestCase {
     assertEquals("10001111101101101111011110001100100000000", Long.toBinaryString(1234500000000L));
     assertEquals("1111111111111111111111101110000010010010000100001110011100000000",
         Long.toBinaryString(-1234500000000L));
+    assertEquals(
+        "1111111111111111111111111111111111111111111111111111111111111111",
+        Long.toBinaryString(-1));
   }
 
   public void testToHexString() {
@@ -258,6 +261,7 @@ public class LongTest extends GWTTestCase {
     assertEquals("12345", Long.toHexString(0x12345L));
     assertEquals("1234500000000", Long.toHexString(0x1234500000000L));
     assertEquals("fff1234500000000", Long.toHexString(0xFFF1234500000000L));
+    assertEquals("ffffffffffffffff", Long.toHexString(-1));
   }
 
   public void testToOctalString() {
@@ -265,6 +269,7 @@ public class LongTest extends GWTTestCase {
     assertEquals("77777777777", Long.toOctalString(077777777777L));
     assertEquals("1000000000000000000000", Long.toOctalString(Long.MIN_VALUE));
     assertEquals("777777777777777777777", Long.toOctalString(Long.MAX_VALUE));
+    assertEquals("1777777777777777777777", Long.toOctalString(-1));
   }
 
   public void testToString() {

commit 916d48dce86874e4ba90942427eb6e46da7ab374

Correctly write Long.toBinaryString as an unsigned value
A previous fix to negative longs incorrectly allowed some positive longs
that would be represented as negative ints to be printed as negative
binary values, instead of as unsigned values. This patch fixes that case
and adds tests to confirm that these edge cases are well represented.
This bug was introduced by 7616ceb5.

The binary, hex, and octal tests all fail before this change, the
toString test was just for completeness, but wasn't actually broken.

Fixes #9764

user/super/com/google/gwt/emul/java/lang/Long.java
@@ -237,7 +237,7 @@ public final class Long extends Number implements Comparable<Long> {
 
     int highBits = LongUtils.getHighBits(value);
     if (highBits == 0) {
-      return Integer.toString((int) value, radix);
+      return Integer.toUnsignedString((int) value, radix);
     }
 
     final int mask = radix - 1;

user/test/com/google/gwt/emultest/java/lang/LongTest.java
@@ -254,6 +254,14 @@ public class LongTest extends GWTTestCase {
     assertEquals(
         "1111111111111111111111111111111111111111111111111111111111111111",
         Long.toBinaryString(-1));
+
+    // Test int MAX+1 and int -1, as both are positive longs, but negative ints
+    assertEquals(
+        "10000000000000000000000000000000",
+        Long.toBinaryString(((long) Integer.MAX_VALUE) + 1));
+    assertEquals(
+        "11111111111111111111111111111111",
+        Long.toBinaryString(4294967295L));
   }
 
   public void testToHexString() {
@@ -262,6 +270,8 @@ public class LongTest extends GWTTestCase {
     assertEquals("1234500000000", Long.toHexString(0x1234500000000L));
     assertEquals("fff1234500000000", Long.toHexString(0xFFF1234500000000L));
     assertEquals("ffffffffffffffff", Long.toHexString(-1));
+    assertEquals("80000000", Long.toHexString(((long) Integer.MAX_VALUE) + 1));
+    assertEquals("ffffffff", Long.toHexString(4294967295L));
   }
 
   public void testToOctalString() {
@@ -270,6 +280,8 @@ public class LongTest extends GWTTestCase {
     assertEquals("1000000000000000000000", Long.toOctalString(Long.MIN_VALUE));
     assertEquals("777777777777777777777", Long.toOctalString(Long.MAX_VALUE));
     assertEquals("1777777777777777777777", Long.toOctalString(-1));
+    assertEquals("20000000000", Long.toOctalString(((long) Integer.MAX_VALUE) + 1));
+    assertEquals("37777777777", Long.toOctalString(4294967295L));
   }
 
   public void testToString() {
@@ -281,6 +293,8 @@ public class LongTest extends GWTTestCase {
     assertEquals("80765", Long.toString(80765L));
     assertEquals("-2147483648", Long.toString((long) Integer.MIN_VALUE));
     assertEquals("2147483647", Long.toString((long) Integer.MAX_VALUE));
+    assertEquals("2147483648", Long.toString(((long) Integer.MAX_VALUE) + 1));
+    assertEquals("4294967295", Long.toString((long) 4294967295L));
     assertEquals("-89000000005", Long.toString(-89000000005L));
     assertEquals("89000000005", Long.toString(89000000005L));
     assertEquals("-9223372036854775808", Long.toString(Long.MIN_VALUE));
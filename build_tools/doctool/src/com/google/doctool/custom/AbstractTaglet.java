95b2e2fef738a479d1a16579e1e551ee960c872f
Colin Alworth <niloc132@gmail.com>
2022-10-26 16:18:47 -0500

    Javadoc for style check

--- build_tools/doctool/src/com/google/doctool/custom/AbstractTaglet.java
+++ build_tools/doctool/src/com/google/doctool/custom/AbstractTaglet.java
@@ -10,6 +10,9 @@ import jdk.javadoc.doclet.Taglet;
 import javax.lang.model.element.Element;
 import javax.tools.Diagnostic;
 
+/**
+ * Abstract taglet baseclass to facilitate some basic operations.
+ */
 public abstract class AbstractTaglet implements Taglet {
     protected DocletEnvironment env;
 
@@ -18,6 +21,12 @@ public abstract class AbstractTaglet implements Taglet {
         this.env = env;
     }
 
+    /**
+     * Given a tag, returns the text within that tag.
+     *
+     * @param tag the tag to read the text from
+     * @return the body of the textnode within that tag
+     */
     protected String getText(DocTree tag) {
         return tag.accept(new DocTreeScanner<String, Void>() {
             @Override
@@ -27,6 +36,14 @@ public abstract class AbstractTaglet implements Taglet {
         }, null);
     }
 
+    /**
+     * Helper method to log a diagnostic message to the user about a particular element+doctree.
+     *
+     * @param kind the kind of message to log
+     * @param message the message text to write
+     * @param element the element that the message applies to
+     * @param docTree the doctree node that the message applies to
+     */
     protected void printMessage(Diagnostic.Kind kind, String message, Element element, DocTree docTree) {
         env.getDocTrees().printMessage(
                 kind,

9d4286e1cc54fbf043649c59b68853488cdb8792
Axel Uhl <axel.uhl@sap.com>
2023-01-31 11:49:06 +0100

    issue-9584: added missing newline at file end; fixed package name in Javadoc

--- user/src/com/google/gwt/user/server/rpc/EnumMapUtil.java
+++ user/src/com/google/gwt/user/server/rpc/EnumMapUtil.java
@@ -44,12 +44,12 @@ import java.io.ObjectStreamClass;
  * a {@link ByteArrayOutputStream} from which it is read again with a specialized
  * {@link ObjectInputStream}where the {@code resolveClass} method is overridden. Upon reading a
  * class descriptor for class {@link java.util.EnumMap}, instead of regularly resolving the class
- * descriptor a package-protected class {@link com.sap.sse.util.EnumMap} is returned which has a
+ * descriptor a package-protected class {@link com.google.gwt.user.server.rpc.EnumMap} is returned which has a
  * serial version UID equal to that of {@link java.util.EnumMap} and the same set of fields. By
  * having the same simple name (despite living in a different package) the {@link ObjectInputStream}
  * considers this class to be compatible to {@link java.util.EnumMap} and de-serializes the stream
- * contents into an instance of {@link com.sap.sse.util.EnumMap} which in turn offers a public
- * getter {@link com.sap.sse.util.EnumMap#getKeyType()} for the key type. Through this getter the
+ * contents into an instance of {@link com.google.gwt.user.server.rpc.EnumMap} which in turn offers a public
+ * getter {@link com.google.gwt.user.server.rpc.EnumMap#getKeyType()} for the key type. Through this getter the
  * key type is then determined and returned.
  *
  */

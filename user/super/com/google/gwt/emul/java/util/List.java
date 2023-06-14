c62594054b385a18a2731b2c1daff0df65ee4e88
Colin Alworth <colin@vertispan.com>
2023-06-14 10:35:34 -0500

    Avoid an extra array copy when handling short lists

--- user/super/com/google/gwt/emul/java/util/List.java
+++ user/super/com/google/gwt/emul/java/util/List.java
@@ -42,53 +42,67 @@ public interface List<E> extends Collection<E> {
 
   @JsIgnore
   static <E> List<E> of(E e1) {
-    return of((E[]) new Object[] {e1});
+    return __ofInternal((E[]) new Object[] {e1});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2) {
-    return of((E[]) new Object[] {e1, e2});
+    return __ofInternal((E[]) new Object[] {e1, e2});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3) {
-    return of((E[]) new Object[] {e1, e2, e3});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4) {
-    return of((E[]) new Object[] {e1, e2, e3, e4});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5, e6});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5, e6});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8, e9});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8, e9});
   }
 
   @JsIgnore
   static <E> List<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10) {
-    return of((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10});
+    return __ofInternal((E[]) new Object[] {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10});
   }
+
+  // CHECKSTYLE_OFF: Internal only method that cannot collide with future JRE changes
+  /**
+   * Internal-only helper to avoid copying the incoming array, and instead just wrap it with an
+   * immutable List after checking there are no nulls.
+   */
+  @JsIgnore
+  static <E> List<E> __ofInternal(E[] elements) {
+    for (int i = 0; i < elements.length; i++) {
+      checkNotNull(elements[i]);
+    }
+    return Collections.unmodifiableList(Arrays.asList(elements));
+  }
+  // CHECKSTYLE_ON
 
   @JsIgnore
   static <E> List<E> of(E... elements) {

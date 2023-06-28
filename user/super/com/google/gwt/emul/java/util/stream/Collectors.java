26faec52c6f3766ddf45c23b7a3ae5c187f7e33b
Colin Alworth <colin@vertispan.com>
2023-06-28 11:44:03 -0500

    Remove unmodifiable methods, which belong in a later PR

--- user/super/com/google/gwt/emul/java/util/stream/Collectors.java
+++ user/super/com/google/gwt/emul/java/util/stream/Collectors.java
@@ -337,11 +337,6 @@ public final class Collectors {
     return toCollection(ArrayList::new);
   }
 
-  public static <T> Collector<T, ?, List<T>> toUnmodifiableList() {
-    Collector<T, ?, List<T>> mapping = mapping(Objects::requireNonNull, toList());
-    return collectingAndThen(mapping, Collections::unmodifiableList);
-  }
-
   public static <T, K, U> Collector<T, ?, Map<K, U>> toMap(
       final Function<? super T, ? extends K> keyMapper,
       final Function<? super T, ? extends U> valueMapper) {
@@ -358,27 +353,6 @@ public final class Collectors {
       Function<? super T, ? extends U> valueMapper,
       BinaryOperator<U> mergeFunction) {
     return toMap(keyMapper, valueMapper, mergeFunction, HashMap::new);
-  }
-
-  public static <T, K, U> Collector<T, ?, Map<K, U>> toUnmodifiableMap(Function<? super T, ?
-      extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
-    return collectingAndThen(
-            toMap(disallowNulls(keyMapper), disallowNulls(valueMapper)),
-            Collections::unmodifiableMap
-    );
-  }
-
-  public static <T, K, U> Collector<T, ?, Map<K, U>> toUnmodifiableMap(Function<? super T, ?
-      extends K> keyMapper, Function<? super T, ? extends U> valueMapper, BinaryOperator<U>
-                                                                            mergeFunction) {
-    return collectingAndThen(
-            toMap(disallowNulls(keyMapper), disallowNulls(valueMapper), mergeFunction),
-            Collections::unmodifiableMap
-    );
-  }
-
-  private static  <T, R> Function<T, R> disallowNulls(Function<T, R> func) {
-    return func.andThen(Objects::requireNonNull);
   }
 
   public static <T, K, U, M extends Map<K, U>> Collector<T, ?, M> toMap(
@@ -411,11 +385,6 @@ public final class Collectors {
         s -> s,
         Collector.Characteristics.UNORDERED, Collector.Characteristics.IDENTITY_FINISH
     );
-  }
-
-  public static <T> Collector<T, ?, Set<T>> toUnmodifiableSet() {
-    Collector<T, ?, Set<T>> mapping = mapping(Objects::requireNonNull, toSet());
-    return collectingAndThen(mapping, Collections::unmodifiableSet);
   }
 
   private static <T, D, A> D streamAndCollect(Collector<? super T, A, D> downstream, List<T> list) {

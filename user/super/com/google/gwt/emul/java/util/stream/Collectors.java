b07159311da53bfe27400c73ffb577bc9a24fbf6
Colin Alworth <colin@vertispan.com>
2023-06-28 13:05:15 -0500

    Remove extra import

--- user/super/com/google/gwt/emul/java/util/stream/Collectors.java
+++ user/super/com/google/gwt/emul/java/util/stream/Collectors.java
@@ -18,7 +18,6 @@ package java.util.stream;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.HashMap;

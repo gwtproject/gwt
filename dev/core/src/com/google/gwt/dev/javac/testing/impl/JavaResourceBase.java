d250e5870ee7aa17cdab6b682070e2d3df0fe35f
Colin Alworth <colin@vertispan.com>
2024-03-04 16:49:45 -0600

    Add missing class that tests need, add exceptions in test to fix behavior

--- dev/core/src/com/google/gwt/dev/javac/testing/impl/JavaResourceBase.java
+++ dev/core/src/com/google/gwt/dev/javac/testing/impl/JavaResourceBase.java
@@ -275,6 +275,12 @@ public class JavaResourceBase {
           "  public Class<?> getClass() { return ___clazz; }",
           "}");
 
+  // This class must exist for JDT to be able to compile record types.
+  public static final MockJavaResource OBJECTMETHODS =
+      createMockJavaResource("java.lang.runtime.ObjectMethods",
+          "package java.lang.runtime;",
+          "public class ObjectMethods {}");
+
   // We only need Objects.hash() for Records - the real JRE would synthesize these methods on the
   // fly using ObjectMethods, but we need to generate the code up front. This implementation is
   // wrong, but the important thing is only that it exists for these tests.
@@ -461,10 +467,11 @@ public class JavaResourceBase {
         AUTOCLOSEABLE, ANNOTATION, ARRAY_LIST, BYTE, BOOLEAN, CHARACTER, CHAR_SEQUENCE, CLASS,
         CLASS_NOT_FOUND_EXCEPTION, CLONEABLE, COLLECTION, COMPARABLE, DOUBLE, ENUM, EXCEPTION,
         ERROR, FUNCTIONALINTERFACE, FLOAT, INTEGER, IS_SERIALIZABLE, JAVASCRIPTEXCEPTION,
-        JAVASCRIPTOBJECT, LIST, LONG, MAP, NO_CLASS_DEF_FOUND_ERROR, NUMBER, OBJECT, OBJECTS,
-        RECORD, RUNTIME_EXCEPTION, SERIALIZABLE, SHORT, STRING, STRING_BUILDER, SUPPRESS_WARNINGS,
-        SYSTEM, THROWABLE, SPECIALIZE_METHOD, DO_NOT_AUTOBOX, JSTYPE, JSCONSTRUCTOR, JSPACKAGE,
-        JSPROPERTY, JSMETHOD, JSIGNORE, JSFUNCTION, JSOVERLAY, JSOPTIONAL};
+        JAVASCRIPTOBJECT, LIST, LONG, MAP, NO_CLASS_DEF_FOUND_ERROR, NUMBER, OBJECT, OBJECTMETHODS,
+        OBJECTS, RECORD, RUNTIME_EXCEPTION, SERIALIZABLE, SHORT, STRING, STRING_BUILDER,
+        SUPPRESS_WARNINGS, SYSTEM, THROWABLE, SPECIALIZE_METHOD, DO_NOT_AUTOBOX, JSTYPE,
+        JSCONSTRUCTOR, JSPACKAGE, JSPROPERTY, JSMETHOD, JSIGNORE, JSFUNCTION, JSOVERLAY,
+        JSOPTIONAL};
   }
 
   /**

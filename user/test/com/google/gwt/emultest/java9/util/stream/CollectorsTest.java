d5173a8e504ff27c1ae7826bb250fab268940b33
Dmitrii Tikhomirov <chani.liet@gmail.com>
2024-04-15 21:08:38 -0700

    Each mapped stream must be closed after its contents have been placed downstream

--- user/test/com/google/gwt/emultest/java9/util/stream/CollectorsTest.java
+++ user/test/com/google/gwt/emultest/java9/util/stream/CollectorsTest.java
@@ -49,6 +49,11 @@ public class CollectorsTest extends EmulTestBase {
       return items.stream();
     }, toList());
     applyItems(Arrays.asList("a"), flatMappingToNull, Arrays.asList("a"), Arrays.asList("b", "c"));
+
+    int[] calledCount = {0};
+    Stream<String> mapped = Stream.of("x").onClose(() -> calledCount[0]++);
+    Stream.of(1).collect(flatMapping(x -> mapped, toList()));
+    assertEquals(1, calledCount[0]);
   }
 
   public void testFiltering() {

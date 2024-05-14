7e3a1e6e9b53050171fcf1c7acaa93c9e53469f6
Colin Alworth <colin@vertispan.com>
2024-05-14 16:02:09 -0500

    Require an extra local for varargs if accessing the var has side effects

--- dev/core/src/com/google/gwt/dev/jjs/impl/ImplementJsVarargs.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/ImplementJsVarargs.java
@@ -463,7 +463,7 @@ public class ImplementJsVarargs {
       // qualifier to make sure it is only evaluated once.
       JExpression instance = x.getInstance();
       if (x.getTarget().needsDynamicDispatch() && !x.isStaticDispatchOnly()
-          && instance != null && !(instance instanceof JVariableRef)) {
+          && instance != null && !(instance instanceof JVariableRef && !instance.hasSideEffects())) {
         // Move the potentially sideffecting qualifier to a temporary variable so that
         // the code generation for calls that need .apply don't need to hande the case.
         SourceInfo sourceInfo = x.getSourceInfo();

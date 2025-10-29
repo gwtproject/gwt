79b7bca7965c0e9865e30447ecdac3c209284580
Zbynek Konecny <zbynek1729@gmail.com>
2025-10-29 02:14:51 +0100

    Add JavaDoc for test
    Co-authored-by: Colin Alworth <colin@vertispan.com>

--- user/test/com/google/gwt/emultest/java17/util/stream/IntStreamTest.java
+++ user/test/com/google/gwt/emultest/java17/util/stream/IntStreamTest.java
@@ -21,6 +21,9 @@ import com.google.gwt.junit.DoNotRunWith;
 import com.google.gwt.junit.JUnitShell;
 import com.google.gwt.junit.Platform;
 
+/**
+ * Tests for java.util.stream.IntStream Java 17 emulation.
+ */
 @DoNotRunWith(Platform.Devel)
 public class IntStreamTest extends EmulTestBase {
 

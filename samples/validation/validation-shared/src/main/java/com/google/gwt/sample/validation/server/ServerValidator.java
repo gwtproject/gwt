9d64950e1fc295c5969e8f369205e7a03dbfbadd
Colin Alworth <colin@vertispan.com>
2026-01-12 13:44:06 -0600

    Quick self-review

--- samples/validation/validation-shared/src/main/java/com/google/gwt/sample/validation/server/ServerValidator.java
+++ samples/validation/validation-shared/src/main/java/com/google/gwt/sample/validation/server/ServerValidator.java
@@ -22,8 +22,6 @@ import java.lang.reflect.Method;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-
-
 /**
  * Fails only on the server if the persons name is "Fail".
  */
